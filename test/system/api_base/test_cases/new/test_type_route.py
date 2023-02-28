ab80feacf25b5399f1a75648f667792d84b026c1
Edward Moulsdale <em29@sanger.ac.uk>
2023-02-28 13:54:17 +0000

    new test

--- test/system/api_base/test_cases/new/test_type_route.py
+++ test/system/api_base/test_cases/new/test_type_route.py
@@ -27,3 +27,49 @@ class TestTypeRoute(BaseTestCase):
                 }
             }
         )
+
+    def test_compound_type_route(self):
+        # the class to add doc on
+        class TestClass:
+            @classmethod
+            @type_route('/test', 'GET')
+            def test_get(cls):
+                pass
+        
+            @classmethod
+            @type_route('/test', 'POST')
+            def test_post(cls):
+                pass
+        
+            @classmethod
+            @type_route('/test', 'DELETE')
+            def test_delete(cls):
+                pass
+        
+            @classmethod
+            @type_route('/test/<id>', 'GET')
+            def test_detail_get(cls):
+                pass
+
+            @classmethod
+            @type_route('/test/<id>', 'PATCH')
+            def test_detail_patch(cls):
+                pass
+        
+        self.assertTrue(
+            hasattr(TestClass, '_doc')
+        )
+        self.assertEqual(
+            TestClass._doc,
+            {
+                '/test': {
+                    'GET': TestClass.test_get,
+                    'POST': TestClass.test_post,
+                    'DELETE': TestClass.test_delete
+                },
+                '/test/<id>': {
+                    'GET': TestClass.test_detail_get,
+                    'PATCH': TestClass.test_detail_patch
+                }
+            }
+        )
