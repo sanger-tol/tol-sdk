d7e98dd447a0f124bdc9e76ab2e383248b99797a
Edward Moulsdale <em29@sanger.ac.uk>
2023-03-03 13:42:04 +0000

    add enums

---
+++ src/tol/api_base/utils/enum.py
@@ -0,0 +1,38 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+
+from enum import Enum
+
+
+class _StrEnum(str, Enum):
+    pass
+
+
+class Methods(_StrEnum):
+    """
+    An enum containing all of the methods possible
+    on a type.
+    """
+
+    GET = 'GET'
+    BULK_GET = 'BULK_GET'
+    CREATE = 'CREATE'
+    BULK_CREATE = 'BULK_CREATE'
+    UPDATE = 'UPDATE'
+    BULK_UPDATE = 'BULK_UPDATE'
+    DELETE = 'DELETE'
+    BULK_DELETE = 'BULK_DELETE'
+    UPSERT = 'UPSERT'
+    BULK_UPSERT = 'UPSERT'
+
+
+class Sources(_StrEnum):
+    """
+    An enum containing all possible
+    sources of data
+    """
+    DATABASE = 'DATABASE'
+    ELASTIC = 'ELASTIC'
+    JIRA = 'JIRA'
