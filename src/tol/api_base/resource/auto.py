a70856f2a265d0841ae05f6b35285f2b58366727
Edward Moulsdale <em29@sanger.ac.uk>
2023-03-21 15:01:37 +0000

    new code

--- src/tol/api_base/resource/auto.py
+++ src/tol/api_base/resource/auto.py
@@ -2,10 +2,39 @@
 #
 # SPDX-License-Identifier: MIT
 
+from typing import List
 
-class AutoResouceCollection:
+from flask_restx import (
+    Namespace as FlaskRestxNamespace,
+    Resource
+)
+
+from ..utils.config import IndividualConfig
+from ..service.namespace import ServiceNamespace
+
+
+class ApiNamespace(FlaskRestxNamespace):
     """
     Takes a service namespace and implements it using
     flask-restx resources. Consider this a private API.
     """
-    pass
+    
+    def __init__(
+        self,
+        config: IndividualConfig,
+        custom_service_ns: ServiceNamespace = None,
+        description: str = None
+    ) -> None:
+        object_type = IndividualConfig.object_type
+        self.__config = config
+        self.__service_ns = custom_service_ns
+        self.__resources: List[Resource] = []
+        super(FlaskRestxNamespace, self).__init__(
+            object_type,
+            description=description,
+            path=f'/{object_type}'
+        )
+        self.__initialise_resources()
+
+    def __initialise_resources(self) -> None:
+        pass
---
+++ test/unit/api_base/test_api_namespace.py
@@ -0,0 +1,62 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from copy import deepcopy
+
+from marshmallow_jsonapi import fields as schema_fields
+
+from tol.api_base import IdSchemes, tol_fields
+from tol.api_base.resource.auto import ApiNamespace
+from tol.api_base.utils.config import IndividualConfig
+
+from .data_source import _TestDataSource
+
+# TODO test:
+# - all declared fields are on the schema
+# - datatypes are correct
+# - dump only is respected
+# - required fields are respected
+# - excluded fields are respected
+# - update works correctly, with a correctly injected
+#   and instantiated DataModel object
+# - update is passed the ResouceMeta data
+
+
+INDIVIDUAL_CONFIG_DICT = {
+    'object_type': 'specimen',
+    'data_source': _TestDataSource({}),
+    'id_scheme': IdSchemes.EXTERNAL,
+    'methods': {
+        'auth': [
+            'get_by_id'
+        ],
+        'noauth': [
+            'get_list_page'
+        ]
+    },
+    'attributes': {
+        'tolid': tol_fields.String(unique=True, example='mHomSap52', required=True),
+        'active': tol_fields.Boolean(required=False)
+    },
+    'relationships': {
+        'one': {
+            'species': tol_fields.ToOneRelationship(
+                'species',
+                'taxon_id',
+                required=True,
+                example='9606'
+            ),
+            'creator': tol_fields.ToOneRelationship(
+                'users',
+                'created_by',
+                required=True,
+                example='1',
+                dump_only=True
+            )
+        },
+        'many': {
+            'samples': 'samples'
+        }
+    }
+}
