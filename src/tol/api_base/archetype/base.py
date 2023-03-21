59b86e4cc8b4fd7d85f0d7c0864bb93cdebcc11d
Edward Moulsdale <em29@sanger.ac.uk>
2023-03-21 11:03:21 +0000

    add Archetype base

--- src/tol/api_base/archetype/base.py
+++ src/tol/api_base/archetype/base.py
@@ -2,15 +2,26 @@
 #
 # SPDX-License-Identifier: MIT
 
-from abc import ABC
+from abc import ABC, abstractmethod
+
+from ..utils.config import IndividualConfig
+from ...core.datasource import DataSource
 
 
 class Archetype(ABC):
     """
-    Specifies behaviour for a declarative Archetype
-    class for an object_type. Contains:
-
-    - fields
-    - relationships
-    - metadata
+    A declarative Archetype class for an object_type.
+    
+    Must implement the to_config method.
     """
+
+    def __init__(self, data_source: DataSource) -> None:
+        self.__data_source = data_source
+
+    @property
+    def data_source(self) -> DataSource:
+        return self.__data_source
+
+    @abstractmethod
+    def to_config(self) -> IndividualConfig:
+        pass
