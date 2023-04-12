8d42a48cf70e8b9901ede5e6d71569ee914d8191
Edward Moulsdale <em29@sanger.ac.uk>
2023-04-12 10:07:30 +0100

    skeletal test structure

---
+++ test/unit/api_client/test_api_object_serializer.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from tol.api_client.api_object_serializer import ApiDataObjectSerializer
+
+
+serializer = ApiDataObjectSerializer()
+
+
+class TestApiDataObjectSerializer:
+    def test_single_object(self):
+        pass
+
+    def test_many_objects_same_type(self):
+        pass
+
+    def test_to_one_reference(self):
+        pass
+
+    def test_to_one_reference_chain(self):
+        pass
+
+    def test_to_many_references(self):
+        pass
+
+    def test_both_to_one_to_many_references(self):
+        pass
