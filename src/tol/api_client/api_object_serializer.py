ba4b05e5ac6d13a68f9821dd4dbf6d900b718e01
Edward Moulsdale <em29@sanger.ac.uk>
2023-04-12 10:00:13 +0100

    add new class

---
+++ src/tol/api_client/api_object_serializer.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from ..core import DataObject
+
+
+class ApiDataObjectSerializer:
+    """
+    Serializes a complex, nested list of DataObjects into
+    a flat JSON array.
+    """
+    pass
