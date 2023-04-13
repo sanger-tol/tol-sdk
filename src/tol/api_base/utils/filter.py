f3290ae07b43db06b9a325679b093519e53dfee3
Kiernan Harding <kh16@sanger.ac.uk>
2023-04-13 14:38:18 +0100

    temp back-up

--- src/tol/api_base/utils/filter.py
+++ src/tol/api_base/utils/filter.py
@@ -4,6 +4,7 @@
 
 import json
 from typing import Dict, Tuple
+import logging
 
 from ..error import BadParameterStringException
 
@@ -20,9 +21,16 @@ def parse_filters(filter_string: str) -> Tuple[Dict, Dict, Dict]:
             'The filter parameter string is not a valid JSON object.'
         )
 
-def parse_relationship_filter_key(key: str) -> Tuple:
-    
-    pass
+def parse_relationship_filter_joins(filters: Tuple[Dict, Dict, Dict]) -> Tuple:
+    joins = []
+    for filter_type in filters:
+        if filter_type:
+            for key in filter_type.keys():
+                relationship = key.split('.')
+                if len(relationship) > 1:
+                    for r in range(len(relationship)-1):
+                        joins.append(relationship[r])
+    return filters + tuple(set(joins))
 
 def parse_range_filters(range_dict: dict) -> Tuple[str, str]:
     if len(range_dict) != 2 or 'from' not in range_dict or 'to' not in range_dict:
