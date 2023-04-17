16ca1a1f029adc65af1b2e227e11b017fd69734d
Edward Moulsdale <em29@sanger.ac.uk>
2023-04-17 12:20:35 +0100

    better

--- src/tol/api_client/api_data_object.py
+++ src/tol/api_client/api_data_object.py
@@ -6,7 +6,7 @@ from __future__ import annotations
 
 import typing
 
-from typing import Any, Dict
+from typing import Any, Dict, List
 
 from ..core import DataDict, DataObject
 
@@ -27,5 +27,27 @@ class ApiResponseDataObject(DataObject):
     ):
         self.__data_source = data_source
         self.__json_api_response = json_api_response
+        self.__init_relationships()
         object_type = json_api_response['type']
         super().__init__(object_type, data)
+
+    def __init_relationships(self) -> None:
+        self.__relationships = self.__json_api_response.get(
+            'relationships',
+            {}
+        )
+        for relationship in self.__relationships:
+            v = property(
+                lambda : self.__get_relationship_link(relationship)
+            )
+            setattr(
+                self,
+                relationship,
+                v
+            )
+        
+    def __get_relationship_link(
+        self,
+        relationship: str
+    ) -> ApiResponseDataObject:
+        pass
