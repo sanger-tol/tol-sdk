b3040444e135dafd7e2bb1dbe6c28c7fdb45645a
Edward Moulsdale <em29@sanger.ac.uk>
2023-04-25 13:25:59 +0100

    skeletal schema code

---
+++ src/tol/api_base/datasource/schema.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from marshmallow import Schema
+
+
+class UpsertSchema(Schema):
+    pass
