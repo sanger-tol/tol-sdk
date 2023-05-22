fca9f57a193b056d9d5f3971a5f88e092f74afc2
Edward Moulsdale <em29@sanger.ac.uk>
2023-05-22 14:59:25 +0100

    type hint and good code

--- src/tol/benchling/datasource.py
+++ src/tol/benchling/datasource.py
@@ -5,8 +5,10 @@
 from typing import Dict, Iterable, List, Optional
 
 import psycopg2
+from psycopg2.extensions import connection
 
 from ..core import (
+    CoreDataObject,
     DataObject,
     ReadOnlyDataSource,
     DataSourceConfig,
@@ -45,27 +47,47 @@ class BenchlingDataSource(ReadOnlyDataSource):
         object_ids: Iterable[str],
         **kwargs
     ) -> Iterable[Optional[DataObject]]:
-        conn = self.__get_connection()
-        with conn.cursor() as cur:
-            formatted_ids = "', '".join(object_ids)
-            cur.execute(
-                f"""
-                SELECT c.barcode, c.name, subsam.name$, dna.name$, t.tolid, t.tubewell_id
-FROM pacbio_sequencing_submission2$raw AS pbsum
-LEFT JOIN container$raw AS c ON pbsum.sample_tube_id = c.id
-LEFT JOIN container_content$raw AS cc ON pbsum.sample_tube_id = cc.container_id
-LEFT JOIN submission_samples$raw AS subsam ON cc.entity_id = subsam.id
-LEFT JOIN dna_extract$raw AS dna ON subsam.original_dna_extract = dna.id
-LEFT JOIN tissue_prep$raw AS tp ON dna.tissue_prep = tp.id
-LEFT JOIN tissue$raw AS t ON tp.tissue = t.id
-WHERE c.archived$ = 'FALSE'
-	AND pbsum.archived$ = 'FALSE'
-	AND subsam.archived$ = 'FALSE'
-	AND dna.archived$ = 'FALSE'
-    AND c.barcode in ('{formatted_ids}')
-                """
-            )
-            return cur.fetchall()
+        with self.__get_connection() as conn:
+            with conn.cursor() as cur:
+                formatted_ids = "', '".join(object_ids)
+                cur.execute(
+                    f"""
+                    SELECT c.barcode, t.tolid, t.tubewell_id
+    FROM pacbio_sequencing_submission2$raw AS pbsum
+    LEFT JOIN container$raw AS c ON pbsum.sample_tube_id = c.id
+    LEFT JOIN container_content$raw AS cc ON pbsum.sample_tube_id = cc.container_id
+    LEFT JOIN submission_samples$raw AS subsam ON cc.entity_id = subsam.id
+    LEFT JOIN dna_extract$raw AS dna ON subsam.original_dna_extract = dna.id
+    LEFT JOIN tissue_prep$raw AS tp ON dna.tissue_prep = tp.id
+    LEFT JOIN tissue$raw AS t ON tp.tissue = t.id
+    WHERE c.archived$ = 'FALSE'
+        AND pbsum.archived$ = 'FALSE'
+        AND subsam.archived$ = 'FALSE'
+        AND dna.archived$ = 'FALSE'
+        AND c.barcode in ('{formatted_ids}')
+                    """
+                )
+                results = cur.fetchall()
+                names = (
+                    'id',
+                    'tolid',
+                    "TUBE_OR_WELL_ID",
+                )
+                result_dicts = [
+                    {
+                        k: v for k, v in zip(names, r)
+                    }
+                    for r in results
+                ]
+                data_objects = [
+                    CoreDataObject(
+                        object_type,
+                        data=r_dict,
+                        id_ = r_dict['id']
+                    )
+                    for r_dict in result_dicts
+                ]
+                return data_objects
 
     @unsupported
     def get_list(self, *args, **kwargs):
@@ -75,7 +97,7 @@ WHERE c.archived$ = 'FALSE'
     def get_list_page(self, *args, **kwargs):
         pass
 
-    def __get_connection(self):
+    def __get_connection(self) -> connection:
         return psycopg2.connect(
             user=self.username,
             password=self.password,
