2757be0e108614e0d4ad7c493f1bff195d70f409
Edward Moulsdale <em29@sanger.ac.uk>
2023-05-22 15:21:26 +0100

    get_list working too

--- src/tol/benchling/datasource.py
+++ src/tol/benchling/datasource.py
@@ -97,26 +97,25 @@ class BenchlingDataSource(ReadOnlyDataSource):
 
     def get_list(self, object_type: str, **kwargs) -> Iterable[DataObject]:
         with self.__get_connection() as conn:
-            while True:
-                with conn.cursor() as cur:
-                    cur.execute(
-                        f"""
-                        SELECT c.barcode, t.tolid, t.tubewell_id
-        FROM pacbio_sequencing_submission2$raw AS pbsum
-        LEFT JOIN container$raw AS c ON pbsum.sample_tube_id = c.id
-        LEFT JOIN container_content$raw AS cc ON pbsum.sample_tube_id = cc.container_id
-        LEFT JOIN submission_samples$raw AS subsam ON cc.entity_id = subsam.id
-        LEFT JOIN dna_extract$raw AS dna ON subsam.original_dna_extract = dna.id
-        LEFT JOIN tissue_prep$raw AS tp ON dna.tissue_prep = tp.id
-        LEFT JOIN tissue$raw AS t ON tp.tissue = t.id
-        WHERE c.archived$ = 'FALSE'
-            AND pbsum.archived$ = 'FALSE'
-            AND subsam.archived$ = 'FALSE'
-            AND dna.archived$ = 'FALSE'
-        LIMIT 100;
-                        """
-                    )
-                    result = cur.fetchone()
+            with conn.cursor() as cur:
+                cur.execute(
+                    f"""
+                    SELECT c.barcode, t.tolid, t.tubewell_id
+    FROM pacbio_sequencing_submission2$raw AS pbsum
+    LEFT JOIN container$raw AS c ON pbsum.sample_tube_id = c.id
+    LEFT JOIN container_content$raw AS cc ON pbsum.sample_tube_id = cc.container_id
+    LEFT JOIN submission_samples$raw AS subsam ON cc.entity_id = subsam.id
+    LEFT JOIN dna_extract$raw AS dna ON subsam.original_dna_extract = dna.id
+    LEFT JOIN tissue_prep$raw AS tp ON dna.tissue_prep = tp.id
+    LEFT JOIN tissue$raw AS t ON tp.tissue = t.id
+    WHERE c.archived$ = 'FALSE'
+        AND pbsum.archived$ = 'FALSE'
+        AND subsam.archived$ = 'FALSE'
+        AND dna.archived$ = 'FALSE'
+    LIMIT 100;
+                    """
+                )
+                for result in cur:
                     names = (
                         'id',
                         'tolid',
