441bf3438415f0dbeaeb5c147feb28a0dce976d7
William Eagles <weaglesbio@gmail.com>
2023-05-22 16:11:23 +0100

    Initial commit

--- src/tol/ena/ena_datasource.py
+++ src/tol/ena/ena_datasource.py
@@ -3,15 +3,21 @@
 # SPDX-License-Identifier: MIT
 
 import xml.etree.ElementTree as ElementTree
-from typing import Dict, Tuple
+from typing import Dict, Iterable, Tuple
+import json
 
 import requests
 from requests.auth import HTTPBasicAuth
 
 import tol.ena.ena_methods as em
 
-from ..core import DataSource, DataSourceError
-
+from ..core import (
+    CoreDataObject,
+    DataSourceError,
+    DataSourceFilter,
+    DataSource,
+    unsupported    
+)
 
 class EnaDataSource(DataSource):
 
@@ -39,6 +45,42 @@ class EnaDataSource(DataSource):
                                   detail=f"(status code '{str(response.status_code)}')'")
 
         return response
+
+    def _get_field_mappings_ena(self):
+        return {
+            "project_name":"project_name"
+        }
+
+    def _get_ena_xml_search_query(self):
+        return {
+            "project_name":"project_name"
+        }
+        sql = f"""
+        
+        
+        """
+
+    def get_list(
+        self,
+        object_type: str,
+        object_filters: DataSourceFilter = None,
+        **kwargs
+    ) -> Iterable[CoreDataObject]:
+
+        if object_type != 'sample':
+            raise DataSourceError('Only objects of type sample are supported')
+        if object_filters is None or \
+                not isinstance(object_filters.contains, dict) or \
+                'project_name' not in object_filters.contains:
+            raise DataSourceError('Filter must contain project_name contains filter')
+
+        #  output = self.get_request(f'/ena/browser/api/xml/{checklist_id}')
+        output = self.get_request(f'/ena/browser/api/xml/search?result=sample&query=project_name%3D%22DTOL%22&limit=10&fields=all&includeLinks=false')
+
+        # request_data_dict = json.loads(output.text)
+        return output.text
+        # return CoreDataObject(object_type, data=request_data_dict)
+
 
     def get_xml_checklist(self, checklist_id: str) -> Dict[str, Tuple[str, str, object]]:
         output = self.get_request(f'/ena/browser/api/xml/{checklist_id}')
@@ -97,3 +139,19 @@ class EnaDataSource(DataSource):
             return False, errors
         else:
             return True, assigned_samples
+
+
+    @unsupported
+    def get_by_id(self, *args, **kwargs):
+        pass
+
+    @unsupported
+    def get_list_page(self, *args, **kwargs):
+        pass
+
+    @property
+    def supported_types(self):
+        raise NotImplementedError()
+
+    def get_attribute_types(self, object_type: str) -> Dict:
+        raise NotImplementedError()
