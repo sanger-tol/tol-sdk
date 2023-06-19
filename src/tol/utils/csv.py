fe78bc7e4803ff3308d6e5000d5da7b43c106ba0
Kiernan Harding <kh16@sanger.ac.uk>
2023-06-19 13:32:35 +0100

    TOLP-5782: CSV to JSON converter

---
+++ src/tol/utils/csv.py
@@ -0,0 +1,18 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+import numpy as np
+
+import pandas as pd
+
+
+def convert_csv_to_json(file_path):
+    csv_file = pd.read_csv(
+        file_path,
+        sep=',',
+        header=0,
+        index_col=False
+    )
+    csv_file.replace({np.nan: None}, inplace=True)
+    return csv_file.to_dict(orient='records')
---
+++ test/unit/utils/csv.py
@@ -0,0 +1,31 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+import os
+from unittest import (TestCase)
+
+from tol.utils import convert_csv_to_json
+
+
+class TestCSV(TestCase):
+    def test_convert_csv_to_json(self):
+        expected = [
+            {
+                'order': 'Sapindales',
+                'family': 'Sapindaceae',
+                'genus': 'Acer',
+                'species': 'acuminatum'
+            },
+            {
+                'order': 'Sapindales',
+                'family': 'Sapindaceae',
+                'genus': None,
+                'species': 'albopurpurascens'
+            }
+        ]
+
+        dir_name = os.path.dirname(__file__)
+        file = open(f'{dir_name}/test.csv', 'rb')
+        json = convert_csv_to_json(file)
+        self.assertEqual(expected, json)
