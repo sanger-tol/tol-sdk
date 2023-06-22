ec89b469e5030228ab809b193c639347f01b9122
Andrew Varley <av10@sanger.ac.uk>
2023-06-22 22:00:23 +0100

    TOLP-5845 Sort out "nan"s in convert_excel_to_json

--- src/tol/excel/excel.py
+++ src/tol/excel/excel.py
@@ -2,9 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 
+import numpy as np
+
 import pandas as pd
 
 
 def convert_excel_to_json(file, sheet_name):
     excel_data = pd.read_excel(file, sheet_name=sheet_name)
+    excel_data.replace({np.nan: None}, inplace=True)
     return excel_data.to_dict(orient='records')
