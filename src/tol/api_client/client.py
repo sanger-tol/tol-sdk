12a5011d9b75408c5b22f0d7d2fceeddd341fe6b
Edward Moulsdale <em29@sanger.ac.uk>
2023-07-25 10:57:53 +0100

    template

---
+++ src/tol/api_client/client.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from abc import ABC, abstractmethod
+from typing import Any, Optional
+
+import requests
+
+
+ObjectDump = dict[str, Any]
+
+
+class ApiClient(ABC):
+    """Makes requests to a remote API"""
+
+    @abstractmethod
+    def get_detail(
+        self,
+        type_: str,
+        id_: str
+    ) -> ObjectDump:
+        """
+        Gets the dict-dump of a `DataObject` with the
+        specified type and ID.
+        """
+
+    @abstractmethod
+    def get_page(
+        self,
+        type_: str,
+        page_number: int,
+        page_size: Optional[int],
+        filters: Optional[dict[str, Any]],
+        sort_by: Optional[str]
+    ) -> list[ObjectDump]
