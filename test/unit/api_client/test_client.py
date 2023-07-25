f50347f442eaf2c14f6d36463c5d9b17cb663c84
Edward Moulsdale <em29@sanger.ac.uk>
2023-07-25 11:05:17 +0100

    test class and docstring

--- test/unit/api_client/test_client.py
+++ test/unit/api_client/test_client.py
@@ -1,3 +1,14 @@
 # SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 #
 # SPDX-License-Identifier: MIT
+
+import responses
+
+from tol.api_client.client import DefaultApiClient
+
+
+class TestDefaultApiClient:
+    """
+    Tests `DefaultApiClient`, with outward calls mocked using
+    `responses`.
+    """
