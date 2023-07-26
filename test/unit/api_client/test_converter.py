b9c6e875e99eb75a74ef494c23fe3389b487bb81
Edward Moulsdale <em29@sanger.ac.uk>
2023-07-26 10:19:42 +0100

    class name and docstring

---
+++ test/unit/api_client/test_converter.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from tol.api_client.converter import DefaultObjectParser
+
+
+class TestDefaultObjectParser:
+    """
+    Tests `DefaultObjectParser`
+    """
