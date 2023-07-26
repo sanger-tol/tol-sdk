ac27868aea0fae79aff15333906c720e98efdf46
Edward Moulsdale <em29@sanger.ac.uk>
2023-07-26 10:04:01 +0100

    abc

--- src/tol/api_client/converter.py
+++ src/tol/api_client/converter.py
@@ -1,3 +1,15 @@
 # SPDX-FileCopyrightText: 2023 Genome Research Ltd.
 #
 # SPDX-License-Identifier: MIT
+
+import typing
+from abc import ABC
+
+from ..core import Converter, DataObject, DataObjectFactory
+
+if typing.TYPE_CHECKING:
+    from .client import ObjectDump
+
+
+class ObjectParser(Converter[ObjectDump, DataObject], ABC):
+    """Converts object-dumps back to `DataObject` instances"""
