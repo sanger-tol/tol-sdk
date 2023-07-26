9877786ab9a28fa52056be63147df735bbfc1d5f
Edward Moulsdale <em29@sanger.ac.uk>
2023-07-26 10:25:16 +0100

    new test (broken)

--- test/unit/api_client/test_converter.py
+++ test/unit/api_client/test_converter.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 
+from unittest.mock import Mock
+
 from tol.api_client.converter import DefaultObjectParser
 
 
@@ -12,3 +14,24 @@ class TestDefaultObjectParser:
 
     def test_convert(self):
         """the user-defined `DefaultObjectParser().convert`"""
+
+        in_ = {
+            'type': 'no_fun',
+            'id': '890',
+            'attributes': {
+                'a': 'bc',
+                'bool': True
+            }
+        }
+
+        def __mock_factory(type_, id_ = None, attributes = None):
+            assert type_ == 'no_fun'
+            assert id_ == '890'
+            assert attributes == {
+                'a': 'bc',
+                'bool': True
+            }
+            return Mock()
+
+        parser = DefaultObjectParser(__mock_factory)
+        parser.convert(in_)
