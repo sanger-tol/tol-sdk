a9ececc9b78b763fe6754245be5b52bd42129e58
Edward Moulsdale <em29@sanger.ac.uk>
2023-07-27 11:22:11 +0100

    get abc working with autospec

--- test/unit/api_client/test_with_api.py
+++ test/unit/api_client/test_with_api.py
@@ -2,6 +2,9 @@
 #
 # SPDX-License-Identifier: MIT
 
+from typing import Iterable
+from unittest.mock import create_autospec
+
 from tol.api_base2 import data_blueprint
 from tol.core import DataSource
 from tol.core.operator import (
@@ -12,5 +15,20 @@ from tol.core.operator import (
     Updater,
     Upserter
 )
+
+
+class _AllDataSource(
+    DataSource,
+    Aggregator,
+    Deleter,
+    DetailGetter,
+    PageGetter,
+    Updater,
+    Upserter
+):
+    pass
+
+
+create_autospec(_AllDataSource)
 
 
