31a83ba0095bc0b3bef8f232cfd4a8299a1f97f7
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 10:35:55 +0100

    test name and docstring

---
+++ test/unit/api_client/test_parser.py
@@ -0,0 +1,11 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from tol.api_client.parser import DefaultParser
+
+
+class TestDefaultParser:
+    """
+    Various tests for `DefaultParser`
+    """
