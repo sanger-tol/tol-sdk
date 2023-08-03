b130e4340858819d0723181daaa7bf8f728eac86
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 15:25:02 +0100

    all abstract

--- src/tol/api_client/api_datasource.py
+++ src/tol/api_client/api_datasource.py
@@ -2,18 +2,24 @@
 #
 # SPDX-License-Identifier: MIT
 
-from typing import Callable
+from typing import Callable, Iterable, Optional
 
 from .client import ApiClient, DefaultApiClient
 from .dumper import Dumper, DefaultDumper
 from .parser import Parser, DefaultParser
-from ..core import DataObjectFactory, DataSource
+from ..core import (
+    DataObject,
+    DataObjectFactory,
+    DataSource,
+    DataSourceFilter
+)
 from ..core.operator import (
     DetailGetter,
     ListGetter,
     PageGetter,
     Relational
 )
+from ..core.relationship import RelationshipConfig
 
 
 ClientFactory = Callable[[str, str], ApiClient]
@@ -68,3 +74,53 @@ class ApiDataSource(
         self.__client_factory = client_factory
         self.__dumper_factory = dumper_factory
         self.__parser_factory = parser_factory
+
+    @property
+    def supported_types(self) -> list[str]:
+        return super().supported_types
+
+    @property
+    def relationship_config(self) -> dict[str, RelationshipConfig]:
+        return super().relationship_config
+
+    def get_attribute_types(self, object_type: str) -> dict:
+        raise NotImplementedError()
+
+    def get_by_id(
+        self,
+        object_type: str,
+        object_ids: Iterable[str]
+    ) -> Iterable[Optional[DataObject]]:
+
+        return super().get_by_id(object_type, object_ids)
+
+    def get_list(
+        self,
+        object_type: str,
+        object_filters: Optional[DataSourceFilter] = None
+    ) -> Iterable[DataObject]:
+        return super().get_list(object_type, object_filters)
+
+    def get_list_page(
+        self,
+        object_type: str,
+        page_number: int,
+        page_size: Optional[int] = None,
+        object_filters: Optional[DataSourceFilter] = None,
+        sort_by: Optional[str] = None
+    ) -> tuple[Iterable[DataObject], int]:
+        pass
+
+    def get_to_one_relation(
+        self,
+        source: DataObject,
+        relationship_name: str
+    ) -> Optional[DataObject]:
+        return super().get_to_one_relation(source, relationship_name)
+
+    def get_to_many_relations(
+        self,
+        source: DataObject,
+        relationship_name: str
+    ) -> Iterable[DataObject]:
+        return super().get_to_many_relations(source, relationship_name)
