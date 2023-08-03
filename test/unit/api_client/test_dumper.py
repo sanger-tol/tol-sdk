d47f0e1af317f062ec00d7f572850acb56fc1338
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 09:51:36 +0100

    mock data object

--- test/unit/api_client/test_dumper.py
+++ test/unit/api_client/test_dumper.py
@@ -2,7 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
+from typing import Any
+from unittest.mock import Mock, PropertyMock
+
 from tol.api_client.dumper import DefaultDumper
+from tol.core import DataObject
 
 
 class TestDefaultDumper:
@@ -18,3 +22,21 @@ class TestDefaultDumper:
 
     def test_all(self):
         """Attributes and relationships"""
+
+    def __mock_object(
+        self,
+        type_: str,
+        id_: str,
+        attributes: dict[str, Any] = {},
+        to_one_objects: dict[str, DataObject] = {}
+    ) -> Mock:
+
+        mock_object = Mock()
+        type(mock_object).type = PropertyMock(return_value=type_)
+        type(mock_object).id = PropertyMock(return_value=id_)
+        type(mock_object).attributes = PropertyMock(
+            return_value=attributes
+        )
+        type(mock_object)._to_one_objects = PropertyMock(
+            return_value=to_one_objects
+        )
