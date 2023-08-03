25b907e30802fad9ac123fbac5e249b032c0806f
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 10:33:59 +0100

    stuff

--- src/tol/api_client/parser.py
+++ src/tol/api_client/parser.py
@@ -7,7 +7,7 @@ from abc import ABC
 from .dumper import RelationDict
 from ..api_base2.parser import JsonApiResource
 from ..core.converter import Converter
-from ..core import DataObject
+from ..core import DataObject, DataObjectFactory
 
 
 class Parser(Converter[JsonApiResource, DataObject], ABC):
@@ -15,3 +15,11 @@ class Parser(Converter[JsonApiResource, DataObject], ABC):
     Deserializes `DataObject` instances from `JsonApiResource`
     dumps.
     """
+
+
+class DefaultParser(Parser):
+    def __init__(
+        self,
+        data_object_factory: DataObjectFactory
+    ) -> None:
+        super().__init__()
