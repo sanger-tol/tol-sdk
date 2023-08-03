3cd80df7d3b1294aa8d074665d337d20e1fe8864
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 10:07:49 +0100

    closer

--- src/tol/api_client/dumper.py
+++ src/tol/api_client/dumper.py
@@ -16,6 +16,46 @@ class Dumper(Converter[DataObject, JsonApiResource], ABC):
     """
 
 
+RelationDict = dict[str, JsonApiResource]
+
+
 class DefaultDumper(Dumper):
-    def convert(self, input_: DataObject) -> JsonApiResource:
-        pass
+    def convert(self, data_object: DataObject) -> JsonApiResource:
+        dump = {
+            'type': data_object.type,
+            'id': data_object.id
+        }
+        dump = self.__add_attributes(data_object, dump)
+        dump = self.__add_one_relationships(data_object, dump)
+
+        return dump
+
+    def __add_attributes(
+        self,
+        data_object: DataObject,
+        dump: JsonApiResource
+    ) -> JsonApiResource:
+
+        attributes = data_object.attributes
+        if attributes:
+            dump['attributes'] = attributes
+        return dump
+
+    def __add_one_relationships(
+        self,
+        data_object: DataObject,
+        dump: JsonApiResource
+    ) -> JsonApiResource:
+
+        ones = data_object._to_one_objects
+        if ones:
+            dump['relationships'] = {
+                k: self.__dump_one_relationship(v)
+                for k, v in ones.items()
+            }
+        return dump
+
+    def __dump_one_relationship(self, relation_object) -> RelationDict:
+        return {
+            'data': self.convert(relation_object)
+        }
