fcd3b6c24d44202c0d01dd0fdc25d1d58154726a
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 10:42:24 +0100

    factory method

--- test/unit/api_client/test_parser.py
+++ test/unit/api_client/test_parser.py
@@ -2,7 +2,10 @@
 #
 # SPDX-License-Identifier: MIT
 
+from typing import Any, Optional
+
 from tol.api_client.parser import DefaultParser
+from tol.core import DataObject, DataObjectFactory
 
 
 class TestDefaultParser:
@@ -18,3 +21,28 @@ class TestDefaultParser:
 
     def test_all(self):
         """Attributes and relationships"""
+
+    def __get_factory(
+        self,
+        expected_type: str,
+        expected_id: str,
+        expected_attributes: Optional[dict[str, Any]] = {},
+        expected_to_one_objects: Optional[dict[str, DataObject]] = {},
+        return_value: Any = None
+    ) -> DataObjectFactory:
+
+        expected_data = expected_attributes | expected_to_one_objects
+
+        def __factory(
+            type_: str,
+            id_: str,
+            data: Optional[dict[str, Any]] = {}
+        ) -> Any:
+
+            assert type_ == expected_type
+            assert id_ == expected_id
+            assert data == expected_data
+
+            return return_value
+
+        return __factory
