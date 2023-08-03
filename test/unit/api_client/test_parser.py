c7e40b12ed2999d6f6186a1c9cdca9ce0bac2b9d
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-03 11:05:59 +0100

    first test

--- test/unit/api_client/test_parser.py
+++ test/unit/api_client/test_parser.py
@@ -2,10 +2,9 @@
 #
 # SPDX-License-Identifier: MIT
 
-from typing import Any, Optional
+from unittest.mock import MagicMock
 
 from tol.api_client.parser import DefaultParser
-from tol.core import DataObject, DataObjectFactory
 
 
 class TestDefaultParser:
@@ -16,39 +15,20 @@ class TestDefaultParser:
     def test_empty(self):
         """No attributes or relationships"""
 
-        
+        factory = MagicMock()
+        parser = DefaultParser(factory)
+
+        in_ = {
+            'type': 'hello',
+            'id': 'parser'
+        }
+
+        parser.convert(in_)
+
+        factory.assert_called_once_with('type', 'hello', data=None)
 
     def test_attributes(self):
         """No relationships"""
 
     def test_all(self):
         """Attributes and relationships"""
-
-    def __get_factory(
-        self,
-        expected_type: str,
-        expected_id: str,
-        expected_attributes: dict[str, Any] = {},
-        expected_to_one_objects: dict[str, DataObject] = {},
-        return_value: Optional[Any] = None
-    ) -> DataObjectFactory:
-
-        expected_data = expected_attributes | expected_to_one_objects
-
-        def __factory(
-            type_: str,
-            id_: str,
-            data: Optional[dict[str, Any]] = None
-        ) -> Any:
-
-            assert type_ == expected_type
-            assert id_ == expected_id
-
-            if expected_data:
-                assert data == expected_data
-            else:
-                assert not data
-
-            return return_value
-
-        return __factory
