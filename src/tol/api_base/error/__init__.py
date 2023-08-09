194c99bca1fa4084d115f5f1b6bdf07dea482837
Edward Moulsdale <em29@sanger.ac.uk>
2023-08-09 13:39:26 +0100

    new local, integrated testing

--- src/tol/api_base/error/__init__.py
+++
@@ -1,5 +0,0 @@
-# SPDX-FileCopyrightText: 2022 Genome Research Ltd.
-#
-# SPDX-License-Identifier: MIT
-
-from .error import * # noqa
--- src/tol/jira/__init__.py
+++
@@ -1,7 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
-#
-# SPDX-License-Identifier: MIT
-
-from .jira_auth import *  # noqa
-from .jira_datasource import *  # noqa
-from .jira_methods import *  # noqa
