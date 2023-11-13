1142251b50c07c49556bfd1fc034b42abc3f1d11
Edward Moulsdale <em29@sanger.ac.uk>
2023-11-13 11:19:41 +0000

    first test

--- src/tol/log/logger.py
+++ src/tol/log/logger.py
@@ -6,13 +6,11 @@ from __future__ import annotations
 
 import typing
 from typing import Callable, Optional
+from uuid import uuid4
 
 if typing.TYPE_CHECKING:
     from ..core import DataSource
     from ..core.operator import Upserter
-
-
-IdGetter = Callable[[], Optional[str]]
 
 
 class Logger:
@@ -20,19 +18,19 @@ class Logger:
     Logs access requests using a given `DataSource` instance
     that implements `Upserter`.
 
-    To prevent infinite recursion, `Logger().add()` must not
+    To prevent infinite recursion, `Logger().register()` must not
     have been previously called on this `DataSource` instance.
     """
 
     def __init__(
         self,
         logging_datasource: Upserter,
-        uuid_generator: Callable[[], str] = None,
-        user_id_getter: IdGetter = None
+        user_id_getter: Callable[[], Optional[str]],
+        uuid_generator: Callable[[], str] = lambda: uuid4().hex
     ) -> None:
         pass
 
-    def add(self, logged: DataSource) -> None:
+    def register(self, logged: DataSource) -> None:
         """
         Adds logging to the various operation methods
         supported by the `logged` `DataSource` instance.
--- test/unit/log/test_logger.py
+++ test/unit/log/test_logger.py
@@ -4,7 +4,12 @@
 
 from unittest.mock import Mock, create_autospec
 
+from tol.core.operator import Deleter, Updater, Upserter
 from tol.log import Logger
+
+
+class _Modifier(Deleter, Updater, Upserter):
+    """A `DataSource` that has all unsafe, modifying methods"""
 
 
 class TestLogger:
@@ -13,5 +18,32 @@ class TestLogger:
     def test_none_user_id(self):
         """got `user_id` is `None` -> don't log"""
 
-    def test_set_user_id(self):
-        """`user_id` is set and not `None` -> should log"""
+        mock_upserter = create_autospec(Upserter)
+        logger = Logger(
+            mock_upserter,
+            lambda: None,
+            uuid_generator=lambda: 'test-uuid'
+        )
+
+        # a mock DataSource instance implementing all operators
+        mock_ds = create_autospec(_Modifier)
+
+        # register the `DataSource` with the `Logger` instance
+        logger.register(mock_ds)
+
+        # do all modifications
+        mock_ds.delete('test', ['1', '2', '3'])
+        mock_ds.upsert('test', [Mock(), Mock()])
+        mock_ds.update('test', [Mock(), Mock()])
+
+        # no upserts on the logging `DataSource` instance
+        mock_upserter.upsert.assert_not_called()
+
+    def test_delete_log(self):
+        """`user_id` is set and not `None` -> log delete"""
+
+    def test_update_log(self):
+        """`user_id` is set and not `None` -> log update"""
+
+    def test_upsert_log(self):
+        """`user_id` is set and not `None` -> log upsert"""
