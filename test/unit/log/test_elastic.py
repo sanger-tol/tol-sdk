12d866c7caeddc14976ef0633b1ccedf2dafe88b
Edward Moulsdale <em29@sanger.ac.uk>
2023-11-13 15:29:34 +0000

    great fun

--- test/unit/log/test_elastic.py
+++ test/unit/log/test_elastic.py
@@ -2,8 +2,29 @@
 #
 # SPDX-License-Identifier: MIT
 
+from unittest.mock import Mock
+
 from tol.log import elastic_logger
 
 
 class TestElasticLogger:
     """Tests the `elastic_logger` factory function"""
+
+    def test_arguments(self):
+        """Tests arguments are passed in correctly"""
+
+        mock_ds_class = Mock()
+        elastic_logger(
+            'a fun URI that is really great',
+            'me',
+            'please',
+            factory=lambda c: mock_ds_class(c)
+        )
+
+        mock_ds_class.assert_called_once_with(
+            'a fun URI that is really great',
+            'me',
+            'please',
+            '',
+            {}
+        )
