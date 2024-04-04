c6c83e052fde43d4af113cf5e498f1f9e3963a03
Edward Moulsdale <em29@sanger.ac.uk>
2024-04-04 11:29:28 +0100

    remove bare sql integration test

--- test/system/data_source/fixtures/__init__.py
+++ test/system/data_source/fixtures/__init__.py
@@ -6,12 +6,10 @@ from .api.elastic import api_elastic
 from .api.sql import api_sql
 from .base import DataSourceFixture
 from .elastic_ds import elastic
-from .sql_ds import sql
 
 
 all_fixtures: tuple[DataSourceFixture] = (
     elastic,
-    sql,
     api_elastic,
     api_sql
 )
