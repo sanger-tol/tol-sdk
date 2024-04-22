ee6aff20ccc615b98db48f9da820aee6cf66ebdd
Andrew Varley <av10@sanger.ac.uk>
2024-04-22 16:51:34 +0100

    TOLP-7459 Added JsonDataSource and tolqc_legacy source

---
+++ src/tol/sources/tolqc_legacy.py
@@ -0,0 +1,34 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from ..core import (
+    core_data_object
+)
+from ..json import (
+    JsonDataSource
+)
+
+
+def tolqc_legacy():
+    tolqc_legacy = JsonDataSource({
+        'uri': 'https://tolqc.cog.sanger.ac.uk/data.json',
+        'type': 'species',
+        'id_attribute': 'taxon',
+        'mappings': {
+            'group': {
+                'heading': 'group',
+                'type': 'str'
+            },
+            'hierarchy_name': {
+                'heading': '_name',
+                'type': 'str'
+            },
+            'assembly_stage': {
+                'heading': 'asm.stage',
+                'type': 'str'
+            }
+        }
+    })
+    core_data_object(tolqc_legacy)
+    return tolqc_legacy
