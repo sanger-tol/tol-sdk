f704cf8fca4c9368bd7cb5b7d2a3030cee45ad6f
Andrew Varley <av10@sanger.ac.uk>
2024-04-22 16:51:34 +0100

    TOLP-7459 Added JsonDataSource and tolqc_legacy source

---
+++ src/tol/json/json_datasource.py
@@ -0,0 +1,129 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from functools import cache
+from typing import Any, Dict, Iterable, Optional
+
+from dateutil import parser as dateutil_parser
+
+import requests
+
+from ..core import (
+    DataObject,
+    DataSource,
+    DataSourceError,
+    DataSourceFilter
+)
+from ..core.operator import (
+    DetailGetter,
+    ListGetter
+)
+
+
+class JsonDataSource(
+    DataSource,
+
+    # the supported operators
+    DetailGetter,
+    ListGetter
+):
+    """
+    A `DataSource` that connects to a remote JSON data file
+    """
+
+    def __init__(
+        self,
+        config: Dict
+    ) -> None:
+        super().__init__(
+            config,
+            expected=['uri', 'type', 'id_attribute', 'mappings']
+        )
+        self.__raw_data = self._load_json()
+        self.__keyed_by_id = {
+            v[self.id_attribute]: v
+            for v in self.__raw_data
+            if self.id_attribute in v
+        }
+
+    def _load_json(self):
+        response = requests.get(self.uri)
+        if response.status_code != 200:
+            raise DataSourceError()
+        data = response.json()
+        return data
+
+    def __map_entry(self, entry: Dict):
+        ret = {}
+        for mapping_key, mapping_value in self.mappings.items():
+            if mapping_value['heading'] != self.id_attribute:
+                subentry = entry
+                for level in mapping_value['heading'].split('.'):
+                    if subentry is not None:
+                        subentry = subentry.get(level)
+                ret[mapping_key] = self.__parse_date(mapping_key, subentry)
+        return ret
+
+    def __create_data_object(self, entry: Dict):
+        return self.data_object_factory(
+            self.type,
+            entry.get(self.id_attribute),
+            attributes=self.__map_entry(entry)
+        )
+
+    def __parse_date(self, attribute_name: str, value: Any):
+        if self.mappings[attribute_name]['type'] == 'datetime' and value is not None:
+            return dateutil_parser.parse(value)
+        if self.mappings[attribute_name]['type'] == 'str' and value is not None:
+            return str(value)
+        return value
+
+    @property
+    @cache
+    def attribute_types(self) -> dict[str, dict[str, str]]:
+        return {
+            self.type: {
+                k: v['type']
+                for k, v in self.mappings.items()
+            }
+        }
+
+    @property
+    @cache
+    def supported_types(self) -> list[str]:
+        return list(
+            self.attribute_types.keys()
+        )
+
+    def get_by_id(
+        self,
+        object_type: str,
+        object_ids: Iterable[str]
+    ) -> Iterable[Optional[DataObject]]:
+        if object_type not in self.supported_types:
+            raise DataSourceError(f'{object_type} is not supported')
+
+        return (
+            self.__create_data_object(self.__keyed_by_id[object_id])
+            if self.__keyed_by_id[object_id] is not None else None
+            for object_id in object_ids
+        )
+
+    def get_list(
+        self,
+        object_type: str,
+        object_filters: Optional[DataSourceFilter] = None
+    ) -> Iterable[DataObject]:
+        """
+        Gets an Iterable of DataObject instances of the given
+        type, according to the given DataSourceFilter.
+        """
+        if object_filters is not None:
+            raise DataSourceError('Filtering is not supported on JsonDataSource')
+        if object_type not in self.supported_types:
+            raise DataSourceError(f'{object_type} is not supported')
+        for entry in self.__raw_data:
+            id_ = entry.get(self.id_attribute)
+            if id_ is not None and id_ != '':
+                yield self.__create_data_object(entry)
---
+++ test/unit/json/test_json_datasource.py
@@ -0,0 +1,182 @@
+# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from datetime import datetime
+from unittest import (
+    TestCase,
+    mock
+)
+
+from tol.core import (
+    core_data_object
+)
+from tol.json import (
+    JsonDataSource
+)
+
+
+class MockJsonDataSource(JsonDataSource):
+    def _initialise_google_sheet(self):
+        return mock.Mock()
+
+    def _load_json(self):
+        return [
+            {
+                'Id': 1,
+                'Value': 'Value 1',
+                'Optional': 'YES',
+                'Boolean': True,
+                'Float': 2.34,
+                'Datetime': datetime(2024, 1, 1, 12, 13, 14).isoformat()
+            },
+            {
+                'Id': 2,
+                'Value': 'Value 2',
+                'Optional': None,
+                'Boolean': False,
+                'Float': None,
+                'Datetime': None
+            },
+            {
+                'Id': 3,
+                'Value': 'Value 3',
+                'Optional': 'NO',
+                'Boolean': None,
+                'Float': None,
+                'Datetime': '14/12/2030'
+            },
+            {
+                'Id': 4,
+                'Value': 'Value 4',
+                'Optional': None,
+                'Boolean': None,
+                'Float': None,
+                'Datetime': '2024-07-08 13:14:15'
+            }
+
+        ]
+
+
+def mock_json_data_source() -> JsonDataSource:
+    jds = MockJsonDataSource({
+        'uri': '',  # Not used in mock
+        'type': 'object1',
+        'id_attribute': 'Id',
+        'mappings': {
+            'id': {
+                'heading': 'Id',
+                'type': 'int'
+            },
+            'value': {
+                'heading': 'Value',
+                'type': 'str'
+            },
+            'optional': {
+                'heading': 'Optional',
+                'type': 'str'
+            },
+            'boolean': {
+                'heading': 'Boolean',
+                'type': 'boolean'
+            },
+            'float': {
+                'heading': 'Float',
+                'type': 'float'
+            },
+            'datetime': {
+                'heading': 'Datetime',
+                'type': 'datetime'
+            }
+        }
+    })
+    core_data_object_mock = core_data_object(jds)
+    return core_data_object_mock, jds
+
+
+class TestJsonDataSource(TestCase):
+
+    def test_attribute_types(self):
+        _, jds = mock_json_data_source()
+        expected = {
+            'object1': {
+                'id': 'int',
+                'value': 'str',
+                'optional': 'str',
+                'boolean': 'boolean',
+                'float': 'float',
+                'datetime': 'datetime'
+            }
+        }
+        self.assertEqual(expected, jds.attribute_types)
+        self.assertEqual(['object1'], jds.supported_types)
+
+    def test_get_by_id(self):
+        _, jds = mock_json_data_source()
+
+        ret = jds.get_by_id('object1', [1, 2, 4])
+        obj1 = next(ret)
+        self.assertEqual(1, obj1.id)
+        self.assertEqual({
+            'value': 'Value 1',
+            'optional': 'YES',
+            'boolean': True,
+            'float': 2.34,
+            'datetime': datetime(2024, 1, 1, 12, 13, 14)}, obj1.attributes)
+        obj2 = next(ret)
+        self.assertEqual(2, obj2.id)
+        self.assertEqual({
+            'value': 'Value 2',
+            'optional': None,
+            'boolean': False,
+            'float': None,
+            'datetime': None}, obj2.attributes)
+        obj4 = next(ret)
+        self.assertEqual(4, obj4.id)
+        self.assertEqual({
+            'value': 'Value 4',
+            'optional': None,
+            'boolean': None,
+            'float': None,
+            'datetime': datetime(2024, 7, 8, 13, 14, 15)}, obj4.attributes)
+        with self.assertRaises(StopIteration):
+            next(ret)
+
+    def test_get_list(self):
+        _, jds = mock_json_data_source()
+
+        ret = jds.get_list('object1')
+        obj1 = next(ret)
+        self.assertEqual(1, obj1.id)
+        self.assertEqual({
+            'value': 'Value 1',
+            'optional': 'YES',
+            'boolean': True,
+            'float': 2.34,
+            'datetime': datetime(2024, 1, 1, 12, 13, 14)}, obj1.attributes)
+        obj2 = next(ret)
+        self.assertEqual(2, obj2.id)
+        self.assertEqual({
+            'value': 'Value 2',
+            'optional': None,
+            'boolean': False,
+            'float': None,
+            'datetime': None}, obj2.attributes)
+        obj3 = next(ret)
+        self.assertEqual(3, obj3.id)
+        self.assertEqual({
+            'value': 'Value 3',
+            'optional': 'NO',
+            'boolean': None,
+            'float': None,
+            'datetime': datetime(2030, 12, 14)}, obj3.attributes)
+        obj4 = next(ret)
+        self.assertEqual(4, obj4.id)
+        self.assertEqual({
+            'value': 'Value 4',
+            'optional': None,
+            'boolean': None,
+            'float': None,
+            'datetime': datetime(2024, 7, 8, 13, 14, 15)}, obj4.attributes)
+        with self.assertRaises(StopIteration):
+            next(ret)
