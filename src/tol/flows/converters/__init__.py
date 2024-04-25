af9945ca08cebedddf6ebdaeda476a42c43bba4a
Andrew Varley <av10@sanger.ac.uk>
2024-04-25 15:53:58 +0100

    TOLP-7347 Added STS sample_project to elastic sample converter

--- src/tol/flows/converters/__init__.py
+++ src/tol/flows/converters/__init__.py
@@ -6,6 +6,7 @@ from .elastic_sequencing_request_to_sts_sequencing_request_converter import Elas
 from .goat_taxon_to_elastic_species_converter import GoatTaxonToElasticSpeciesConverter  # noqa F401
 from .labwhere_location_to_elastic_sample_update_converter import LabwhereLocationToElasticSampleUpdateConverter  # noqa F401
 from .sts_banked_sample_to_elastic_sample_converter import StsBankedSampleToElasticSampleConverter  # noqa F401
+from .sts_sample_project_to_elastic_sample_converter import StsSampleProjectToElasticSampleConverter  # noqa F401
 from .sts_species_to_elastic_species_converter import StsSpeciesToElasticSpeciesConverter  # noqa F401
 from .tolqc_species_to_elastic_species_converter import TolqcSpeciesToElasticSpeciesConverter  # noqa F401
 from .tolqc_sample_to_elastic_sequencing_request_converter import TolqcSampleToElasticSequencingRequestConverter  # noqa F401
