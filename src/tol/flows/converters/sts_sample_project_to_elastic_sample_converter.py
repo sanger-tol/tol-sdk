cf4efc00b97cdd221e13c94cdea138ab41f3d303
Andrew Varley <av10@sanger.ac.uk>
2024-04-25 15:53:58 +0100

    TOLP-7347 Added STS sample_project to elastic sample converter

---
+++ src/tol/flows/converters/sts_sample_project_to_elastic_sample_converter.py
@@ -0,0 +1,83 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+import datetime
+import re
+from typing import Iterable
+
+from dateutil import parser as dateutil_parser
+
+from ...core import (
+    DataObject,
+    DataObjectToDataObjectOrUpdateConverter,
+    DataSourceError
+)
+
+
+class StsSampleProjectToElasticSampleConverter(
+        DataObjectToDataObjectOrUpdateConverter):
+    def convert(self, data_object: DataObject) -> Iterable[DataObject]:
+        # The project (note this is adding to a list)
+        s = data_object.sample
+        attributes = {
+            'project': [data_object.project.id],
+            **s.attributes
+        }
+        try:
+            if 'location' in s.to_one_relationships:
+                if s.location is not None:
+                    location = self.__split_location(s.location.location)
+                    if location is not None:
+                        attributes['collection_country'] = location['country']
+                        attributes['collection_locality'] = location['locality']
+                    attributes['latitude'] = s.location.lat
+                    attributes['longitude'] = s.location.long
+                    attributes['elevation'] = s.location.elevation
+                    attributes['depth'] = s.location.depth
+
+            attributes['col_date'] = self.__sanitise_date_field(s.col_date)
+            attributes['original_collection_date'] = \
+                self.__sanitise_date_field(s.original_collection_date)
+            attributes['pre_date'] = self.__sanitise_date_field(s.pre_date)
+            if 'gal' in s.to_one_relationships:
+                if s.gal is not None:
+                    attributes['gal_name'] = s.gal.name
+                    attributes['gal_abbreviation'] = s.gal.abbreviation
+            if 'specimen' in s.to_one_relationships:
+                if s.specimen is not None:
+                    attributes['specimen'] = {'id': s.specimen.id}
+            if 'sampleset' in s.to_one_relationships:
+                if s.sampleset is not None:
+                    attributes['sampleset_id'] = s.sampleset.id
+            # Make tolid a relationship
+            if s.public_name is not None and s.public_name != '':
+                attributes['tolid'] = {'id': s.public_name}
+                attributes['public_name'] = None
+        except DataSourceError:
+            print(f'Problem with sample {s.id}')
+
+        ret = self._data_object_factory(
+            'sample',
+            s.id,
+            attributes=attributes
+        )
+        return iter([ret])
+
+    def __sanitise_date_field(self, date_field: str) -> datetime.datetime:
+        if date_field is None or date_field == '':
+            return None
+        try:
+            parsed_date = dateutil_parser.parse(date_field, fuzzy=False)
+            return parsed_date
+        except ValueError:
+            return None
+
+    def __split_location(self, location: str) -> dict[str, str]:
+        if location is None:
+            return None
+        splits = re.split(r'\s*\|\s*', location)
+        return {
+            'country': splits[0],
+            'locality': ' | '.join(splits[1:])
+        }
---
+++ test/unit/flows/test_sts_sample_project_to_elastic_sample_converter.py
@@ -0,0 +1,173 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+import datetime
+from unittest import (TestCase)
+
+from tol.core import (
+    DataObject,
+    DataSource,
+    core_data_object
+)
+from tol.core.operator import (
+    Relational
+)
+from tol.core.relationship import RelationshipConfig
+from tol.flows.converters import (
+    StsSampleProjectToElasticSampleConverter
+)
+
+
+class _MockDataSourceRelational(DataSource, Relational):
+
+    @property
+    def supported_types(self):
+        return ['sample_project', 'sample', 'project',
+                'location', 'gal', 'sampleset', 'specimen']
+
+    @property
+    def attribute_types(self):
+        raise NotImplementedError()
+
+    @property
+    def relationship_config(self):
+        rc_sample_project = RelationshipConfig()
+        rc_sample_project.to_one = {
+            'sample': 'sample',
+            'project': 'project'
+        }
+        rc_sample = RelationshipConfig()
+        rc_sample.to_one = {
+            'location': 'location',
+            'gal': 'gal',
+            'sampleset': 'sampleset',
+            'specimen': 'specimen'
+        }
+        return {
+            'sample_project': rc_sample_project,
+            'sample': rc_sample
+        }
+
+    def get_to_one_relation(
+        self,
+        source: DataObject,
+        relationship_name: str
+    ):
+        pass
+
+    def get_to_many_relations(
+        self
+    ):
+        raise NotImplementedError()
+
+
+class _MockDataSource(DataSource):
+
+    @property
+    def supported_types(self):
+        return ['sample']
+
+    @property
+    def attribute_types(self):
+        raise NotImplementedError()
+
+
+class TestStsSampleProjectToElasticSampleConverter(TestCase):
+    def test_convert(self):
+
+        source = _MockDataSourceRelational(config={})
+        destination = _MockDataSource(config={})
+        core_data_object(source)
+        core_data_object(destination)
+        converter = StsSampleProjectToElasticSampleConverter(
+            data_object_factory=destination.data_object_factory
+        )
+
+        CoreDataObject = source.data_object_factory  # noqa N806
+        project = CoreDataObject(
+            id_='test_project',
+            type_='project',
+            attributes={}
+        )
+        location = CoreDataObject(
+            id_='test_gal',
+            type_='location',
+            attributes={
+                'location': 'Country | County | Town',
+                'lat': 12.345678,
+                'long': 23.456789,
+                'elevation': 65,
+                'depth': 200
+            }
+        )
+        gal = CoreDataObject(
+            id_='test_gal',
+            type_='gal',
+            attributes={
+                'name': 'Test Gal',
+                'abbreviation': 'TESTGAL'
+            }
+        )
+        specimen = CoreDataObject(
+            id_='test_specimen',
+            type_='specimen',
+            attributes={}
+        )
+        sampleset = CoreDataObject(
+            id_='test_sampleset',
+            type_='sampleset',
+            attributes={}
+        )
+        sample = CoreDataObject(
+            id_='test_sample',
+            type_='sample',
+            attributes={
+                'col_date': '2020-02-02',
+                'original_collection_date': '2011-01-01 12:00:00',
+                'pre_date': '2000-12-12',
+                'public_name': 'xxTesTest1',
+                'other': 'another'
+            },
+            to_one={
+                'location': location,
+                'gal': gal,
+                'specimen': specimen,
+                'sampleset': sampleset
+            }
+        )
+        sample_project = CoreDataObject(
+            id_='test_sample_project',
+            type_='sample_project',
+            attributes={},
+            to_one={
+                'sample': sample,
+                'project': project
+            }
+        )
+        converteds = converter.convert(sample_project)
+        ret1 = next(converteds)
+        self.assertEqual('test_sample', ret1.id)
+        self.assertEqual('sample', ret1.type)
+        self.assertEqual(ret1.attributes, {
+            'project': ['test_project'],
+            'collection_country': 'Country',
+            'collection_locality': 'County | Town',
+            'latitude': 12.345678,
+            'longitude': 23.456789,
+            'elevation': 65,
+            'depth': 200,
+            'gal_abbreviation': 'TESTGAL',
+            'gal_name': 'Test Gal',
+            'specimen': {'id': 'test_specimen'},
+            'sampleset_id': 'test_sampleset',
+            'col_date': datetime.datetime(2020, 2, 2),
+            'original_collection_date': datetime.datetime(2011, 1, 1, 12),
+            'pre_date': datetime.datetime(2000, 12, 12),
+            'tolid': {'id': 'xxTesTest1'},
+            'public_name': None,
+            'other': 'another'
+        })
+
+        with self.assertRaises(StopIteration):
+            next(converteds)
