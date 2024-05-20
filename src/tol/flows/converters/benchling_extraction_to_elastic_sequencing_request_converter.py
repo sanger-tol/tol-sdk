2bfe23a035af77ae62f7acac6a61d0add0c19ed1
Andrew Varley <av10@sanger.ac.uk>
2024-05-20 11:11:57 +0100

    TOLP-7455 Benchling LRES data into Portal

---
+++ src/tol/flows/converters/benchling_extraction_to_elastic_sequencing_request_converter.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from typing import Iterable
+
+from ...core import (
+    DataObject,
+    DataObjectToDataObjectOrUpdateConverter
+)
+
+
+class BenchlingExtractionToElasticSequencingRequestConverter(
+        DataObjectToDataObjectOrUpdateConverter):
+    def convert(self, data_object: DataObject) -> Iterable[DataObject]:
+        if data_object.sts_id is not None:
+            ret = self._data_object_factory(
+                'extraction',
+                data_object.id,
+                attributes={
+                    'extraction': {'id': data_object.id},
+                    'sample': {'id': data_object.sts_id},
+                    'species': {'id': data_object.taxon_id},
+                    'specimen': {'id': data_object.specimen_id},
+                    'tolid': {'id': data_object.programme_id},
+                    'tissue_prep': {'id': data_object.eln_tissue_prep_id}
+                })
+            yield ret
