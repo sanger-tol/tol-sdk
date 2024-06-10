45133107fa4f5980dc0544558e6f5eb11f03747a
Edward Moulsdale <em29@sanger.ac.uk>
2024-06-10 15:03:45 +0100

    fixtures

---
+++ test/unit/api_client2/test_fetch.py
@@ -0,0 +1,68 @@
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from unittest.mock import create_autospec
+
+import pytest
+
+from tol.api_client2 import ApiDataSource
+from tol.api_client2.client import JsonApiClient
+from tol.api_client2.converter import DataObjectConverter, JsonApiConverter
+from tol.api_client2.filter import ApiFilter
+from tol.core import DataObject, DataSourceError
+from tol.core.relationship import RelationshipConfig
+
+
+@pytest.fixture(scope='function')
+def api_client() -> JsonApiClient:
+    return create_autospec(
+        JsonApiClient,
+        spec_set=True
+    )
+
+
+@pytest.fixture(scope='function')
+def json_api_converter() -> JsonApiConverter:
+    return create_autospec(
+        JsonApiConverter,
+        spec_set=True
+    )
+
+
+@pytest.fixture(scope='function')
+def data_object_converter() -> DataObjectConverter:
+    return create_autospec(
+        DataObjectConverter,
+        spec_set=True
+    )
+
+
+@pytest.fixture(scope='function')
+def api_filter() -> ApiFilter:
+    return create_autospec(
+        ApiFilter,
+        spec_set=True
+    )
+
+
+@pytest.fixture(scope='function')
+def api_ds(
+    api_client: JsonApiClient,
+    json_api_converter: JsonApiConverter,
+    data_object_converter: DataObjectConverter,
+    api_filter: ApiFilter
+) -> ApiDataSource:
+
+    return ApiDataSource(
+        lambda: api_client,
+        lambda: json_api_converter,
+        lambda: data_object_converter,
+        lambda: api_filter
+    )
+
+
+class TestFetch:
+    """
+    No superfluous fetches
+    """
