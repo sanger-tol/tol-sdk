7f76773762314ade34ff57970d213bce9015effc
Edward Moulsdale <em29@sanger.ac.uk>
2024-06-10 15:23:49 +0100

    closer

--- test/unit/api_client2/test_fetch.py
+++ test/unit/api_client2/test_fetch.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
+from typing import Any
 from unittest.mock import create_autospec
 
 import pytest
@@ -10,7 +11,7 @@ from tol.api_client2 import ApiDataSource
 from tol.api_client2.client import JsonApiClient
 from tol.api_client2.converter import DataObjectConverter, JsonApiConverter
 from tol.api_client2.filter import ApiFilter
-from tol.core import DataObject, DataSourceError
+from tol.core import DataObject, core_data_object
 from tol.core.relationship import RelationshipConfig
 
 
@@ -54,18 +55,21 @@ def api_ds(
     api_filter: ApiFilter
 ) -> ApiDataSource:
 
-    return ApiDataSource(
+    ds = ApiDataSource(
         lambda: api_client,
         lambda: json_api_converter,
         lambda: data_object_converter,
         lambda: api_filter
     )
+    core_data_object(ds)
+
+    return ds
 
 
 class TestNoFetch:
     """No superfluous fetches"""
 
-    def test_get_recursive_relation(
+    def test_get_by_id(
         self,
         api_ds: ApiDataSource,
         api_client: JsonApiClient
@@ -73,10 +77,46 @@ class TestNoFetch:
         """
         Endpoint returns relations +
         getting relation.attr on root
-        -> none of the following:
+        -> none of the following are called
+        on the client:
 
-        - get_recursive_relation (on
-          root type)
-        - get_by_id (on relation type)
-        - get_one (on relation type)
+        - `get_detail`
+        - `get_to_one_relation_recursive`
         """
+
+        api_client.config_relationships.return_value = {
+            'root': RelationshipConfig(
+                to_one={
+                    'relation': 'rel'
+                }
+            )
+        }
+        api_client.config_attribute_types.return_value = {
+            'root': {},
+            'rel': {'str_column': 'str'}
+        }
+        api_client.config_operations.return_value = {
+            'root': {
+                'noauth': ['detailGet', 'relational']
+            },
+            'rel': {
+                'noauth': ['detailGet', 'relational']
+            }
+        }
+        api_client.get_detail.return_value = {
+            'data': {
+                'type': 'root',
+                'id': '400',
+                'relationships': {
+                    'data': {
+                        'type': 'rel',
+                        'id': '408',
+                        'attributes': {
+                            'str_column': 'NO FETCH!'
+                        }
+                    }
+                }
+            }
+        }
+
+        api_ds.get_one('root', '400')
