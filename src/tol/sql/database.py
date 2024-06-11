81c05fea514b2326d9e558a6960b12ce58a4e1e6
Edward Moulsdale <em29@sanger.ac.uk>
2024-06-11 14:11:54 +0100

    remove comment

--- src/tol/sql/database.py
+++ src/tol/sql/database.py
@@ -13,7 +13,6 @@ from .model import Model
 from .session import SessionFactory
 from .sort import DatabaseSorter
 from ..core import DataSourceError
-#TODO don't close sessions here!!!!!!!!
 
 class Database(ABC):
     """Encapsulates basic operations on a Database"""
