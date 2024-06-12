ba96ab71a5ae96402a7beb68430b17986065611c
Edward Moulsdale <em29@sanger.ac.uk>
2024-06-12 11:05:05 +0100

    fixtures

--- test/unit/core/test_fetch.py
+++ test/unit/core/test_fetch.py
@@ -4,18 +4,44 @@
 
 from unittest.mock import create_autospec
 
-from tol.core import DataSource, core_data_object
+import pytest
+
+from tol.core import DataObject, DataSource, core_data_object
+
+
+@pytest.fixture(scope='function')
+def ds() -> DataSource:
+    return create_autospec(
+        DataSource,
+        spec_set=True
+    )
+
+
+@pytest.fixture(scope='function')
+def obj_class(
+    ds: DataSource
+) -> type[DataObject]:
+
+    return core_data_object(ds)
 
 
 class TestFetch:
     """No superfluous fetches"""
 
-    def test_empty_dict(self):
+    def test_empty_dict(
+        self,
+        ds: DataSource,
+        obj_class: type[DataObject]
+    ):
         """
         `_to_one_objects[__k] == {}` -> no fetch
         """
 
-    def test_none(self):
+    def test_none(
+        self,
+        ds: DataSource,
+        obj_class: type[DataObject]
+    ):
         """
         `_to_one_objects[__k] is None` -> no fetch
         """
