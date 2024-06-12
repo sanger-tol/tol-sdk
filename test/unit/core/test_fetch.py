beb1195a40d4860e3de192fb12addf0b095cf490
Edward Moulsdale <em29@sanger.ac.uk>
2024-06-12 11:10:34 +0100

    one passing

--- test/unit/core/test_fetch.py
+++ test/unit/core/test_fetch.py
@@ -7,19 +7,28 @@ from unittest.mock import create_autospec
 import pytest
 
 from tol.core import DataObject, DataSource, core_data_object
+from tol.core.operator import Relational
+
+
+class _DS(DataSource, Relational):
+    pass
 
 
 @pytest.fixture(scope='function')
-def ds() -> DataSource:
-    return create_autospec(
-        DataSource,
+def ds() -> _DS:
+    __ds = create_autospec(
+        _DS,
         spec_set=True
     )
+
+    __ds.supported_types = ['test_type']
+
+    return __ds
 
 
 @pytest.fixture(scope='function')
 def obj_class(
-    ds: DataSource
+    ds: _DS
 ) -> type[DataObject]:
 
     return core_data_object(ds)
@@ -33,12 +42,22 @@ class TestFetch:
 
     def test_empty_dict(
         self,
-        ds: DataSource,
+        ds: _DS,
         obj_class: type[DataObject]
     ):
         """
         `_to_one_objects[__k] == {}` -> no fetch
         """
+
+        obj = obj_class(
+            'test_type',
+            'test ID',
+            attributes={'hype': 'train'},
+            to_one={'relation': {}}
+        )
+
+        assert not obj.relation
+        ds.get_to_one_relation.assert_not_called()
 
     def test_none(
         self,
