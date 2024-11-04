1c13a661ed41f4cdd7a0072b772286a17b625543
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2024-11-04 13:55:33 +0000

    elastics updated

--- src/tol/sources/elastic.py
+++ src/tol/sources/elastic.py
@@ -194,46 +194,43 @@ def elastic():
                 'type': 'keyword',
                 'script': {
                     'source': """
-                        def statusMapping = [
-                            '41_data_asm': '4. In Assembly', '42_asm_r&d': '4. In Assembly',
-                            '43_hold_for_analysis': '4. In Assembly', '44_faculty_asm':
-                            '4. In Assembly', '45_resubmit_asm': '4. In Assembly',
-                            '46_faculty_asm': '4. In Assembly', '51_pacbio_fail':
-                            '5. In Sequencing', '52_hic_fail': '5. In Sequencing',
-                            '54_species_id': '5. In Sequencing', '55_data_query':
-                            '5. In Sequencing', '56_data_wrangle': '5. In Sequencing',
-                            '61_lr_asm_10x': '5. In Sequencing', '62_lr_asm':
-                            '5. In Sequencing', '63_lr_topup_hic': '5. In Sequencing',
-                            '64_10x_only': '5. In Sequencing', '64_10x_hic_only':
-                            '5. In Sequencing', '65_hic_only': '5. In Sequencing'
-                        ];
-                        if (doc.containsKey('grit_curation_grit_done_date_min')
-                            && doc['grit_curation_grit_done_date_min'].size() > 0) {
-                            emit("1. Submitted");
-                        } else if (doc.containsKey('grit_curation_grit_in_submission_date_min')
-                            && doc['grit_curation_grit_in_submission_date_min'].size() > 0) {
-                            emit("2. Curated");
-                        } else if (doc.containsKey('grit_curation_grit_open_date_min')
-                            && doc['grit_curation_grit_open_date_min'].size() > 0) {
-                            emit("3. In Curation");
-                        } else if (doc.containsKey('informatics_tolid_' +
-                            'informatics_status_min.keyword')
-                            && doc['informatics_tolid_informatics_' +
-                            'status_min.keyword'].size() > 0) {
-                            def status = doc['informatics_tolid_' +
-                            'informatics_status_min.keyword'].value;
-                            if (statusMapping.containsKey(status)) {
-                                emit(statusMapping[status]);
-                            }
-                        } else if (doc.containsKey('sts_sample_sts_' +
-                            'tollab_assign_date_min')
-                            && doc['sts_sample_sts_tollab_assign_' +
-                            'date_min'].size() > 0) {
+                    def statusMapping = [
+                        '11_published': '1. Submitted', '12_public': '1. Submitted',
+                        '13_submitted': '1. Submitted',
+                        '21_in_submission': '2. Curated', '22_postprocessing': '2. Curated',
+                        '23_submission_hold': '2. Curated',
+                        '31_curation': '3. In curation', '33_build': '3. In curation',
+                        '34_contam_check': '3. In curation','34_contam_check_btk':
+                        '3. In curation', '35_open': '3. In curation', 
+                        '36_awaiting_data': '3. In curation',
+                        '41_data_asm': '4. In Assembly', '42_asm_r&d': '4. In Assembly',
+                        '43_hold_for_analysis': '4. In Assembly', '44_faculty_asm':
+                        '4. In Assembly', '44_awaiting_new_data' : '4. In Assembly',
+                        '45_resubmit_asm': '4. In Assembly',
+                        '46_faculty_asm': '4. In Assembly', '51_pacbio_fail':
+                        '5. In Sequencing', '52_hic_fail': '5. In Sequencing',
+                        '54_species_id': '5. In Sequencing', '55_data_query':
+                        '5. In Sequencing','56_data_wrangle': '5. In Sequencing',
+                        '61_lr_asm_10x': '5. In Sequencing','62_lr_asm':
+                        '5. In Sequencing', '63_lr_topup_hic': '5. In Sequencing',
+                        '63_lr_topup': '5. In Sequencing','64_10x_only':
+                        '5. In Sequencing', '64_10x_hic_only': '5. In Sequencing',
+                        '65_hic_only': '5. In Sequencing', '66_rnaseq_only':
+                        '5. In Sequencing'
+                    ];
+                    if (doc['informatics_tolid_informatics_status_min.keyword'].size() > 0) {
+                        def status = doc['informatics_tolid_informatics_status_min.keyword'].value;
+                        if (statusMapping.containsKey(status)) {
+                            emit(statusMapping[status]);
+                        }
+                    } else if (doc.containsKey('sts_sample_sts_tollab_assign_date_min')) {
+                        if (doc['sts_sample_sts_tollab_assign_date_min'].size() > 0) {
                             emit("6. Sent to lab");
                         } else {
                             emit("7. Onboarding");
                         }
-                    """
+                    }
+                """
                 }
             },
         },
