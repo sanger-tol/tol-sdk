15f41ff91267a279ce5328a5f25a5869cf346caf
Logan Howat <lh31@sanger.ac.uk>
2024-11-18 15:47:37 +0000

    updating

--- src/tol/sources/elastic.py
+++ src/tol/sources/elastic.py
@@ -249,8 +249,8 @@ def elastic():
                                                 'tolid_species.sts_genome_size',
                                                 operation='/'),
             'calc_ongoing_sumbissions': RuntimeFields.math(
-                'tolqc_sequencing_request_count',
-                'tolqc_completed_sequencing_request_count',
+                'tolqc_pacbio_sequencing_request_count',
+                'tolqc_pacbio_completed_sequencing_request_count',
                 operation='-')
         },
         'sample': {
