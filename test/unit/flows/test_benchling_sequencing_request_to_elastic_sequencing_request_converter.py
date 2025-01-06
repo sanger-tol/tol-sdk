e367f271ab62815f9bd0217478e06780d653adb4
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-01-06 14:56:46 +0000

    Lint

--- test/unit/flows/test_benchling_sequencing_request_to_elastic_sequencing_request_converter.py
+++ test/unit/flows/test_benchling_sequencing_request_to_elastic_sequencing_request_converter.py
@@ -68,6 +68,7 @@ class TestBenchlingSequencingRequestToElasticSequencingRequestConverter(TestCase
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
                 'source': 'v1'
 =======
                 'source': 'v1'                 
@@ -78,6 +79,9 @@ class TestBenchlingSequencingRequestToElasticSequencingRequestConverter(TestCase
 =======
                 'source': 'v1'                 
 >>>>>>> cab324a (Modified converter to implement new sequencing platforms)
+=======
+                'source': 'v1'
+>>>>>>> db9900d (Lint)
             }
         )
 
