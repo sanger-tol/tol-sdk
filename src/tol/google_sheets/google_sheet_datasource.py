a13fe54d81174c7489108d390a3bec394eb3024e
sabrine33 <sa33@sanger.ac.uk>
2025-01-30 14:29:13 +0000

    linting

--- src/tol/google_sheets/google_sheet_datasource.py
+++ src/tol/google_sheets/google_sheet_datasource.py
@@ -74,8 +74,7 @@ class GoogleSheetDataSource(
         header_mappings = self._get_header_mappings(object_type)
         target_type = self.mappings[object_type]['columns'][header_mappings[header]]['type']
         try:
-            value  = str(value)
-            value = value.strip()
+            value = str(value).strip()
             if value == '':
                 return None
             if target_type == 'int':
