70fdd2b8aeb58589c2decfa3834da1a31c4e0851
Edward Moulsdale <em29@sanger.ac.uk>
2025-01-30 09:35:43 +0000

    fix up models

--- test/system/sql/authorization/conftest.py
+++ test/system/sql/authorization/conftest.py
@@ -9,11 +9,10 @@ from flask import Flask
 
 import pytest
 
-from sqlalchemy.orm import Session
-
 from tol.core import DataSource, OperableDataSource
-from tol.core.operator import Deleter
+from tol.core.operator import Deleter, OperatorMethod
 from tol.sql import model_base
+from tol.sql.session import SessionFactory
 from tol.sql.auth import ModelTuple, db_auth_blueprint
 from tol.sql.auth.blueprint import DbAuthBlueprint
 
@@ -80,66 +79,66 @@ def auth_mock_ds() -> OperableDataSource:
     return mock_ds
 
 
-def create_test_data(session: Session):
-    # Create roles
-    admin_role = Role(name='Admin', system_access=True)
-    user_role = Role(name='User', system_access=False)
-    session.add_all([admin_role, user_role])
-    session.flush()  # Ensures IDs are available
-    
-    # Create users
-    admin_user = User(username='admin')
-    regular_user = User(username='user')
-    session.add_all([admin_user, regular_user])
-    session.flush()
-    
-    # Create memberships
-    root_membership = Membership(name='Root Membership')
-    child_membership = Membership(name='Child Membership', parent=root_membership)
-    session.add_all([root_membership, child_membership])
-    session.flush()
-    
-    # Link users to memberships with roles
-    admin_membership = UserMembership(user=admin_user, membership=root_membership, role=admin_role)
-    user_membership = UserMembership(user=regular_user, membership=child_membership, role=user_role)
-    session.add_all([admin_membership, user_membership])
-    session.flush()
-    
-    # Create a source and data object types
-    source = Source(name='Main Source')
-    data_type = DataObjectType(name='sample', source=source)
-    session.add_all([source, data_type])
-    session.flush()
-    
-    # Create data object type attributes
-    attribute1 = DataObjectTypeAttribute(name='project_id', data_object_type=data_type, system=True)
-    attribute2 = DataObjectTypeAttribute(name='biosample_id', data_object_type=data_type, system=False)
-    session.add_all([attribute1, attribute2])
-    session.flush()
-    
-    # Link memberships to data object types
-    membership_data_object = MembershipDataObjectType(membership=root_membership, data_object_type=data_type)
-    session.add(membership_data_object)
-    session.flush()
-    
-    # Allow specific attributes for membership data object types
-    allowed_attr = MembershipDataObjectTypeAllowedAttribute(
-        membership_data_object_type=membership_data_object, data_object_type_attribute=attribute1
-    )
-    session.add(allowed_attr)
-    session.flush()
-    
-    # Create methods and needs
-    read_method = Method(identifier='READ')
-    write_method = Method(identifier='WRITE')
-    session.add_all([read_method, write_method])
-    session.flush()
-    
-    read_need = Need(data_object_type=data_type)
-    session.add(read_need)
-    session.flush()
-    
-    # Associate needs with methods and roles
-    need_method_admin = NeedMethod(need=read_need, method=read_method, role=admin_role)
-    session.add(need_method_admin)
-    session.commit()
+@pytest.fixture(autouse=True)
+def test_data(
+    auth_models: ModelTuple,
+    session_factory: SessionFactory,
+) -> None:
+
+    with session_factory() as session:
+        admin_role = auth_models.role_class(name='Admin', system_access=True)
+        user_role = auth_models.role_class(name='auth_models.user_class', system_access=False)
+        session.add_all([admin_role, user_role])
+        session.flush()
+
+        admin_user = auth_models.user_class(username='admin')
+        regular_user = auth_models.user_class(username='user')
+        session.add_all([admin_user, regular_user])
+        session.flush()
+
+        root_membership = auth_models.membership(name='Root Membership')
+        child_membership = auth_models.membership(name='Child Membership', parent=root_membership)
+        session.add_all([root_membership, child_membership])
+        session.flush()
+
+        admin_membership = auth_models.user_membership(user=admin_user, membership=root_membership, role=admin_role)
+        user_membership = auth_models.user_membership(user=regular_user, membership=child_membership, role=user_role)
+        session.add_all([admin_membership, user_membership])
+        session.flush()
+
+        source = auth_models.source(name='Main Source')
+        data_type = auth_models.data_object_type(name='sample', source=source)
+        session.add_all([source, data_type])
+        session.flush()
+
+        attribute1 = auth_models.data_object_type_attribute(name='project_id', data_object_type=data_type, system=True)
+        attribute2 = auth_models.data_object_type_attribute(name='biosample_id', data_object_type=data_type, system=False)
+        session.add_all([attribute1, attribute2])
+        session.flush()
+
+        membership_data_object = auth_models.membership_data_object_type(membership=root_membership, data_object_type=data_type)
+        session.add(membership_data_object)
+        session.flush()
+
+        allowed_attr = auth_models.membership_data_object_type_allowed_attribute(
+            membership_data_object_type=membership_data_object, data_object_type_attribute=attribute1
+        )
+        session.add(allowed_attr)
+        session.flush()
+
+        detail_read_method = auth_models.method(
+            identifier=OperatorMethod.DETAIL
+        )
+        detail_delete_method = auth_models.method(
+            identifier=OperatorMethod.DELETE
+        )
+        session.add_all([detail_read_method, detail_delete_method])
+        session.flush()
+        
+        read_need = auth_models.need(data_object_type=data_type)
+        session.add(read_need)
+        session.flush()
+
+        need_method_admin = auth_models.need_method(need=read_need, method=detail_read_method, role=admin_role)
+        session.add(need_method_admin)
+        session.commit()
