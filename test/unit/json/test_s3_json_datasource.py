84a27d8a4bf4bfd654a2bb8b181fcb126bda7d7b
Logan Howat <lh31@sanger.ac.uk>
2025-02-10 11:05:30 +0000

    Starting tests for S3JsonDataSource

---
+++ test/unit/json/test_s3_json_datasource.py
@@ -0,0 +1,16 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from datetime import datetime
+from unittest import (
+    TestCase,
+    mock
+)
+
+from tol.core import (
+    core_data_object
+)
+from tol.json import (
+    S3JsonDataSource
+)
