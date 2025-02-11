314dcbece1efe1b05400b802ed1aecbdaa8814e1
Edward Moulsdale <em29@sanger.ac.uk>
2025-02-11 11:30:54 +0000

    fix small one

--- src/tol/core/core_converter.py
+++ src/tol/core/core_converter.py
@@ -100,9 +100,10 @@ class ChainedConverter(Converter, Generic[In, Out]):
     """
 
     def __init__(
+        self,
         *converters: Converter
     ):
-        pass
+        self.__converters = converters
 
     def convert(self, input_: In) -> Out:
         pass
