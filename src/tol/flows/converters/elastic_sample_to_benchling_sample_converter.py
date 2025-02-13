dcdf34f71a685944151e58c649553d732d73772c
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-02-13 13:05:22 +0000

    lint + package udpate

--- src/tol/flows/converters/elastic_sample_to_benchling_sample_converter.py
+++ src/tol/flows/converters/elastic_sample_to_benchling_sample_converter.py
@@ -23,7 +23,8 @@ class ElasticSampleToBenchlingSampleConverter(
             attributes = {
                 k[10:]: v  # Remove 'benchling_' prefix
                 for k, v in data_object.attributes.items()
-                if k.startswith('benchling_') and k != 'benchling_eln_tissue_id' # eln_tissue_id is a unique marker and should be treated separately
+                # eln_tissue_id is a unique marker and should be treated separately
+                if k.startswith('benchling_') and k != 'benchling_eln_tissue_id'
             }
 
             # Add the relationship IDs to attributes
