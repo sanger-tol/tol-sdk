c88efd14e60a84c9635082e27e9cd4df57ce6380
Logan Howat <lh31@sanger.ac.uk>
2025-02-19 11:41:37 +0000

    Adding gap datasource, getting access denied error

---
+++ src/tol/gap/gap_datasource.py
@@ -0,0 +1,77 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+
+import json
+from io import BytesIO
+from typing import Dict, Iterable, Optional
+
+from functools import cache
+
+from minio import Minio
+
+
+from ..json import S3JsonDataSource
+
+from ..core import (
+    DataSourceError,
+    DataObject,
+)
+
+from ..core.operator import Relational
+
+from ..core.relationship import RelationshipConfig
+
+
+
+
+class GapDataSource(
+    S3JsonDataSource,
+    Relational
+):
+    def __init__(
+        self,
+        config: Dict,
+        secure: bool = True
+    ) -> None:
+        super().__init__(
+            config=config,
+            secure=secure
+        )
+        
+    @property
+    @cache
+    def relationship_config(self) -> dict[str, RelationshipConfig]:
+        rc_pipeline = RelationshipConfig()
+        rc_pipeline.to_many = {
+            'pipeline': 'pipeline'
+        }
+        return {
+            'pipeline': rc_pipeline
+        }
+
+    def get_to_one_relation(
+        self,
+        source: DataObject,
+        relationship_name: str
+    ) -> Optional[DataObject]:
+        return None
+
+    def get_to_many_relations(
+        self,
+        assmbly: DataObject
+    ) -> Iterable[DataObject]:
+        temp_config = {
+            'uri': f's3://gap/{assmbly.id}/data/analysis.json',
+            'type': 'pipeline',
+            'id_attribute': 'id',
+            'mappings': {
+            },
+            's3_host': 'cog.sanger.ac.uk',
+            's3_access_key': self.config['s3_access_key'],
+            's3_secret_key': self.config['s3_secret_key']
+        }
+        temp_ds = S3JsonDataSource(config=temp_config, secure=True)
+        
+        return temp_ds.get_list('pipeline')
