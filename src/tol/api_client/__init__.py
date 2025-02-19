b21ef1494ec571621e4e3af618fd987eabaef001
Edward Moulsdale <em29@sanger.ac.uk>
2025-02-19 08:44:31 +0000

    remove api_client

--- src/tol/api_client/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2023 Genome Research Ltd.
-#
-# SPDX-License-Identifier: MIT
-
-from .api_datasource import ApiDataSource  # noqa
-from .api_object import ApiObject  # noqa
