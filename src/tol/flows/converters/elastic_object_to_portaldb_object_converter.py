1a6792d19ca933a2d447b5fc1857c033d6afa928
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-02-24 14:32:35 +0000

    new elastic to portal object converter

--- src/tol/flows/converters/elastic_object_to_portaldb_object_converter.py
+++ src/tol/flows/converters/elastic_object_to_portaldb_object_converter.py
@@ -16,16 +16,18 @@ class ElasticObjectToPortaldbObjectConverter(
         DataObjectToDataObjectOrUpdateConverter):
 
     def __init__(self, data_object_factory, fields: dict = {},
-                 destination_object_type: str = 'tolid_event'):
+                 destination_object_type: str = 'tolid_event',
+                 id_field: str = 'id'):
         super().__init__(data_object_factory)
         self.__fields = fields
         self.__destination_object_type = destination_object_type
+        self.__id_field = id_field
 
     def convert(self, data_object: DataObject) -> Iterable[DataObject]:
         if data_object is not None:
             ret = self._data_object_factory(
                 self.__destination_object_type,
-                data_object.id,
+                data_object.get_field_by_name(self.__id_field),
                 attributes=self.__fields
             )
             yield ret
