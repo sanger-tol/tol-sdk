268c855653443864290b88191e6d43d3d778f6e1
Logan Howat <lh31@sanger.ac.uk>
2025-03-03 13:13:42 +0000

    Making changes to the converter class to account for the details

--- src/tol/flows/converters/gap_assembly_to_elastic_assembly_converter.py
+++ src/tol/flows/converters/gap_assembly_to_elastic_assembly_converter.py
@@ -16,6 +16,17 @@ class GapAssemblyToElasticAssemblyConverter(
         super().__init__(data_object_factory)
         self.gap_ds = gap_ds
 
+    def convert_details(self, assembly: DataObject) -> Iterable[DataObject]:
+        detail_atts = {}
+        for d in assembly.assembly_details:
+            case_corrected = d.id.replace(' ', '_').lower()
+            detail_atts = {
+                **detail_atts,
+                case_corrected: d.info,
+            }
+
+        return detail_atts
+
     def convert(self, assembly: DataObject) -> Iterable[DataObject]:
 
         pipeline_atts = {}
@@ -28,6 +39,8 @@ class GapAssemblyToElasticAssemblyConverter(
                 f'{prefix}_lustre_path_analysis': p.lustre_path_analysis,
                 **pipeline_atts,
             }
+
+        details = self.convert_details(assembly)
 
         to_one_relations = {
             'species': self._data_object_factory(
@@ -42,11 +55,20 @@ class GapAssemblyToElasticAssemblyConverter(
             if k not in ['taxon_id', 'species', 'phylum_id', 'phylum']
         }
 
+        detail_attributes = {
+            k: v
+            for k, v in details.items()
+            if k in ['organelles', 'total_ingapped_length', 'total_sequence_length',
+                     'number_of_chromosomes', 'scaffold_n50', 'number_of_scaffolds',
+                     'contig_n50', 'number_of_contigs', 'gc_percent', 'genome_coverage']
+        }
+
         ret = self._data_object_factory(
             'assembly',
             assembly.id,
             attributes={
                 **pipeline_atts,
+                **detail_attributes,
                 **attributes
             },
             to_one=to_one_relations
