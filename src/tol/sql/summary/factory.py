4b5f3a887475de996ade775e904501edbf39102c
Andrew Varley <av10@sanger.ac.uk>
2025-03-11 13:56:45 +0000

    Added summary tables

---
+++ src/tol/sql/summary/factory.py
@@ -0,0 +1,94 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from collections.abc import Iterable as IterableABC
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Any, Iterator
+
+from sqlalchemy import (
+    CheckConstraint,
+    ForeignKey,
+    UniqueConstraint
+)
+from sqlalchemy.dialects.postgresql import JSONB
+from sqlalchemy.orm import (
+    Mapped,
+    declared_attr,
+    mapped_column,
+    relationship
+)
+
+from ..model import Model
+
+
+@dataclass(frozen=True, kw_only=True)
+class SummaryModels(IterableABC[type[Model]]):
+    """
+    Contains the needed models for summarys.
+
+    """
+
+    summary: type[Model]
+
+    def __iter__(self) -> Iterator[type[Model]]:
+        """
+        Returns in order they should be deleted
+        """
+
+        return iter(
+            [
+                self.summary
+            ]
+        )
+
+
+def create_summary_models(
+    base_model_class: type[Model]
+) -> SummaryModels:
+    """
+    Creates all needed models (and joining tables) for
+    summaries.
+
+    Returns a `SummaryModels` instance that functions like an
+    `Iterable`.
+    """
+
+    class Summary(base_model_class):
+        __tablename__ = 'summary'
+
+        id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)  # noqa A003
+
+        source_object_type: Mapped[str] = mapped_column(nullable=False)
+        destination_object_type: Mapped[str] = mapped_column(nullable=True)
+
+        object_filters: Mapped[dict] = mapped_column(
+            JSONB,
+            nullable=False,
+            default={}
+        )
+
+        group_by: Mapped[dict] = mapped_column(
+            JSONB,
+            nullable=False,
+            default=[]
+        )
+
+        stats_fields: Mapped[dict] = mapped_column(
+            JSONB,
+            nullable=False,
+            default=[]
+        )
+
+        stats: Mapped[dict] = mapped_column(
+            JSONB,
+            nullable=False,
+            default=[]
+        )
+
+    return SummaryModels(
+        summary=Summary,
+    )
