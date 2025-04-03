55863f27e8ce079213ee55774ee09f8c58346c84
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-04-03 13:00:13 +0100

    TOLP-8616: Fix for elastic execution error

--- src/tol/sources/elastic.py
+++ src/tol/sources/elastic.py
@@ -678,9 +678,10 @@ def elastic():
                         emit(0.0);
                     } else if (doc.containsKey('benchling_remaining_weight') &&
                         doc['benchling_remaining_weight'].size() > 0) {
-                        emit(doc['benchling_remaining_weight'].value);
-                    } else {
-                        emit(null);
+                        def value = doc['benchling_remaining_weight'].value;
+                        if (!Double.isNaN(value)) {
+                            emit(value);
+                        }
                     }
                 """
                 }
@@ -724,9 +725,10 @@ def elastic():
                         emit(0.0);
                     } else if (doc.containsKey('mlwh_volume_remaining') &&
                         doc['mlwh_volume_remaining'].size() > 0) {
-                        emit(doc['mlwh_volume_remaining'].value);
-                    } else {
-                        emit(null);
+                        def value = doc['mlwh_volume_remaining'].value;
+                        if (!Double.isNaN(value)) {
+                            emit(value);
+                        }
                     }
                 """
                 }
@@ -747,9 +749,10 @@ def elastic():
                         emit(0.0);
                     } else if (doc.containsKey('benchling_volume_ul') &&
                         doc['benchling_volume_ul'].size() > 0) {
-                        emit(doc['benchling_volume_ul'].value);
-                    } else {
-                        emit(null);
+                        def value = doc['benchling_volume_ul'].value;
+                        if (!Double.isNaN(value)) {
+                            emit(value);
+                        }
                     }
                 """
                 }
@@ -765,9 +768,10 @@ def elastic():
                         emit(0.0);
                     } else if (doc.containsKey('benchling_weight_mg') &&
                         doc['benchling_weight_mg'].size() > 0) {
-                        emit(doc['benchling_weight_mg'].value);
-                    } else {
-                        emit(null);
+                        def value = doc['benchling_weight_mg'].value;
+                        if (!Double.isNaN(value)) {
+                            emit(value);
+                        }
                     }
                 """
                 }
