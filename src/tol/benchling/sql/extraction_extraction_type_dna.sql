3929317c32db401c1f3eaa86ee2dc3a43aa16db0
ene goktan <eg21@sanger.ac.uk>
2025-04-10 15:42:09 +0100

    fix errors from last commit

--- src/tol/benchling/sql/extraction_extraction_type_dna.sql
+++ src/tol/benchling/sql/extraction_extraction_type_dna.sql
@@ -127,7 +127,6 @@ SELECT DISTINCT
         WHEN con.archive_purpose$ IN ('Retired', 'Expended') THEN 0 -- Retired or expended DNA extractions have a weight of 0
         ELSE con.volume_si * 1000000
     END AS volume_ul,
-    con.volume_si * 1000000 AS volume_ul,
     loc.name AS location,
     box.barcode AS rack,
     dna.bt_id AS bnt_id,
