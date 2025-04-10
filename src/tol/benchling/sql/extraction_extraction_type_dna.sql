0a48edb21fc2e810708b7a915341007260257625
ene goktan <eg21@sanger.ac.uk>
2025-04-10 15:42:09 +0100

    fix errors from last commit

--- src/tol/benchling/sql/extraction_extraction_type_dna.sql
+++ src/tol/benchling/sql/extraction_extraction_type_dna.sql
@@ -127,7 +127,6 @@ SELECT DISTINCT
         WHEN con.archive_purpose$ IN ('Retired', 'Expended') THEN 0 -- Retired or expended DNA extractions have a weight of 0
         ELSE con.volume_si * 1000000
     END AS volume_ul,
-    con.volume_si * 1000000 AS volume_ul,
     loc.name AS location,
     box.barcode AS rack,
     dna.bt_id AS bnt_id,
@@ -178,7 +177,7 @@ WHERE tube.type IS NULL -- Excluding vouchers
     AND con.volume_si * 1000000 != 10
     AND proj.name = 'ToL Core Lab'
     AND  (f.name IN ('Routine Throughput', 'DNA', 'Core Lab Entities', 'Benchling MS Project Move') OR f.name IS NULL)
-    AND (dna.archive_purpose$ != ('Made in error') OR con.archive_purpose$ IS NULL)
+    AND (dna.archive_purpose$ != ('Made in error') OR dna.archive_purpose$ IS NULL)
     AND (con.archive_purpose$ != ('Made in error') OR con.archive_purpose$ IS NULL)
     AND con.barcode NOT LIKE 'CON%'
 ORDER BY completion_date DESC
