bc536dc2e817c30a725c309f2c9693f4091d8165
Ene Goktan <eg21@sanger.ac.uk>
2025-04-23 11:43:29 +0100

    TOLIT-3176: Further query modification for TUM

--- src/tol/benchling/sql/tissue_prep.sql
+++ src/tol/benchling/sql/tissue_prep.sql
@@ -31,13 +31,14 @@ Columns in output table:
 7) eln_tissue_prep_name: [character]
 8) sampleprep_date: [Date] date of sample preparation.
 9) tissue_prep_fluidx_id: [character] fluidx id of the tissue prep container
-10) weight_mg: [double] weight in mg of the tissue prep.
-11) downstream_protocol: [text] downstream process the tissue prep was prepped for.
-12) disruption_method: [character] method used to disrupt the tissue.
-13) tissue_prep_type: [character] tissue type for HiC SciOps submissions.
-14) sciops_protocol_required: [character] protocol required for HiC SciOps submissions.
-15) sts_labwork_category: [character] Reason for exporting tissue. Aid to interpret downstream protocol for legacy samples.
-16) tissue_prep_bnt_id: [character] Batches and Tracking legacy id.
+10) tube_location: [character] location of the tissue prep container.
+11) weight_mg: [double] weight in mg of the tissue prep.
+12) downstream_protocol: [text] downstream process the tissue prep was prepped for.
+13) disruption_method: [character] method used to disrupt the tissue.
+14) tissue_prep_type: [character] tissue type for HiC SciOps submissions.
+15) sciops_protocol_required: [character] protocol required for HiC SciOps submissions.
+16) sts_labwork_category: [character] Reason for exporting tissue. Aid to interpret downstream protocol for legacy samples.
+17) tissue_prep_bnt_id: [character] Batches and Tracking legacy id.
 */
 
 WITH tissue_preps AS (
@@ -52,13 +53,17 @@ WITH tissue_preps AS (
 		tp.name$ AS eln_tissue_prep_name,
 		DATE(tp.created_at$) AS sampleprep_date,
 		con.barcode AS tissue_prep_fluidx_id,
+		loc.name AS tube_location,
 		CASE
 			WHEN con.archive_purpose$ IN ('Retired', 'Expended') THEN 0 -- Retired or expended tissue preps have a weight of 0
 			WHEN loc.name = 'SciOps ToL Lab' THEN 0 -- Tissue preps sent to LRES have a weight of 0
 			ELSE con.volume_si * 1000000
 		END AS weight_mg,
 		tube.tissue_prep_downstream_process AS downstream_protocol,
-		tube.tissue_prep_disruption_method AS disruption_method,
+		CASE
+			WHEN tube.tissue_prep_disruption_method = 'None' THEN NULL
+			ELSE tube.tissue_prep_disruption_method
+		END AS disruption_method,
 		tube.tissue_prep_type,
 		tube.sciops_protocol_required,
 		t.lab_work_category AS labwork_category_sts,
@@ -96,13 +101,14 @@ legacy_tissue_preps AS (
 		tp.name$ AS eln_tissue_prep_name,
 		DATE(tp.created_at$) AS sampleprep_date,
 		con.barcode AS tissue_prep_fluidx_id,
+		loc.name AS tube_location,
 		CASE
 			WHEN con.archive_purpose$ IN ('Retired', 'Expended') THEN 0 -- Retired or expended tissue preps have a weight of 0
 			WHEN loc.name = 'SciOps ToL Lab' THEN 0 -- Tissue preps sent to LRES have a weight of 0
 			ELSE con.volume_si * 1000000 
 		END AS weight_mg,
 		tpr.downstream_protocol,
-		''::varchar AS disruption_method,
+		NULL::varchar AS disruption_method,
 		tube.tissue_prep_type,
 		tube.sciops_protocol_required,
 		t.lab_work_category AS labwork_category_sts,
