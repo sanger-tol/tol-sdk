3fc2e0c4f957c8b3211c76ef40a32dc230f7c492
Edward Moulsdale <em29@sanger.ac.uk>
2025-04-30 10:35:12 +0100

    red test

--- test/unit/api_base/test_view_requested_fields.py
+++ test/unit/api_base/test_view_requested_fields.py
@@ -15,19 +15,49 @@ class TestViewRequestedFields:
     """
 
     def test_no_hops(self) -> None:
-        pass
+        obj = self.__mock_object(
+            'a',
+            'AAAA',
+            {
+                'i_can_do': 'anything',
+                'ignore_me': True,
+            },
+            {},
+        )
+
+        view = DefaultView(
+            requested_fields=[
+                'i_can_do'
+            ]
+        )
+
+        dumped = view.dump(obj)
+
+        assert 'i_can_do' in dumped['data']['attributes']
+        assert 'ignore_me' not in dumped['data']['attributes']
 
     def test_two_hops(self) -> None:
         pass
 
     def __mock_object(
         self,
-        sets: dict[str, Any]
+        type_: str,
+        id_: str,
+        attributes: dict[str, Any],
+        ones: dict[str, DataObject],
     ) -> DataObject:
 
         obj: DataObject = create_autospec(DataObject)
 
-        for k, v in sets.items():
+        obj.type = type_
+        obj.id = id_
+
+        obj.attributes = attributes
+        for k, v in attributes.items():
+            setattr(obj, k, v)
+
+        obj._to_one_objects = ones
+        for k, v in ones.items():
             setattr(obj, k, v)
 
         return obj
