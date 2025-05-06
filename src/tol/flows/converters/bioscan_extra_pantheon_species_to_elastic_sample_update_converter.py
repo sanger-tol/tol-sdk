84bddb43c11761414597fb7ff6e862529ae7b5e8
Andrew Varley <av10@sanger.ac.uk>
2025-05-06 15:45:25 +0100

    TOLP-8562 Pantheon and UKSI tweaks

--- src/tol/flows/converters/bioscan_extra_pantheon_species_to_elastic_sample_update_converter.py
+++ src/tol/flows/converters/bioscan_extra_pantheon_species_to_elastic_sample_update_converter.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
 #
 # SPDX-License-Identifier: MIT
 
@@ -11,12 +11,11 @@ from ...core import (
 from ...core.operator.updater import DataObjectUpdate
 
 
-class BioscanQcUksiEntryToElasticSpeciesUpdateConverter(
+class BioscanExtraPantheonSpeciesToElasticSampleUpdateConverter(
         DataObjectToDataObjectOrUpdateConverter):
-
     def convert(self, data_object: DataObject) -> Iterable[DataObjectUpdate]:
-        if data_object is not None and data_object.id is not None:
-
+        if data_object is not None:
             yield (None, {
-                'goat_scientific_name': data_object.id,
-                'uksi_name_status': data_object.uksi_name_status})
+                'bold_species': data_object.id
+            } | data_object.attributes
+            )
--- src/tol/flows/converters/bioscan_qc_uksi_entry_to_elastic_sample_update_converter.py
+++ src/tol/flows/converters/bioscan_qc_uksi_entry_to_elastic_sample_update_converter.py
@@ -1,4 +1,4 @@
-# SPDX-FileCopyrightText: 2024 Genome Research Ltd.
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
 #
 # SPDX-License-Identifier: MIT
 
@@ -11,11 +11,12 @@ from ...core import (
 from ...core.operator.updater import DataObjectUpdate
 
 
-class BioscanExtraSpeciesToElasticSampleUpdateConverter(
+class BioscanQcUksiEntryToElasticSampleUpdateConverter(
         DataObjectToDataObjectOrUpdateConverter):
+
     def convert(self, data_object: DataObject) -> Iterable[DataObjectUpdate]:
-        if data_object is not None:
+        if data_object is not None and data_object.id is not None:
+
             yield (None, {
-                'bold_species': data_object.id
-            } | data_object.attributes
-            )
+                'bold_species': data_object.id,
+                'uksi_name_status': data_object.uksi_name_status})
