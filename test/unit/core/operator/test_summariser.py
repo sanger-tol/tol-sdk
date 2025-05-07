bcbf7ce58380c442e488b212c1a6456e0451b9db
Ahmad Zuheir bin Zaidon <zz6@sanger.ac.uk>
2025-05-07 11:33:17 +0100

    test for get_objects_to_summarise

--- test/unit/core/operator/test_summariser.py
+++ test/unit/core/operator/test_summariser.py
@@ -104,42 +104,14 @@ def mock_summariser() -> Summariser:
 
     mock_sum.get_by_ids.side_effect = __get_by_ids
 
-    def __get_objects_to_summarise(
-        summary_objects: Iterable[DataObject],
-        source_object_type: str,
-        source_object_ids: Iterable[str],
-    ):
-
-        filtered_objs = [
-            s for s in summary_objects
-            if s.source_object_type == source_object_type
-        ]
-
-        if not filtered_objs:
-            return [], {}
-
-        summary_obj = filtered_objs[0]
-
-        # Create the expected filter based on object type and IDs
-        rel_field = 'back_a.id' if source_object_type == 'first' else 'back_i.id'
-        id_list = [f'rel_{c}' for c in source_object_ids]
-
-        ext_and = {
-            rel_field: {
-                'in_list': {
-                    'value': id_list
-                }
-            }
-        }
-
-        return [(summary_obj, ext_and)], {}
-
-    mock_sum.get_objects_to_summarise.side_effect = __get_objects_to_summarise
-
     # internal methods that still need to be concrete
     mock_sum._filter_by_source_type.side_effect = (
         lambda *args: Summariser._filter_by_source_type(mock_sum, *args)
     )
+
+    # get_objects_to_summarise method is tested elsewhere
+    # but we need to mock it here
+    mock_sum.get_objects_to_summarise.side_effect = Summariser.get_objects_to_summarise.__get__(mock_sum)
 
     return mock_sum
 
@@ -215,16 +187,6 @@ class TestSummariser:
     ) -> None:
         """Only one relationship"""
 
-        mock_summariser.get_objects_to_summarise.return_value = ([
-            (summary_objs[1], {
-                'back_i.id': {
-                    'in_list': {
-                        'value': ['rel_a', 'rel_b', 'rel_c']
-                    }
-                }
-            })
-        ], {})
-
         Summariser.resummarise_by_ids(
             mock_summariser,
             summary_objs,
@@ -249,16 +211,6 @@ class TestSummariser:
         summary_objs: Iterable[DataObject],
     ) -> None:
         """Many relationships pointing to the target type"""
-
-        mock_summariser.get_objects_to_summarise.return_value = ([
-            (summary_objs[1], {
-                'back_i.id': {
-                    'in_list': {
-                        'value': ['rel_a', 'rel_b', 'rel_c']
-                    }
-                }
-            })
-        ], {})
 
         Summariser.resummarise_by_ids(
             mock_summariser,
