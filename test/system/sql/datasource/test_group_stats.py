4134185fdd7b2e52e582d5bdc107edf86093bf48
Edward Moulsdale <em29@sanger.ac.uk>
2025-05-13 09:35:04 +0100

    test skel

---
+++ test/system/sql/datasource/test_group_stats.py
@@ -0,0 +1,27 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+
+class TestGroupStats:
+
+    def test_min(self, session_factory, models_list) -> None:
+        pass
+
+    def test_max(self, session_factory, models_list) -> None:
+        pass
+
+    def test_sum(self, session_factory, models_list) -> None:
+        pass
+
+    def test_unique(self, session_factory, models_list) -> None:
+        pass
+
+    def test_cardinality(self, session_factory, models_list) -> None:
+        pass
+
+    def test_union(self, session_factory, models_list) -> None:
+        pass
+
+    def test_count(self, session_factory, models_list) -> None:
+        pass
