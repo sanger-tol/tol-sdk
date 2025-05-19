b954e08531615785f26ed9c9cdc1649412ecda30
Edward Moulsdale <em29@sanger.ac.uk>
2025-05-19 11:35:58 +0100

    green test

--- src/tol/core/yaml/converter.py
+++ src/tol/core/yaml/converter.py
@@ -4,11 +4,11 @@
 
 from __future__ import annotations
 
-from typing import Iterable
+from typing import Any, Iterable
 
 from yaml import safe_load
 
-from .model import YamlConfig
+from .model import AttributeConfig, YamlConfig
 from ..data_object import DataObject
 from ..data_object_converter import (
     DataObjectToDataObjectOrUpdateConverter,
@@ -28,6 +28,7 @@ class YamlConverter(DataObjectToDataObjectOrUpdateConverter):
         yaml_path: str,
         *,
         pydantic_class: type[YamlConfig] = YamlConfig,
+        destination_object_type: str | None = None,
     ) -> None:
 
         super().__init__(data_object_factory)
@@ -36,13 +37,87 @@ class YamlConverter(DataObjectToDataObjectOrUpdateConverter):
             yaml_path,
             pydantic_class,
         )
+        self.__dest_type = destination_object_type
 
     def convert(
         self,
         input_: DataObject
     ) -> Iterable[DataObject]:
 
-        pass
+        attributes = self.__convert_attributes(
+            input_,
+        )
+        destination_type = self.__get_destination_type(
+            input_,
+        )
+
+        yield self._data_object_factory(
+            destination_type,
+            id_=input_.id,
+            attributes=attributes,
+        )
+
+    def __get_destination_type(
+        self,
+        input_: DataObject,
+    ) -> str:
+
+        if self.__dest_type:
+            return self.__dest_type
+        else:
+            return input_.type
+
+    def __convert_attributes(
+        self,
+        input_: DataObject
+    ) -> dict[str, Any]:
+
+        attr_pairs = (
+            self.__convert_attribute(
+                input_,
+                attribute_config,
+            )
+            for attribute_config
+            in self.__config.attributes
+        )
+
+        return dict(attr_pairs)
+
+    def __convert_attribute(
+        self,
+        input_: DataObject,
+        attribute_config: AttributeConfig,
+    ) -> tuple[str, Any]:
+
+        d = attribute_config.destination
+
+        if len(d.import_values) == 1:
+            value = getattr(input_, d.import_values[0])
+        else:
+            value = self.__convert_compound_value(
+                input_,
+                attribute_config,
+            )
+
+        return d.key, value
+
+    def __convert_compound_value(
+        self,
+        input_: DataObject,
+        attribute_config: AttributeConfig,
+    ) -> str:
+
+        # TODO need to account for "magic" all/integers
+
+        seperator = attribute_config.destination.separator
+
+        values = (
+            getattr(input_, k)
+            for k
+            in attribute_config.destination.import_values
+        )
+
+        return seperator.join(values)
 
     def __load_yaml(
         self,
