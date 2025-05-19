8bdce73523c7ac388520887e60a63d485ca74e0e
Edward Moulsdale <em29@sanger.ac.uk>
2025-05-19 10:27:53 +0100

    first code

---
+++ src/tol/core/yaml/converter.py
@@ -0,0 +1,28 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from __future__ import annotations
+
+from .model import YamlValidatorConfig
+from ..datasource import DataObjectFactory
+from ..data_object_converter import (
+    DataObjectToDataObjectOrUpdateConverter,
+)
+
+
+class YamlConverter(DataObjectToDataObjectOrUpdateConverter):
+    """
+    Converts `DataObject` instances dynamically, according to
+    a YAML specification.
+    """
+
+    def __init__(
+        self,
+        data_object_factory: DataObjectFactory,
+        yaml_path: str,
+        *,
+        config: type[YamlValidatorConfig] = YamlValidatorConfig,
+    ) -> None:
+
+        super().__init__(data_object_factory)
---
+++ src/tol/core/yaml/model.py
@@ -0,0 +1,37 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from pydantic import BaseModel, Field
+
+
+class FileConfig(BaseModel):
+    format: str
+    header: bool
+    name: str
+    submitter: str
+
+    comment: str = "#"
+    default_reference_header: str | None = Field(
+        None,
+        alias="default reference header",
+    )
+
+
+class DestinationConfig(BaseModel):
+    key: str
+    import_values: list[str]
+
+    separator: str = "|"
+    ignore: list[str] = []
+
+
+class AttributeConfig(BaseModel):
+    column_name: str
+    reference: str
+    destination: DestinationConfig
+
+
+class YamlValidatorConfig(BaseModel):
+    file: FileConfig
+    attributes: list[AttributeConfig]
