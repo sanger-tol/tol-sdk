e6741c645b4cf31c1faedeefc43c0871730419ae
Edward Moulsdale <em29@sanger.ac.uk>
2025-05-19 10:39:15 +0100

    test

---
+++ test/unit/core/yaml/test_yaml_loading.py
@@ -0,0 +1,44 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from unittest.mock import create_autospec
+
+from pydantic import ValidationError
+
+import pytest
+
+from tol.core import DataObjectFactory
+from tol.core.yaml import YamlConverter
+
+
+@pytest.fixture
+def data_object_factory() -> DataObjectFactory:
+    return create_autospec(
+        DataObjectFactory,
+        spec_set=True,
+    )
+
+
+class TestYamlConverterLoading:
+
+    def test_good(
+        self,
+        data_object_factory: DataObjectFactory,
+    ) -> None:
+
+        YamlConverter(
+            data_object_factory,
+            'good.yaml',
+        )
+
+    def test_bad(
+        self,
+        data_object_factory: DataObjectFactory,
+    ) -> None:
+
+        with pytest.raises(ValidationError):
+            YamlConverter(
+                data_object_factory,
+                'bad.yaml',
+            )
