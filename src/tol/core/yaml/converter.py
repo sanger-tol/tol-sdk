e75c9e005982f276e8f026a530275c2a12c7e0fb
Edward Moulsdale <em29@sanger.ac.uk>
2025-05-19 10:43:02 +0100

    add

--- src/tol/core/yaml/converter.py
+++ src/tol/core/yaml/converter.py
@@ -25,14 +25,27 @@ class YamlConverter(DataObjectToDataObjectOrUpdateConverter):
         data_object_factory: DataObjectFactory,
         yaml_path: str,
         *,
-        config: type[YamlConfig] = YamlConfig,
+        pydantic_class: type[YamlConfig] = YamlConfig,
     ) -> None:
 
         super().__init__(data_object_factory)
+
+        self.__config = self.__load_yaml(
+            yaml_path,
+            pydantic_class,
+        )
 
     def convert(
         self,
         input_: DataObject
     ) -> Iterable[DataObject]:
+
+        pass
+
+    def __load_yaml(
+        self,
+        yaml_path: str,
+        pydantic_class: type[YamlConfig],
+    ) -> YamlConfig:
 
         pass
