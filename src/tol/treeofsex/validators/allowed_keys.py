950b1026f9188f949f128073dcee2badb3f32e51
Edward Moulsdale <em29@sanger.ac.uk>
2025-06-02 11:25:43 +0100

    all green tests

--- src/tol/treeofsex/validators/allowed_keys.py
+++ src/tol/treeofsex/validators/allowed_keys.py
@@ -18,13 +18,42 @@ class AllowedKeysValidator(Validator):
         allowed_keys: list[str],
         *,
         is_error: bool = True,
+        detail: str = 'Key is not allowed'
     ) -> None:
 
         super().__init__()
+
+        self.__keys = allowed_keys
+        self.__is_error = is_error
+        self.__detail = detail
 
     def _validate_data_object(
         self,
         obj: DataObject
     ) -> DataObject:
 
-        pass
+        for key in obj.attributes:
+            if key not in self.__keys:
+                self.__add_result(
+                    obj,
+                    key,
+                )
+
+    def __add_result(
+        self,
+        obj: DataObject,
+        key: str,
+    ) -> None:
+
+        if self.__is_error:
+            self.add_error(
+                object_id=obj.id,
+                detail=self.__detail,
+                field=key,
+            )
+        else:
+            self.add_warning(
+                object_id=obj.id,
+                detail=self.__detail,
+                field=key,
+            )
