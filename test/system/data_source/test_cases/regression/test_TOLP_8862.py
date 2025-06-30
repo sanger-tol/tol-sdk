21584a6a0a7acf80fa0b7e301256669c4f357f73
Edward Moulsdale <em29@sanger.ac.uk>
2025-06-30 08:16:35 +0100

    red test

---
+++ test/system/data_source/test_cases/regression/test_TOLP_8862.py
@@ -0,0 +1,62 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+
+from tol.core import DataSourceFilter, OperableDataSource
+
+from ...dec import against
+from ...fixtures import api_sql, sql
+
+
+class TestTOLP_8862:
+
+    @against(api_sql, sql)
+    def test_filter_by_int_id(
+        self,
+        data_source: OperableDataSource,
+        ds_sleep
+    ) -> None:
+
+        objs = [
+            data_source.data_object_factory(
+                'inc',
+                id_=str(i),
+            )
+            for i in range(3)
+        ]
+        data_source.upsert('inc', objs)
+
+        f = DataSourceFilter(
+            and_={
+                'id': {'gt': {'value': 0}, 'lt': {'value': 2}}
+            }
+        )
+        (fetched, ) = list(data_source.get_list('inc', object_filters=f))
+
+        assert fetched.id == '1'
+
+    @against(api_sql, sql)
+    def test_filter_by_str_id(
+        self,
+        data_source: OperableDataSource,
+        ds_sleep
+    ) -> None:
+
+        objs = [
+            data_source.data_object_factory(
+                'root',
+                id_=str(i),
+            )
+            for i in range(3)
+        ]
+        data_source.upsert('root', objs)
+
+        f = DataSourceFilter(
+            and_={
+                'id': {'gt': {'value': 0}, 'lt': {'value': 2}}
+            }
+        )
+        (fetched, ) = list(data_source.get_list('root', object_filters=f))
+
+        assert fetched.id == '1'
