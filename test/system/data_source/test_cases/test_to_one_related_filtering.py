9ec55803ba551fb94dd07c56983af3c30af992f1
Edward Moulsdale <em29@sanger.ac.uk>
2025-07-07 15:43:34 +0100

    red tests

--- test/system/data_source/test_cases/test_to_one_related_filtering.py
+++ test/system/data_source/test_cases/test_to_one_related_filtering.py
@@ -11,7 +11,7 @@ from ..fixtures import api_sql, sql
 class TestToOneRelatedFiltering:
     """TOLP-8867"""
 
-    @against(api_sql, sql)
+    @against(sql, api_sql)
     def test_filter_by_to_one_related_id(
         self,
         data_source: OperableDataSource,
@@ -20,14 +20,58 @@ class TestToOneRelatedFiltering:
 
         self.__upsert_objs(data_source)
 
-    @against(api_sql, sql)
-    def test_filter_by_to_related_attributes(
+        f = DataSourceFilter(
+            and_={
+                'my_root.id': {
+                    'eq': {
+                        'value': '1'
+                    }
+                },
+                'str_column': {
+                    'int_column': {
+                        'value': [0, 42, 2099]
+                    }
+                }
+            }
+        )
+
+        (rel_obj, ) = list(
+            data_source.get_list('related', f)
+        )
+
+        assert rel_obj.id == 'a'
+        assert rel_obj.int_column == 0
+
+    @against(sql, api_sql)
+    def test_filter_by_to_one_related_attribute(
         self,
         data_source: OperableDataSource,
         ds_sleep
     ) -> None:
 
         self.__upsert_objs(data_source)
+
+        f = DataSourceFilter(
+            and_={
+                'my_root.str_column': {
+                    'eq': {
+                        'value': 'hello, world'
+                    }
+                },
+                'id': {
+                    'int_column': {
+                        'value': list('cyz')
+                    }
+                }
+            }
+        )
+
+        (rel_obj, ) = list(
+            data_source.get_list('related', f)
+        )
+
+        assert rel_obj.id == 'c'
+        assert rel_obj.int_column == 2
 
     def __upsert_objs(
         self,
