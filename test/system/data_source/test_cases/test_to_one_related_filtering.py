eada93016b269db8b91c617e39e8ffb55ca8aa46
Edward Moulsdale <em29@sanger.ac.uk>
2025-07-07 15:34:11 +0100

    test skel

---
+++ test/system/data_source/test_cases/test_to_one_related_filtering.py
@@ -0,0 +1,30 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from tol.core import DataSourceFilter, OperableDataSource
+
+from ..dec import against
+from ..fixtures import api_sql, sql
+
+
+class TestToOneRelatedFiltering:
+    """TOLP-8867"""
+
+    @against(api_sql, sql)
+    def test_filter_by_to_one_related_id(
+        self,
+        data_source: OperableDataSource,
+        ds_sleep
+    ) -> None:
+
+        pass
+
+    @against(api_sql, sql)
+    def test_filter_by_str_id(
+        self,
+        data_source: OperableDataSource,
+        ds_sleep
+    ) -> None:
+
+        pass
