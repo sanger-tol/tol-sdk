bb2b4faf53f7a701e3ad536c02312272bd405d2c
Andrew Varley <av10@sanger.ac.uk>
2025-07-24 10:41:14 +0100

    TOLP-8942 Sorted out ID field in data loaders

--- src/tol/core/data_object_converter.py
+++ src/tol/core/data_object_converter.py
@@ -54,16 +54,17 @@ class DefaultDataObjectToDataObjectConverter(DataObjectToDataObjectOrUpdateConve
     def __init__(
         self,
         data_object_factory: DataObjectFactory,
-        destination_object_type: str | None = None
+        destination_object_type: str | None = None,
+        id_field: str | None = None
     ):
 
         super().__init__(data_object_factory)
         self.__dest_object_type = destination_object_type
+        self.__id_field = id_field
 
     def convert(
         self,
-        data_object: DataObject,
-        id_field: str | None = None
+        data_object: DataObject
     ) -> Iterable[DataObject]:
         """
         A "passthrough" converter only dealing with attributes:
@@ -79,11 +80,11 @@ class DefaultDataObjectToDataObjectConverter(DataObjectToDataObjectOrUpdateConve
             )
             ret = self._data_object_factory(
                 id_=(
-                    data_object.id if id_field is None
-                    else data_object.get_field_by_name(id_field)
+                    data_object.id if self.__id_field is None
+                    else data_object.get_field_by_name(self.__id_field)
                 ),
                 type_=dest_type,
-                attributes={k: v for k, v in data_object.attributes.items() if k != id_field}
+                attributes={k: v for k, v in data_object.attributes.items() if k != self.__id_field}
             )
             return iter([ret])
         return iter([])
