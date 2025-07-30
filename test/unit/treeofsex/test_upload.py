7338fe9e82d304c0619169a432fce3b7b648596c
Edward Moulsdale <em29@sanger.ac.uk>
2025-07-30 10:08:06 +0100

    lol

--- test/unit/treeofsex/test_upload.py
+++ test/unit/treeofsex/test_upload.py
@@ -83,3 +83,14 @@ class TestTOSUpload:
 
             for k in needed_keys:
                 assert getattr(obj, k) is not None
+
+        first = converted[0]
+
+        assert first.type == 'test'
+        assert int(first.id) == 1
+
+        assert first.source == 'book1'
+        assert int(first.taxon_id_lol) == 9606
+        assert first.attribute_key == 'average_height'
+        assert float(first.attribute_value) == 1.8
+        assert first.attribute_state_yeaahhhhhh == 'good'
