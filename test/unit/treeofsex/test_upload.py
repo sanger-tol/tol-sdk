4eac25a4f731eb8e4dd0a1822c072c2c4487a931
Edward Moulsdale <em29@sanger.ac.uk>
2025-07-30 09:17:09 +0100

    more test

--- test/unit/treeofsex/test_upload.py
+++ test/unit/treeofsex/test_upload.py
@@ -3,8 +3,13 @@
 # SPDX-License-Identifier: MIT
 
 from pathlib import Path
+from unittest.mock import create_autospec
 
 import pytest
+
+from tol.converter import YamlConverter
+from tol.core import OperableDataSource
+from tol.excel import ExcelDataSource
 
 
 @pytest.fixture(scope='class')
@@ -19,10 +24,32 @@ def xlsx_filename(base_dir: Path) -> str:
     return str(xlsx_path)
 
 
+@pytest.fixture(scope='class')
+def yaml_filename(base_dir: Path) -> str:
+    yaml_path = base_dir / 'tos.yaml'
+
+    return str(yaml_path)
+
+
+@pytest.fixture
+def mock_ds() -> OperableDataSource:
+    ds: OperableDataSource = create_autospec(
+        OperableDataSource,
+        spec_set=True,
+    )
+
+    ds.supported_types = ['test']
+
+    return ds
+
+
 class TestTOSUpload:
 
     def test_upload(
         self,
-        xlsx_filename: str
+        xlsx_filename: str,
+        yaml_filename: str,
+        mock_ds: OperableDataSource,
     ) -> None:
-        """"""
+
+        pass
