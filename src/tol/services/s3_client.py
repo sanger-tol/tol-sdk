7525d70e65b40d2d659071bfb63abe3b1b9d4974
Thomas Assheton <ta21@mib120490s.internal.sanger.ac.uk>
2025-08-21 11:23:23 +0100

    Added list_objects method to S3Client

--- src/tol/services/s3_client.py
+++ src/tol/services/s3_client.py
@@ -40,6 +40,12 @@ class S3Client:
     ) -> None:
 
         self.client.fget_object(bucket_name, object_name, file_path)
+    
+    def list_objects(
+        self,
+        bucket_name: str,
+    ) -> None:
+        self.client.list_objects(bucket_name)
 
     def put_object(
         self,
