c8fa1a2e98f1bf26a595b3836538637b662221fc
Thomas Assheton <ta21@mib120490s.internal.sanger.ac.uk>
2025-08-21 11:38:32 +0100

    Added test for list_objects method call in S3Client

--- test/unit/services/test_s3_client.py
+++ test/unit/services/test_s3_client.py
@@ -40,6 +40,15 @@ def test_get_object_calls_fget_object(mock_minio):
 
 
 @patch('tol.services.s3_client.Minio')
+def test_list_objects_calls_list_objects(mock_minio):
+    mock_instance = MagicMock()
+    mock_minio.return_value = mock_instance
+    client = S3Client()
+    client.list_objects('bucket')
+    mock_instance.list_objects.assert_called_once_with('bucket')
+
+
+@patch('tol.services.s3_client.Minio')
 def test_put_object_calls_fput_object(mock_minio):
     mock_instance = MagicMock()
     mock_minio.return_value = mock_instance
