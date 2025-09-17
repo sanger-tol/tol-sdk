a53670892b9cf7f1512180abe0b35bea7ba418a1
ene goktan <eg21@sanger.ac.uk>
2025-09-17 16:43:28 +0100

    remove previous logic, add vouchers in a separate query without any results metadata

--- src/tol/benchling/sql/extraction_extraction_type_dna.sql
+++ src/tol/benchling/sql/extraction_extraction_type_dna.sql
@@ -217,6 +217,7 @@ vouchers AS (
         'dna'::varchar AS extraction_type,
         f.name,
         dna.archive_purpose$,
+<<<<<<< HEAD
         NULL::double precision AS nanodrop_concentration_ngul,
         NULL::double precision AS dna_260_280_ratio,
         NULL::double precision AS dna_260_230_ratio,
