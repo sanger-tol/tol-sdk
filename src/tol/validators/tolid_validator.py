38922b39739510f75813eb6516a325cad20ccdf0
Logan Howat <lh31@sanger.ac.uk>
2025-11-27 11:55:18 +0000

    Still needs fixing

--- src/tol/validators/tolid_validator.py
+++ src/tol/validators/tolid_validator.py
@@ -5,7 +5,7 @@
 from tol.core import DataObject
 from tol.core.validate import Validator
 from tol.sources.tolid import tolid
-from tol.core import DataSourceError
+from tol.core import DataSourceError, DataSourceFilter
 
 
 class TolidValidator(Validator):
@@ -17,6 +17,9 @@ class TolidValidator(Validator):
     def __init__(
         self,
         species_attribute: str,
+        error_ignore_field: str,
+        error_ignore_value: str,
+        specimen_attribute: str,
         datasource = tolid(),
         warning_detail: str = 'Species not found in Tol ID source',
     ) -> None:
@@ -24,15 +27,20 @@ class TolidValidator(Validator):
         super().__init__()
 
         self._species_attribute = species_attribute
-        self._responses = {}
         self._datasource = datasource
         self._warning_detail = warning_detail
+        self._error_ignore_field = error_ignore_field
+        self._error_ignore_value = error_ignore_value
+        self._specimen_attribute = specimen_attribute
+        self._cached_species_id = {}
+        self._cached_tolids = {}
         
     def _validate_data_object(
         self,
         obj: DataObject
     ) -> None:
-        self._warning_on_species_not_in_tolid(obj=obj)
+        # self._warning_on_species_not_in_tolid(obj=obj)
+        self._error_on_specimen_id_and_taxon_not_matching_tolid(obj=obj)
 
     def _warning_on_species_not_in_tolid(
         self,
@@ -42,20 +50,52 @@ class TolidValidator(Validator):
         obj_species_id = obj.get_field_by_name(self._species_attribute)
         if self._species_attribute in obj.attributes:
             try:
-                if obj_species_id not in self._responses:
-                    if self._datasource.get_one('species', obj_species_id) != None:
-                        self._responses[obj_species_id] = True
+                if obj_species_id not in self._cached_species_id:
+                    if self._datasource.get_one('species', obj_species_id) is not None:
+                        self._cached_species_id[obj_species_id] = True
                     else:
-                        self._responses[obj_species_id] = False
-                
+                        self._cached_species_id[obj_species_id] = False
+
             except DataSourceError as e:
                 if e.status_code == 404:
-                    self._responses[obj_species_id] = False
+                    self._cached_species_id[obj_species_id] = False
 
-        species_in_tolid = self._responses[obj_species_id]
+        species_in_tolid = self._cached_species_id[obj_species_id]
         if species_in_tolid == False:
             self.add_warning(
                 object_id=obj.id,
                 detail=self._warning_detail,
                 field=self._species_attribute
             )
+            
+    def _error_on_specimen_id_and_taxon_not_matching_tolid(
+        self,
+        obj: DataObject,
+    ) -> None:
+        
+        if obj.get_field_by_name(self._error_ignore_field) is self._error_ignore_value:
+            return
+
+        if self._specimen_attribute in obj.attributes:
+            specimen_id = obj.get_field_by_name(self._specimen_attribute)
+            if specimen_id not in self._cached_tolids:
+                f = DataSourceFilter()
+                f.and_ = {'specimen_id': {'eq': {'value': specimen_id}}}
+                self._cached_tolids[specimen_id] = list(self._datasource.get_list(
+                    object_type='specimen',
+                    filters=f
+                ))
+
+            if (len(self._cached_tolids[specimen_id]) == 0):
+                return
+            else:
+                taxons = set()
+                for tolid_ in self._cached_tolids[specimen_id]:
+                    taxons.add(str(tolid_.species.id))
+                if str(obj.get_field_by_name(self._species_attribute)) not in taxons:
+                    self.add_error(
+                        object_id=obj.id,
+                        detail=f"Specimen ID {specimen_id} does not match Taxon ID {obj.get_field_by_name(self._species_attribute)} in TolID source",
+                        field=[self._specimen_attribute, self._species_attribute]
+                    )
+                
--- test/unit/validators/test_tolid_validator.py
+++ test/unit/validators/test_tolid_validator.py
@@ -4,66 +4,66 @@
 
 from typing import Any
 
-from tol.core import DataObject, DataSource
+from pytest import fixture
+from tol.core import DataObject, DataSource, DataSourceFilter
 from tol.core.operator import DetailGetter
-from unittest.mock import MagicMock, PropertyMock
+from unittest.mock import create_autospec, PropertyMock
 from tol.validators import TolidValidator
 
 class _MockDataSource(DataSource, DetailGetter):
 
-    def __init__(self, config: dict[str, Any], ctx_getter=None):
-        self.__ctx_getter = ctx_getter
+    def __init__(self, config: dict[str, Any]):
         super().__init__(config, [])
         
-    def get_by_id(self, object_type,):
+    def get_by_id(self):
         pass
 
     def get_one(self, object_type: str, object_id, **kwargs):
-        mock_object = MagicMock()
+        mock_object = create_autospec(DataObject, instance=True)
         if object_id == 'FAIL':
             return None
         else:
             return_value = {'TAXON_ID': object_id}
-        type(mock_object).attributes = PropertyMock(
-            return_value=return_value
-        )
-        mock_object.get_field_by_name.return_value = object_id
+            type(mock_object).attributes = PropertyMock(
+                return_value=return_value
+            )
+            mock_object.get_field_by_name.return_value = object_id
 
-        return [
-            mock_object
-        ]
+            return [
+                mock_object
+            ]
     
-    def get_list(
-        self,
-        object_type: str,
-        filters: dict[str, Any] | None = None,
-        **kwargs
-    ) -> list[DataObject]:
+    def get_list(self, object_type: str, filters: list[DataSourceFilter] = [], **kwargs):
+        value = filters.and_['specimen_id']['eq']['value']
+        if value == 'FAIL':
+            mock_object = create_autospec(DataObject, instance=True)
+            type(mock_object).attributes = PropertyMock(
+                return_value={'SPECIMEN_ID': 'SPEC' + value}
+            )
+            # Create a mock species object with an id attribute that does not match
+            # Taxon id in the original object
+            mock_species = create_autospec(DataObject, instance=True)
+            mock_species.id = 'SPECIESID_NOT_MATCH'
+            type(mock_object).species = PropertyMock(
+                return_value=mock_species
+            )
+            return [
+                mock_object
+            ]
+        else:
+            mock_object = create_autospec(DataObject, instance=True)
+            type(mock_object).attributes = PropertyMock(
+                return_value={'SPECIMEN_ID': 'SPEC' + value}
+            )
+            mock_species = create_autospec(DataObject, instance=True)
+            mock_species.id = value.replace('SPEC', '')
+            type(mock_object).species = PropertyMock(
+                return_value=mock_species
+            )
+            return [
+                mock_object
+            ]
 
-        mock_object = MagicMock(DataObject)
-        type(mock_object).attributes = PropertyMock(
-            return_value={'TAXON_ID': 'ABC123'}
-        )
-        mock_object.get_field_by_name.return_value = 'ABC123'
-        
-        mock_object2 = MagicMock(DataObject)
-        type(mock_object2).attributes = PropertyMock(
-            return_value={'TAXON_ID': 'DEF123'}
-        )
-        mock_object2.get_field_by_name.return_value = 'DEF123'
-        
-        mock_object3 = MagicMock(DataObject)
-        type(mock_object3).attributes = PropertyMock(
-            return_value={'TAXON_ID': 'FAIL'}
-        )
-        mock_object3.get_field_by_name.return_value = 'FAIL'
-        
-        return [
-            mock_object,
-            mock_object2,
-            mock_object3
-        ]
-    
     @property
     def supported_types(self) -> list[str]:
         return ['species']
@@ -72,21 +72,70 @@ class TestTolidValidator:
 
     def test_warning(
         self,
+        data_objects
     ) -> None:
         
         mock_datasource = _MockDataSource(config={})
-        mock_objs = mock_datasource.get_list(object_type='species')
 
         validator = TolidValidator(
             species_attribute='TAXON_ID',
             datasource=mock_datasource,
-            warning_detail='TEST WARNING'
+            warning_detail='TEST WARNING',
+            error_ignore_field='IGNORE_FIELD',
+            error_ignore_value='IGNORE_VALUE',
+            specimen_attribute='SPECIMEN_ID',
         )
 
-        # consume the `Iterable`
         list (
-            validator.validate(mock_objs)
+            validator.validate(data_objects)
         )
 
         assert validator.results
+        assert len(validator.errors) == 1
         assert len(validator.warnings) == 1
+
+
+    def _make_side_effect(self, object_id):
+        # Helper to create side effect function for get_field_by_name
+        # name is the value passed to get_field_by_name
+        def side_effect(name):
+            values = {
+                "TAXON_ID": object_id,
+                "SPECIMEN_ID": f"SPEC{object_id}"
+            }
+            if object_id == "FAIL":
+                values = {
+                    "TAXON_ID": None,
+                    "SPECIMEN_ID": 'FAIL'
+                }
+            return values.get(name)
+        return side_effect
+
+
+    @fixture
+    def data_objects(
+        self,
+    ) -> list[DataObject]:
+        mock_object = create_autospec(DataObject, instance=True)
+        type(mock_object).attributes = PropertyMock(
+            return_value={'TAXON_ID': 'ABC', 'SPECIMEN_ID': 'SPECABC'}
+        )
+        mock_object.get_field_by_name.side_effect = self._make_side_effect('ABC')
+        
+        mock_object2 = create_autospec(DataObject, instance=True)
+        type(mock_object2).attributes = PropertyMock(
+            return_value={'TAXON_ID': 'DEF', 'SPECIMEN_ID': 'SPECDEF'}
+        )
+        mock_object2.get_field_by_name.side_effect = self._make_side_effect('DEF')
+        
+        mock_object3 = create_autospec(DataObject, instance=True)
+        type(mock_object3).attributes = PropertyMock(
+            return_value={'TAXON_ID': 'FAIL', 'SPECIMEN_ID': 'SPECFAIL'}
+        )
+        mock_object3.get_field_by_name.side_effect = self._make_side_effect('FAIL')
+        
+        return [
+            mock_object,
+            mock_object2,
+            mock_object3
+        ]
