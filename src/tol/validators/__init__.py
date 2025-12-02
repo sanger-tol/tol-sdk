5e75b4813e6a022212fbaa7d1518fa9b74471638
Andrew Varley <av10@sanger.ac.uk>
2025-12-02 17:06:57 +0000

    TOLP-6945 STS Fields validator

--- src/tol/validators/__init__.py
+++ src/tol/validators/__init__.py
@@ -10,6 +10,7 @@ from .ena_submittable import EnaSubmittableValidator  # noqa
 from .regex import Regex, RegexValidator  # noqa
 from .regex_by_value import RegexByValueValidator  # noqa
 from .specimens_have_same_taxon import SpecimensHaveSameTaxonValidator # noqa
+from .sts_fields import StsFieldsValidator  # noqa
 from .tolid_validator import TolidValidator, TolidConfig  # noqa
 from .unique_values import UniqueValuesValidator  # noqa
 from .unique_whole_organisms import UniqueWholeOrganismsValidator # noqa
