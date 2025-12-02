9d0874ce5c97976efe4c7ac566033bbd321d474f
Thomas Assheton <ta21@sanger.ac.uk>
2025-12-02 10:47:56 +0000

    Define config for validator

--- src/tol/validators/mutually_exclusive.py
+++ src/tol/validators/mutually_exclusive.py
@@ -3,16 +3,31 @@
 # SPDX-License-Identifier: MIT
 
 from dataclasses import dataclass
+from typing import List
 
 from tol.core import DataObject, Validator
+
+from .interfaces import ConditionEvaluator, Condition
 
 
 @dataclass(slots=True, frozen=True)
 class MutuallyExclusiveConfig:
-    pass
+    first_field_where: Condition
+    second_field_where: Condition
+    target_fields: List[str]
+    error_message: str | None = None
+
+    def _get_error_message(self) -> str:
+        if self.error_message is None:
+            return (
+                f'The conditions {self.first_field_where} and {self.second_field_where} ' 
+                f'must be mutually exclusive'
+            )
+        else:
+            return self.error_message
 
 
-class MutuallyExclusiveValidator(Validator):
+class MutuallyExclusiveValidator(Validator, ConditionEvaluator):
     __slots__ = ['__config']
     __config: MutuallyExclusiveConfig
     
