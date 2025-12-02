3ed4cfe3c72e9c13890e3bfbe70fa801c5b10d9d
Andrew Varley <av10@sanger.ac.uk>
2025-12-02 11:54:22 +0000

    TOLP-9292 ENA submittable validator

--- src/tol/validators/__init__.py
+++ src/tol/validators/__init__.py
@@ -6,6 +6,7 @@ from .allowed_keys import AllowedKeysValidator  # noqa
 from .allowed_values import AllowedValues, AllowedValuesValidator  # noqa
 from .allowed_values_from_datasource import AllowedValuesFromDataSourceValidator  # noqa
 from .assert_on_condition import AssertOnConditionValidator # noqa
+from .ena_submittable import EnaSubmittableValidator  # noqa
 from .regex import Regex, RegexValidator  # noqa
 from .regex_by_value import RegexByValueValidator  # noqa
 from .specimens_have_same_taxon import SpecimensHaveSameTaxonValidator # noqa
