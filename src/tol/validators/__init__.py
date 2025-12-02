8d1bb7cbf8b8c763aece64ce3a6d1397925c1281
Thomas Assheton <ta21@sanger.ac.uk>
2025-12-02 13:53:13 +0000

    Export Condition from validators package

--- src/tol/validators/__init__.py
+++ src/tol/validators/__init__.py
@@ -12,3 +12,6 @@ from .unique_values import UniqueValuesValidator  # noqa
 from .tolid_validator import TolidValidator, TolidConfig  # noqa
 from .unique_whole_organisms import UniqueWholeOrganismsValidator  # noqa
 from .specimens_have_same_taxon import SpecimensHaveSameTaxonValidator  # noqa
+
+# Conditions are used where validators are defined, not just within validators
+from .interfaces import Condition
