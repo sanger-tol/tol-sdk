ef9a70aa3232a4421bdba3bafccbd9ec33755004
Thomas Assheton <ta21@sanger.ac.uk>
2025-12-03 11:56:39 +0000

    Add unit tests for invalid results

--- test/unit/validators/test_mutually_exclusive.py
+++ test/unit/validators/test_mutually_exclusive.py
@@ -22,9 +22,9 @@ class TestMutuallyExclusiveValidator:
             'RACK_OR_PLATE_ID': 'a',
             'TUBE_OR_WELL_ID': 'b',
         }
-        mock_one: DataObject = create_autospec(DataObject)
-        mock_one.id = 'a'
-        mock_one.attributes = {
+        mock_two: DataObject = create_autospec(DataObject)
+        mock_two.id = 'a'
+        mock_two.attributes = {
             'SYMBIONT': 'NOT SYMBIONT',
             'RACK_OR_PLATE_ID': 'c',
             'TUBE_OR_WELL_ID': 'd',
@@ -43,7 +43,7 @@ class TestMutuallyExclusiveValidator:
             ),
             target_fields=[
                 'RACK_OR_PLATE_ID',
-                'TUBE_OR_WELL_ID'
+                'TUBE_OR_WELL_ID',
             ],
             error_message='All symbionts must have a TARGET with same rack/plate and tube/well'
         )
@@ -52,7 +52,99 @@ class TestMutuallyExclusiveValidator:
 
         # consume the `Iterable`
         list(
-            validator.validate(mock_objs)
+            validator.validate(iter([mock_one, mock_two]))
         )
 
         assert len(validator.results) == 0
+    
+    def test_clash_first_then_second(
+        self, mock_objs: Iterable[DataObject]
+    ) -> None:
+        # Discard the sample mock objects (which won't be useful for this test)
+        mock_one: DataObject = create_autospec(DataObject)
+        mock_one.id = 'a'
+        mock_one.attributes = {
+            'SYMBIONT': 'NOT SYMBIONT',
+            'RACK_OR_PLATE_ID': 'a',
+            'TUBE_OR_WELL_ID': 'b',
+        }
+        mock_two: DataObject = create_autospec(DataObject)
+        mock_two.id = 'a'
+        mock_two.attributes = {
+            'SYMBIONT': 'SYMBIONT',
+            'RACK_OR_PLATE_ID': 'a',
+            'TUBE_OR_WELL_ID': 'b',
+        }
+
+        config = MutuallyExclusiveValidator.Config(
+            first_field_where=Condition(
+                field='SYMBIONT',
+                operator='!=',
+                value='SYMBIONT',
+            ),
+            second_field_where=Condition(
+                field='SYMBIONT',
+                operator='==',
+                value='SYMBIONT',
+            ),
+            target_fields=[
+                'RACK_OR_PLATE_ID',
+                'TUBE_OR_WELL_ID',
+            ],
+            error_message='All symbionts must have a TARGET with same rack/plate and tube/well'
+        )
+
+        validator = MutuallyExclusiveValidator(config)
+
+        # consume the `Iterable`
+        list(
+            validator.validate(iter([mock_one, mock_two]))
+        )
+
+        assert len(validator.errors) == 1
+    
+    def test_clash_second_then_first(
+        self, mock_objs: Iterable[DataObject]
+    ) -> None:
+        # Discard the sample mock objects (which won't be useful for this test)
+        mock_one: DataObject = create_autospec(DataObject)
+        mock_one.id = 'a'
+        mock_one.attributes = {
+            'SYMBIONT': 'SYMBIONT',
+            'RACK_OR_PLATE_ID': 'a',
+            'TUBE_OR_WELL_ID': 'b',
+        }
+        mock_two: DataObject = create_autospec(DataObject)
+        mock_two.id = 'a'
+        mock_two.attributes = {
+            'SYMBIONT': 'NOT SYMBIONT',
+            'RACK_OR_PLATE_ID': 'a',
+            'TUBE_OR_WELL_ID': 'b',
+        }
+
+        config = MutuallyExclusiveValidator.Config(
+            first_field_where=Condition(
+                field='SYMBIONT',
+                operator='!=',
+                value='SYMBIONT',
+            ),
+            second_field_where=Condition(
+                field='SYMBIONT',
+                operator='==',
+                value='SYMBIONT',
+            ),
+            target_fields=[
+                'RACK_OR_PLATE_ID',
+                'TUBE_OR_WELL_ID',
+            ],
+            error_message='All symbionts must have a TARGET with same rack/plate and tube/well'
+        )
+
+        validator = MutuallyExclusiveValidator(config)
+
+        # consume the `Iterable`
+        list(
+            validator.validate(iter([mock_one, mock_two]))
+        )
+
+        assert len(validator.errors) == 1
