dd46660e14ec12a8a065ad21ff9905ca340d8228
Thomas Assheton <ta21@sanger.ac.uk>
2025-12-03 14:15:36 +0000

    Match code style in TolidValidator to other validators

--- src/tol/validators/tolid_validator.py
+++ src/tol/validators/tolid_validator.py
@@ -3,8 +3,9 @@
 # SPDX-License-Identifier: MIT
 
 from dataclasses import dataclass
+from typing import Any, Dict
 
-from tol.core import DataObject
+from tol.core import DataObject, DataSource
 from tol.core import DataSourceError, DataSourceFilter
 from tol.core.validate import Validator
 from tol.sources.tolid import tolid
@@ -15,7 +16,6 @@ class TolidValidator(Validator):
     Validates that a stream of `DataObject` instances
     contains unique Tol IDs.
     """
-
     @dataclass(slots=True, frozen=True, kw_only=True)
     class Config:
         species_id_field: str
@@ -23,6 +23,12 @@ class TolidValidator(Validator):
         error_ignore_field: str
         error_ignore_value: str
         warning_detail: str = 'Species not found in Tol ID source'
+    
+    __slots__ = ['__config', '__datasource', '__cached_species_id', '__cached_tolids']
+    __config: Config
+    __datasource: DataSource
+    __cached_species_ids: Dict[str, Any]
+    __cached_tolids: Dict[str, Any]
 
     def __init__(
         self,
@@ -32,73 +38,73 @@ class TolidValidator(Validator):
 
         super().__init__()
 
-        self._datasource = datasource
-        self._config = config
-        self._cached_species_id = {}
-        self._cached_tolids = {}
+        self.__config = config
+        self.__datasource = datasource
+        self.__cached_species_ids = {}
+        self.__cached_tolids = {}
 
     def _validate_data_object(
         self,
         obj: DataObject
     ) -> None:
-        self._warning_on_species_not_in_tolid(obj=obj)
-        self._error_on_specimen_id_and_taxon_not_matching_tolid(obj=obj)
+        self.__warning_on_species_not_in_tolid(obj=obj)
+        self.__error_on_specimen_id_and_taxon_not_matching_tolid(obj=obj)
 
-    def _warning_on_species_not_in_tolid(
+    def __warning_on_species_not_in_tolid(
         self,
         obj: DataObject,
     ) -> None:
 
-        obj_species_id = obj.get_field_by_name(self._config.species_id_field)
-        if self._config.species_id_field in obj.attributes:
+        obj_species_id = obj.get_field_by_name(self.__config.species_id_field)
+        if self.__config.species_id_field in obj.attributes:
             try:
-                if obj_species_id not in self._cached_species_id:
-                    self._cached_species_id[obj_species_id] = (
-                        self._datasource.get_one('species', obj_species_id) is not None)
+                if obj_species_id not in self.__cached_species_ids:
+                    self.__cached_species_ids[obj_species_id] = (
+                        self.__datasource.get_one('species', obj_species_id) is not None)
 
             except DataSourceError as e:
                 if e.status_code == 404:
-                    self._cached_species_id[obj_species_id] = False
+                    self.__cached_species_ids[obj_species_id] = False
 
-        species_in_tolid = self._cached_species_id[obj_species_id]
+        species_in_tolid = self.__cached_species_ids[obj_species_id]
         if species_in_tolid is False:
             self.add_warning(
                 object_id=obj.id,
-                detail=self._config.warning_detail,
-                field=self._config.species_id_field,
+                detail=self.__config.warning_detail,
+                field=self.__config.species_id_field,
             )
 
-    def _error_on_specimen_id_and_taxon_not_matching_tolid(
+    def __error_on_specimen_id_and_taxon_not_matching_tolid(
         self,
         obj: DataObject,
     ) -> None:
 
-        if (obj.get_field_by_name(self._config.error_ignore_field) is
-                self._config.error_ignore_value):
+        if (obj.get_field_by_name(self.__config.error_ignore_field) is
+                self.__config.error_ignore_value):
             return
 
-        if self._config.specimen_id_field in obj.attributes:
-            specimen_id = obj.get_field_by_name(self._config.specimen_id_field)
-            if specimen_id not in self._cached_tolids:
+        if self.__config.specimen_id_field in obj.attributes:
+            specimen_id = obj.get_field_by_name(self.__config.specimen_id_field)
+            if specimen_id not in self.__cached_tolids:
                 f = DataSourceFilter()
                 f.and_ = {'specimen_id': {'eq': {'value': specimen_id}}}
-                self._cached_tolids[specimen_id] = list(self._datasource.get_list(
+                self.__cached_tolids[specimen_id] = list(self.__datasource.get_list(
                     object_type='specimen',
                     filters=f
                 ))
 
-            if (len(self._cached_tolids[specimen_id]) == 0):
+            if (len(self.__cached_tolids[specimen_id]) == 0):
                 return
             else:
                 taxons = set()
-                for tolid_ in self._cached_tolids[specimen_id]:
+                for tolid_ in self.__cached_tolids[specimen_id]:
                     taxons.add(str(tolid_.species.id))
 
-                if str(obj.get_field_by_name(self._config.species_id_field)) not in taxons:
+                if str(obj.get_field_by_name(self.__config.species_id_field)) not in taxons:
                     self.add_error(
                         object_id=obj.id,
                         detail=f'Specimen ID {specimen_id} does not match Taxon ID '
-                               f'{obj.get_field_by_name(self._config.species_id_field)}'
+                               f'{obj.get_field_by_name(self.__config.species_id_field)}'
                                'in TolID source',
-                        field=[self._config.specimen_id_field, self._config.species_id_field]
+                        field=[self.__config.specimen_id_field, self.__config.species_id_field]
                     )
