17646ff3bd7bbe887149740c6cdcdd0dc08d416a
Nithin Mathew Joseph <nj9@sanger.ac.uk>
2026-01-05 12:35:57 +0000

    TOLP-9408: Port missing internal validators from STS

---
+++ test/unit/validators/test_value_check.py
@@ -0,0 +1,36 @@
+# SPDX-FileCopyrightText: 2025 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from typing import Iterable
+from unittest.mock import create_autospec
+
+from tol.core import DataObject
+from tol.validators import ValueCheckValidator
+
+
+class TestValueCheckValidator:
+    def test_valid(
+        self,
+        mock_objs: Iterable[DataObject],
+    ) -> None:
+        # Discard the sample mock objects (which won't be useful for this test)
+        mock_one: DataObject = create_autospec(DataObject)
+        mock_one.id = 'a'
+        mock_one.attributes = {
+            'SYMBIONT': 'SYMBIONT',
+        }
+        mock_one.SYMBIONT = 'SYMBIONT'
+
+        config = ValueCheckValidator.Config(
+            field='SYMBIONT',
+            value='SYMBIONT'
+        )
+
+        validator = ValueCheckValidator(config)
+
+        # consume the `Iterable`
+        list(
+            validator.validate(iter([mock_one]))
+        )
+        assert validator.results
