286340346c1a4acbe79bf485a990546ba1e27687
Shweta Bhagat <sb83@sanger.ac.uk>
2026-01-08 09:27:33 +0000

    unit test cases

--- test/unit/validators/test_value_driven.py
+++ test/unit/validators/test_value_driven.py
@@ -8,80 +8,83 @@ from tol.core import DataObject
 from tol.validators import ValueDrivenValidator
 
 
-class TestValueCheckValidator:
-    # def test_validator_cached(
-    #     self,
-    #     mock_objs: Iterable[DataObject]
-    # ) -> None:
-    #     """
-    #     Uses two data objects that have the same value for their key column. Tests whether the
-    #     validator used for the second data object is the same as the one used for the first.
-    #     """
-    #     # Discard the sample mock objects (which won't be useful for this test)
-    #     mock_one: DataObject = create_autospec(DataObject)
-    #     mock_one.id = 'a'
-    #     mock_one.attributes = {
-    #         'key_column': 'value_one',
-    #         'b': 'b',
-    #     }
-    #     mock_one.key_column = 'value_one'
-    #     mock_one.b = 'b'
-    #     mock_two: DataObject = create_autospec(DataObject)
-    #     mock_two.id = 'b'
-    #     mock_two.attributes = {
-    #         'key_column': 'value_one',
-    #         'b': 'b',
-    #     }
-    #     mock_two.key_column = 'value_one'
-    #     mock_two.b = 'b'
+import sys
+from unittest.mock import MagicMock
 
-    #     config = ValueDrivenValidator.Config(
-    #         key_column='value_one',
-    #         validations={
-    #             'value_one': {
-    #                 'module': 'validators.unique_value_check',
-    #                 'class_name': 'UniqueValueCheckValidator',
-    #                 'config_details': {
-    #                     'field': 'b',
-    #                 }
-    #             }
-    #         }
-    #     )
+class TestValueDrivenValidator:
+    def test_validator_cached(self):
+        """
+        Uses two data objects that have the same value for their key column. Tests whether the
+        validator used for the second data object is the same as the one used for the first.
+        """
+        class DummyValidator:
+            class Config:
+                def __init__(self, field):
+                    self.field = field
+            def __init__(self, config):
+                self.config = config
+                self.called = []
+            def _validate_data_object(self, obj):
+                self.called.append(obj.id)
 
-    #     validator = ValueDrivenValidator(config)
+        sys.modules['validators.unique_value_check'] = MagicMock()
+        setattr(sys.modules['validators.unique_value_check'], 'UniqueValueCheckValidator', DummyValidator)
 
-    #     # consume the `Iterable`
-    #     list(
-    #         validator.validate(iter([mock_one, mock_two]))
-    #     )
-    #     assert validator.results
+        mock_one: DataObject = create_autospec(DataObject)
+        mock_one.id = 'a'
+        mock_one.get_field_by_name.return_value = 'value_one'
+        mock_two: DataObject = create_autospec(DataObject)
+        mock_two.id = 'b'
+        mock_two.get_field_by_name.return_value = 'value_one'
 
-    def test_multiple_validators(
-        self,
-    ) -> None:
+        config = ValueDrivenValidator.Config(
+            key_column='key_column',
+            validations={
+                'value_one': {
+                    'module': 'validators.unique_value_check',
+                    'class_name': 'UniqueValueCheckValidator',
+                    'config_details': {
+                        'field': 'b',
+                    }
+                }
+            }
+        )
+
+        validator = ValueDrivenValidator(config)
+        list(validator.validate(iter([mock_one, mock_two])))
+        cached = validator._ValueDrivenValidator__cached_validators
+        assert len(cached) == 1
+        instance = cached['value_one']
+        assert instance.called == ['a', 'b']
+
+    def test_multiple_validators(self):
         """
         Tests whether the validator is able to work with multiple validators at once
         """
-        # Discard the sample mock objects (which won't be useful for this test)
+        class DummyValidator:
+            class Config:
+                def __init__(self, field):
+                    self.field = field
+            def __init__(self, config):
+                self.config = config
+                self.called = []
+            def _validate_data_object(self, obj):
+                self.called.append(obj.id)
+
+        sys.modules['validators.unique_value_check'] = MagicMock()
+        sys.modules['validators.other_value_check'] = MagicMock()
+        setattr(sys.modules['validators.unique_value_check'], 'UniqueValueCheckValidator', DummyValidator)
+        setattr(sys.modules['validators.other_value_check'], 'OtherValueCheckValidator', DummyValidator)
+
         mock_one: DataObject = create_autospec(DataObject)
         mock_one.id = 'a'
-        mock_one.attributes = {
-            'key_column': 'value_one',
-            'b': 'b',
-        }
-        mock_one.key_column = 'value_one'
-        mock_one.b = 'b'
+        mock_one.get_field_by_name.return_value = 'value_one'
         mock_two: DataObject = create_autospec(DataObject)
         mock_two.id = 'b'
-        mock_two.attributes = {
-            'key_column': 'value_two',
-            'c': 'c',
-        }
-        mock_two.key_column = 'value_two'
-        mock_two.c = 'c'
+        mock_two.get_field_by_name.return_value = 'value_two'
 
         config = ValueDrivenValidator.Config(
-            key_column='value_one',
+            key_column='key_column',
             validations={
                 'value_one': {
                     'module': 'validators.unique_value_check',
@@ -91,8 +94,8 @@ class TestValueCheckValidator:
                     }
                 },
                 'value_two': {
-                    'module': 'validators.unique_value_check',
-                    'class_name': 'UniqueValueCheckValidator',
+                    'module': 'validators.other_value_check',
+                    'class_name': 'OtherValueCheckValidator',
                     'config_details': {
                         'field': 'c',
                     }
@@ -101,45 +104,65 @@ class TestValueCheckValidator:
         )
 
         validator = ValueDrivenValidator(config)
+        list(validator.validate(iter([mock_one, mock_two])))
+        cached = validator._ValueDrivenValidator__cached_validators
+        assert len(cached) == 2
+        assert cached['value_one'].called == ['a']
+        assert cached['value_two'].called == ['b']
 
-        # consume the `Iterable`
-        list(
-            validator.validate(iter([mock_one, mock_two]))
-        )
-        assert not validator.results
+    def test_failing_validator(self):
+        """
+        Tests whether the overall validator fails as expected if one of its subvalidators fails
+        """
+        class DummyValidator:
+            class Config:
+                def __init__(self, field):
+                    self.field = field
+            def __init__(self, config):
+                self.config = config
+            def _validate_data_object(self, obj):
+                raise Exception('Subvalidator failed')
 
-    # def test_failing_validator(
-    #     self,
-    #     mock_objs: Iterable[DataObject]
-    # ) -> None:
-    #     """
-    #     Tests whether the overall validator fails as expected if one of its subvalidators fails
-    #     """
-    #     pass
+        sys.modules['validators.unique_value_check'] = MagicMock()
+        setattr(sys.modules['validators.unique_value_check'], 'UniqueValueCheckValidator', DummyValidator)
 
-    def test_invalid_config(
-        self,
-    ) -> None:
-        # Discard the sample mock objects (which won't be useful for this test)
         mock_one: DataObject = create_autospec(DataObject)
         mock_one.id = 'a'
-        mock_one.attributes = {
-            'key_column': 'value_one',
-            'b': 'b',
-        }
-        mock_one.key_column = 'value_one'
-        mock_one.b = 'b'
-        mock_two: DataObject = create_autospec(DataObject)
-        mock_two.id = 'b'
-        mock_two.attributes = {
-            'key_column': 'value_one',
-            'b': 'b',
-        }
-        mock_two.key_column = 'value_one'
-        mock_two.b = 'b'
+        mock_one.get_field_by_name.return_value = 'value_one'
 
         config = ValueDrivenValidator.Config(
-            key_column='value_one',
+            key_column='key_column',
+            validations={
+                'value_one': {
+                    'module': 'validators.unique_value_check',
+                    'class_name': 'UniqueValueCheckValidator',
+                    'config_details': {
+                        'field': 'b',
+                    }
+                }
+            }
+        )
+
+        validator = ValueDrivenValidator(config)
+        try:
+            list(validator.validate(iter([mock_one])))
+            assert False, 'Should have raised Exception'
+        except Exception as e:
+            assert str(e) == 'Subvalidator failed'
+
+    def test_invalid_config(self):
+        """
+        Tests that an invalid config raises the correct exception
+        """
+        mock_one: DataObject = create_autospec(DataObject)
+        mock_one.id = 'a'
+        mock_one.get_field_by_name.return_value = 'value_one'
+        mock_two: DataObject = create_autospec(DataObject)
+        mock_two.id = 'b'
+        mock_two.get_field_by_name.return_value = 'value_one'
+
+        config = ValueDrivenValidator.Config(
+            key_column='key_column',
             validations={
                 'value_one': {
                     # keys are missing here
@@ -150,44 +173,50 @@ class TestValueCheckValidator:
             }
         )
 
-        validator = ValueDrivenValidator(
-            config=config
-        )
-
+        validator = ValueDrivenValidator(config)
         threw_exception = False
         try:
-            # consume the `Iterable`
-            list(
-                validator.validate(iter([mock_one, mock_two]))
-            )
+            list(validator.validate(iter([mock_one, mock_two])))
         except Exception as e:
             threw_exception = True
-            assert e.args[0] == 'ValueDrivenValidator set up incorrectly. ' + \
-                'Failed to retrieve validator information from config'
-
+            assert e.args[0] == 'ValueDrivenValidator set up incorrectly. Failed to retrieve validator information from config'
         assert threw_exception
 
-    # def test_valid(
-    #     self,
-    #     mock_objs: Iterable[DataObject],
-    # ) -> None:
-    #     # Discard the sample mock objects (which won't be useful for this test)
-    #     mock_one: DataObject = create_autospec(DataObject)
-    #     mock_one.id = 'a'
-    #     mock_one.attributes = {
-    #         'SYMBIONT': 'SYMBIONT',
-    #     }
-    #     mock_one.SYMBIONT = 'SYMBIONT'
+    def test_valid(self):
+        """
+        Tests a valid configuration and successful validation
+        """
+        class DummyValidator:
+            class Config:
+                def __init__(self, field):
+                    self.field = field
+            def __init__(self, config):
+                self.config = config
+                self.called = []
+            def _validate_data_object(self, obj):
+                self.called.append(obj.id)
 
-    #     config = ValueCheckValidator.Config(
-    #         field='SYMBIONT',
-    #         value='SYMBIONT'
-    #     )
+        sys.modules['validators.unique_value_check'] = MagicMock()
+        setattr(sys.modules['validators.unique_value_check'], 'UniqueValueCheckValidator', DummyValidator)
 
-    #     validator = ValueCheckValidator(config)
+        mock_one: DataObject = create_autospec(DataObject)
+        mock_one.id = 'a'
+        mock_one.get_field_by_name.return_value = 'value_one'
 
-    #     # consume the `Iterable`
-    #     list(
-    #         validator.validate(iter([mock_one]))
-    #     )
-    #     assert validator.results
+        config = ValueDrivenValidator.Config(
+            key_column='key_column',
+            validations={
+                'value_one': {
+                    'module': 'validators.unique_value_check',
+                    'class_name': 'UniqueValueCheckValidator',
+                    'config_details': {
+                        'field': 'b',
+                    }
+                }
+            }
+        )
+
+        validator = ValueDrivenValidator(config)
+        list(validator.validate(iter([mock_one])))
+        cached = validator._ValueDrivenValidator__cached_validators
+        assert cached['value_one'].called == ['a']
