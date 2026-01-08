1ff60bc750733a7aefebe6af3ca5cda385b8dbd9
Nithin <nj9@sanger.ac.uk>
2026-01-08 09:07:19 +0000

    unit testing for the bioscan plating and collection date validator

--- src/tol/validators/date_collection_vs_plating_estimation.py
+++ src/tol/validators/date_collection_vs_plating_estimation.py
@@ -3,10 +3,10 @@
 # SPDX-License-Identifier: MIT
 
 from dataclasses import dataclass
+from datetime import datetime
 
 from tol.core import Validator
 from tol.core.data_object import DataObject
-import datetime
 
 
 class DateCollectionVsPlatingEstimationValidator(Validator):
@@ -28,34 +28,46 @@ class DateCollectionVsPlatingEstimationValidator(Validator):
         self.__config = config
 
     def _validate_data_object(self, obj: DataObject) -> None:
-        # This function is used to check if the dates obtained 
-        # are in the standard format and the date of collection 
+        # This function is used to check if the dates obtained
+        # are in the standard format and the date of collection
         # is not preceding the date of plating
 
-        collection_date = obj.attributes.get(self.__config.collection)
-        plating_date = obj.attributes.get(self.__config.plating)
+        collection_value = obj.attributes.get(self.__config.collection)
+        plating_value = obj.attributes.get(self.__config.plating)
 
-        if collection_date is not None:
+        collection_date = None
+        plating_date = None
+
+        # Validate collection date format
+        if collection_value is not None:
             try:
-                datetime.fromisoformat(collection_date)
+                collection_date = datetime.fromisoformat(collection_value)
             except ValueError:
                 self.add_error(
                     object_id=obj.id,
                     detail=f'{self.__config.collection} is not in the right date format',
                     field=self.__config.collection,
                 )
-        if plating_date is not None:
+                return  # Skip comparison if format is invalid
+
+        # Validate plating date format
+        if plating_value is not None:
             try:
-                datetime.fromisoformat(plating_date)
+                plating_date = datetime.fromisoformat(plating_value)
             except ValueError:
                 self.add_error(
                     object_id=obj.id,
                     detail=f'{self.__config.plating} is not in the right date format',
                     field=self.__config.plating,
                 )
-        if plating_date < collection_date:
-            self.add_error(
+                return  # Skip comparison if format is invalid
+
+        # Compare dates if both are valid
+        if collection_date is not None and plating_date is not None:
+            if collection_date > plating_date:
+                self.add_error(
                     object_id=obj.id,
-                    detail=f'conflicting dates between {self.__config.plating} and {self.__config.collection}',
-                    field=[self.__config.plating, self.__config.collection]
+                    detail=f'Collection date ({collection_date})'
+                    f'is after plating date ({plating_date})',
+                    field=[self.__config.collection, self.__config.plating],
                 )
