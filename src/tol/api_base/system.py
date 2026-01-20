12aaab7f1054c7414c596bdb1988df5021133ee4
Logan Howat <lh31@sanger.ac.uk>
2026-01-20 09:15:23 +0000

    TOLP-9451: Adding SDK version to package-versions endpoint

--- src/tol/api_base/system.py
+++ src/tol/api_base/system.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 
+import glob
+
 from flask import Blueprint
 
 
@@ -16,6 +18,7 @@ def system_blueprint(
     By default includes:
     - /healthz      - used for health probes in kubernetes
     - /environment  - indicates the deployment environment
+    - /package-versions  - indicates the deployment package versions
 
     Parameters:
     - url_prefix    - the prefix under which to serve this blueprint's
@@ -39,5 +42,20 @@ def system_blueprint(
         return {
             k: env_vars.get(v) for k, v in env_map.items()
         }, 200
+
+    @system_blueprint.route('/package-versions')
+    def package_versions():
+        glob_paths = glob.glob('./**/requirements.txt', recursive=True)
+        with open(glob_paths[0], 'r') as f:
+            requirements = f.readlines()
+            for requirement in requirements:
+                if 'tol-sdk' in requirement:
+                    tol_sdk_version = requirement.split('==')[1].strip()
+                    break
+            f.close()
+
+        result = {'tol_sdk': tol_sdk_version}
+
+        return result, 200
 
     return system_blueprint
