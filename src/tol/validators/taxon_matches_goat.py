99fd97164d22812fef3f08d924de77372e6cee26
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-26 11:19:49 +0000

    Set up caching

--- src/tol/validators/taxon_matches_goat.py
+++ src/tol/validators/taxon_matches_goat.py
@@ -2,8 +2,28 @@
 #
 # SPDX-License-Identifier: MIT
 
-from tol.core import Validator
+from tol.core import DataObject, Validator
+from tol.sources.goat import goat, GoatDataSource
 
 
 class TaxonMatchesGoatValidator(Validator):
-    pass
+    __slots__ = ["__goat_datasource", "__cached_taxons"]
+    __goat_datasource: GoatDataSource
+    __cached_taxons: dict[str, DataObject]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.__goat_datasource = goat()
+        self.__cached_taxons = {}
+    
+    def _validate_data_object(self, obj: DataObject) -> None:
+        taxon_id = obj.get_field_by_name("taxon_id")
+
+        # Check whether we already have the information for this id in the cache.
+        # If we don't, fetch it from GOAT and add it to the cache
+        taxon: DataObject
+        if taxon_id in self.__cached_taxons:
+            taxon = self.__cached_taxons[taxon_id]
+        else:
+            taxon = self.__goat_datasource.get_one("taxon", taxon_id)
+            # TODO What if None (unable to fetch from goat)?
