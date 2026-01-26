6690ea546077fee8d0d5767582bf8c299f15d37a
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-26 13:00:50 +0000

    Use single quote strings

--- src/tol/validators/taxon_matches_goat.py
+++ src/tol/validators/taxon_matches_goat.py
@@ -7,7 +7,7 @@ from tol.sources.goat import goat, GoatDataSource
 
 
 class TaxonMatchesGoatValidator(Validator):
-    __slots__ = ["__goat_datasource", "__cached_taxons"]
+    __slots__ = ['__goat_datasource', '__cached_taxons']
     __goat_datasource: GoatDataSource
     __cached_taxons: dict[str, DataObject]
 
@@ -17,18 +17,18 @@ class TaxonMatchesGoatValidator(Validator):
         self.__cached_taxons = {}
     
     def _validate_data_object(self, obj: DataObject) -> None:
-        taxon_id = obj.get_field_by_name("taxon_id")
+        taxon_id = obj.get_field_by_name('taxon_id')
 
         # Check whether we already have the information for this id in the cache.
-        # If we don't, fetch it from GOAT and add it to the cache
+        # If we don't, fetch it from GoaT and add it to the cache
         taxon: DataObject | None
         if taxon_id in self.__cached_taxons:
             taxon = self.__cached_taxons[taxon_id]
         else:
-            taxon = self.__goat_datasource.get_one("taxon", taxon_id)
+            taxon = self.__goat_datasource.get_one('taxon', taxon_id)
             if taxon is None:
                 self.add_error(
                     object_id=obj.id,
-                    detail="Invalid TaxonID: " + taxon_id,
-                    field="taxon_id"
+                    detail='Invalid TaxonID: ' + taxon_id,
+                    field='taxon_id'
                 )
