6e1ddcdd54fdd2f7d82a7920a93900d111359a07
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-27 12:03:15 +0000

    Add valid test

--- test/unit/validators/test_taxon_matches_goat.py
+++ test/unit/validators/test_taxon_matches_goat.py
@@ -10,6 +10,69 @@ from tol.validators.taxon_matches_goat import TaxonMatchesGoatValidator
 
 
 class TestTaxonMatchesGoatValidator:
+    def test_valid(
+        self,
+        mock_objs: Iterable[DataObject]
+    ) -> None:
+        """
+        Validates two data objects that have correct matching data with GoaT
+        """
+        class DummyScientificName:
+            """
+            Used to mock the scientific_name field for related data objects to the returned taxon
+            """
+            __slots__ = ['scientific_name']
+            scientific_name: str
+
+            def __init__(self, scientific_name) -> None:
+                self.scientific_name = scientific_name
+
+        # The provided mock objects are not appropriate for this test, so we set up new ones
+        del mock_objs
+        mock_one = create_autospec(DataObject)
+        mock_one.id = 'a'
+        def __get_field_by_name_one(name: str) -> Any:
+            match name:
+                case 'taxon_id':
+                    return '6344'
+                case 'scientific_name':
+                    return 'Arenicola marina'
+        mock_one.get_field_by_name.side_effect = __get_field_by_name_one
+        type(mock_one).species = PropertyMock(return_value=DummyScientificName('Arenicola marina'))
+        type(mock_one).genus = PropertyMock(return_value=DummyScientificName('Arenicolidae'))
+        type(mock_one).family = PropertyMock(return_value=DummyScientificName('Arenicolidae'))
+        type(mock_one).superfamily = PropertyMock(return_value=DummyScientificName(None))
+        type(mock_one).phylum = PropertyMock(return_value=DummyScientificName('Annelida'))
+        type(mock_one).kingdom = PropertyMock(return_value=DummyScientificName('Metazoa'))
+        type(mock_one).superkingdom = PropertyMock(return_value=DummyScientificName(None))
+        type(mock_one).domain = PropertyMock(return_value=DummyScientificName('Eukaryota'))
+        mock_two = create_autospec(DataObject)
+        mock_two.id = 'b'
+        def __get_field_by_name_two(name: str) -> Any:
+            match name:
+                case 'taxon_id':
+                    return '9631'
+                case 'scientific_name':
+                    return 'Vulpes velox'
+        mock_two.get_field_by_name.side_effect = __get_field_by_name_two
+        type(mock_two).species = PropertyMock(return_value=DummyScientificName('Vulpes velox'))
+        type(mock_two).genus = PropertyMock(return_value=DummyScientificName('Vulpes'))
+        type(mock_two).family = PropertyMock(return_value=DummyScientificName('Canidae'))
+        type(mock_two).superfamily = PropertyMock(return_value=DummyScientificName(None))
+        type(mock_two).phylum = PropertyMock(return_value=DummyScientificName('Chordata'))
+        type(mock_two).kingdom = PropertyMock(return_value=DummyScientificName('Metazoa'))
+        type(mock_two).superkingdom = PropertyMock(return_value=DummyScientificName(None))
+        type(mock_two).domain = PropertyMock(return_value=DummyScientificName('Eukaryota'))
+
+        validator = TaxonMatchesGoatValidator()
+
+        # consume the `Iterable`
+        list(
+            validator.validate(iter([mock_one, mock_two]))
+        )
+
+        assert len(validator.results) == 0
+
     def test_validator_cached(
         self,
         mock_objs: Iterable[DataObject]
@@ -19,7 +82,11 @@ class TestTaxonMatchesGoatValidator:
         validator used for the second data object is the same as the one used for the first.
         """
         class DummyScientificName:
+            """
+            Used to mock the scientific_name field for related data objects to the returned taxon
+            """
             __slots__ = ['scientific_name']
+            scientific_name: str
 
             def __init__(self, scientific_name) -> None:
                 self.scientific_name = scientific_name
