4f6217790c35316ea777a6d3041a1ced80af388e
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-29 11:08:25 +0000

    Convert test_taxon_rank_does_not_match

--- test/unit/validators/test_taxon_matches_goat.py
+++ test/unit/validators/test_taxon_matches_goat.py
@@ -271,51 +271,53 @@ class TestTaxonMatchesGoatValidator:
         # because superfamily isn't set in the config
         assert len(validator.results) == 0
 
-    # def test_taxon_rank_does_not_match(
-    #     self,
-    #     mock_objs: Iterable[DataObject]
-    # ) -> None:
-    #     """
-    #     Ensures that a warning will be raised if a taxon rank has a value different to in GoaT
-    #     """
-    #     class DummyScientificName:
-    #         """
-    #         Used to mock the scientific_name field for related data objects to the returned taxon
-    #         """
-    #         __slots__ = ['scientific_name']
-    #         scientific_name: str
+    def test_taxon_rank_does_not_match(
+        self,
+        mock_objs: Iterable[DataObject]
+    ) -> None:
+        """
+        Ensures that a warning will be raised if a taxon rank has a value different to in GoaT
+        """
+        # The provided mock objects are not appropriate for this test, so we set up new ones
+        del mock_objs
+        mock_one = create_autospec(DataObject)
+        mock_one.id = 'a'
 
-    #         def __init__(self, scientific_name) -> None:
-    #             self.scientific_name = scientific_name
+        def __get_field_by_name_one(name: str) -> Any:
+            match name:
+                case 'TAXON_ID':
+                    return '6344'
+                case 'SCIENTIFIC_NAME':
+                    return 'Arenicola marina'
+                case 'GENUS':
+                    return 'INVALID'
+                case 'FAMILY':
+                    return 'Arenicolidae'
+                case 'SUPERFAMILY':
+                    return None
+                case 'PHYLUM':
+                    return 'Annelida'
+                case 'KINGDOM':
+                    return 'Metazoa'
+                case 'SUPERKINGDOM':
+                    return None
+                case 'DOMAIN':
+                    return 'Eukaryota'
+        mock_one.get_field_by_name.side_effect = __get_field_by_name_one
 
-    #     # The provided mock objects are not appropriate for this test, so we set up new ones
-    #     del mock_objs
-    #     mock_one = create_autospec(DataObject)
-    #     mock_one.id = 'a'
+        config = TaxonMatchesGoatValidator.Config(
+            species_field='SCIENTIFIC_NAME',
+            genus_field='GENUS',
+            family_field='FAMILY',
+        )
 
-    #     def __get_field_by_name_one(name: str) -> Any:
-    #         match name:
-    #             case 'taxon_id':
-    #                 return '6344'
-    #             case 'scientific_name':
-    #                 return 'Arenicola marina'
-    #     mock_one.get_field_by_name.side_effect = __get_field_by_name_one
-    #     type(mock_one).species = PropertyMock(return_value=DummyScientificName('Arenicola marina'))
-    #     type(mock_one).genus = PropertyMock(return_value=DummyScientificName('Arenicola'))
-    #     type(mock_one).family = PropertyMock(return_value=DummyScientificName('Arenicolidae'))
-    #     type(mock_one).superfamily = PropertyMock(return_value=None)
-    #     type(mock_one).phylum = PropertyMock(return_value=DummyScientificName('Annelida'))
-    #     type(mock_one).kingdom = PropertyMock(return_value=DummyScientificName('Metazoa'))
-    #     type(mock_one).superkingdom = PropertyMock(return_value=None)
-    #     type(mock_one).domain = PropertyMock(return_value=DummyScientificName('INVALID'))
+        validator = TaxonMatchesGoatValidator(config)
 
-    #     validator = TaxonMatchesGoatValidator()
+        # consume the `Iterable`
+        list(
+            validator.validate(iter([mock_one]))
+        )
 
-    #     # consume the `Iterable`
-    #     list(
-    #         validator.validate(iter([mock_one]))
-    #     )
-
-    #     # Expect there to be a warning for the incorrect domain
-    #     assert len(validator.warnings) == 1
-    #     assert len(validator.errors) == 0
+        # Expect there to be a warning for the incorrect genus
+        assert len(validator.warnings) == 1
+        assert len(validator.errors) == 0
