3712b96b44cc3cd6eeb666e51a8348d7077aea79
Thomas Assheton <ta21@sanger.ac.uk>
2026-01-29 11:04:09 +0000

    Convert test_invalid_taxon_id

--- test/unit/validators/test_taxon_matches_goat.py
+++ test/unit/validators/test_taxon_matches_goat.py
@@ -168,51 +168,57 @@ class TestTaxonMatchesGoatValidator:
         # Check they're the same object
         assert taxon_object_one == taxon_object_two
 
-    # def test_invalid_taxon_id(
-    #     self,
-    #     mock_objs: Iterable[DataObject]
-    # ) -> None:
-    #     class DummyScientificName:
-    #         """
-    #         Used to mock the scientific_name field for related data objects to the returned taxon
-    #         """
-    #         __slots__ = ['scientific_name']
-    #         scientific_name: str
+    def test_invalid_taxon_id(
+        self,
+        mock_objs: Iterable[DataObject]
+    ) -> None:
+        """
+        Ensures that attempting to validate a data object with an invalid Taxon ID will result
+        in an error
+        """
+        # The provided mock objects are not appropriate for this test, so we set up new ones
+        del mock_objs
+        mock_one = create_autospec(DataObject)
+        mock_one.id = 'a'
 
-    #         def __init__(self, scientific_name) -> None:
-    #             self.scientific_name = scientific_name
+        def __get_field_by_name_one(name: str) -> Any:
+            match name:
+                case 'TAXON_ID':
+                    return '-1'  # invalid taxon id
+                case 'SCIENTIFIC_NAME':
+                    return 'Arenicola marina'
+                case 'GENUS':
+                    return 'Arenicola'
+                case 'FAMILY':
+                    return 'Arenicolidae'
+                case 'SUPERFAMILY':
+                    return None
+                case 'PHYLUM':
+                    return 'Annelida'
+                case 'KINGDOM':
+                    return 'Metazoa'
+                case 'SUPERKINGDOM':
+                    return None
+                case 'DOMAIN':
+                    return 'Eukaryota'
+        mock_one.get_field_by_name.side_effect = __get_field_by_name_one
 
-    #     # The provided mock objects are not appropriate for this test, so we set up new ones
-    #     del mock_objs
-    #     mock_one = create_autospec(DataObject)
-    #     mock_one.id = 'a'
+        config = TaxonMatchesGoatValidator.Config(
+            species_field='SCIENTIFIC_NAME',
+            genus_field='GENUS',
+            family_field='FAMILY',
+        )
 
-    #     def __get_field_by_name_one(name: str) -> Any:
-    #         match name:
-    #             case 'taxon_id':
-    #                 return '-1'  # invalid taxon_id
-    #             case 'scientific_name':
-    #                 return 'Arenicola marina'
-    #     mock_one.get_field_by_name.side_effect = __get_field_by_name_one
-    #     type(mock_one).species = PropertyMock(return_value=DummyScientificName('Arenicola marina'))
-    #     type(mock_one).genus = PropertyMock(return_value=DummyScientificName('Arenicola'))
-    #     type(mock_one).family = PropertyMock(return_value=DummyScientificName('Arenicolidae'))
-    #     type(mock_one).superfamily = PropertyMock(return_value=None)
-    #     type(mock_one).phylum = PropertyMock(return_value=DummyScientificName('Annelida'))
-    #     type(mock_one).kingdom = PropertyMock(return_value=DummyScientificName('Metazoa'))
-    #     type(mock_one).superkingdom = PropertyMock(return_value=None)
-    #     type(mock_one).domain = PropertyMock(return_value=DummyScientificName('Eukaryota'))
+        validator = TaxonMatchesGoatValidator(config)
 
-    #     validator = TaxonMatchesGoatValidator()
+        # consume the `Iterable`
+        list(
+            validator.validate(iter([mock_one]))
+        )
 
-    #     # consume the `Iterable`
-    #     list(
-    #         validator.validate(iter([mock_one]))
-    #     )
-
-    #     # Expect there to be an error for the invalid taxon_id
-    #     assert len(validator.errors) == 1
-    #     assert len(validator.warnings) == 0
+        # Expect there to be an error for the invalid taxon_id
+        assert len(validator.errors) == 1
+        assert len(validator.warnings) == 0
 
     # def test_taxon_rank_not_in_goat(
     #     self,
