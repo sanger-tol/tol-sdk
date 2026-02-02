9faa3b6b22b6ed9a7939ec7985635c0c944a4d5c
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-02 10:48:51 +0000

    nevermind

--- src/tol/elastic_new/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-FileCopyrightText: 2026 Genome Research Ltd.
-#
-# SPDX-License-Identifier: MIT
-
-from .elastic_datasource import ElasticDataSource  # noqa
-from .factory import create_elastic_datasource  # noqa
