42538b4ae8a603cc78cf0a86975a1c5330729ceb
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-02 10:57:10 +0000

    Create core DataSourceFilterConverter

---
+++ src/tol/core/datasource_filter_converter.py
@@ -0,0 +1,21 @@
+# SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from abc import ABC, abstractmethod
+
+from . import DataSourceFilter
+
+
+class DataSourceFilterConverter(ABC):
+    """
+    Converts instances of `DataSourceFilter` to a
+    valid filter for the remote source.
+    """
+
+    @abstractmethod
+    def convert(self, filter_: DataSourceFilter) -> dict | str:
+        """
+        Emit a filter valid for the remote source from a `DataSourceFilter` instance
+        """
+        pass
