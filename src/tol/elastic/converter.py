9eec8bdda5de96b8e2947383aac8cbe2994b4983
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-03 10:59:46 +0000

    Update converter to use DataSourceParser

--- src/tol/elastic/converter.py
+++ src/tol/elastic/converter.py
@@ -2,8 +2,17 @@
 #
 # SPDX-License-Identifier: MIT
 
+from ..core import DataSourceParser
+
+from .parser import ElasticApiResource
+
+
 class ElasticConverter:
     """
     Converts from Elastic API transfers to instances of `DataObject`
     """
-    pass
+    __slots__ = ['__parser']
+    __parser: DataSourceParser[ElasticApiResource]
+
+    def __init__(self, parser: DataSourceParser[ElasticApiResource]) -> None:
+        self.__parser = parser
