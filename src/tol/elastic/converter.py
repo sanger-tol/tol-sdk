8a0b64817260d05b1a20f2f1e83f32da62044c8a
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-03 09:50:36 +0000

    Create ElasticConverter

---
+++ src/tol/elastic/converter.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+class ElasticConverter:
+    """
+    Converts from Elastic API transfers to instances of `DataObject`
+    """
+    pass
