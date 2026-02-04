8291851340e2da4293d65ec268778134f3035b4c
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-04 14:04:42 +0000

    Complete ElasticApiConverterFactoryManager

--- src/tol/elastic/factory.py
+++ src/tol/elastic/factory.py
@@ -46,12 +46,18 @@ class _ElasticDSDict(Mapping):
         return len(self.__data_source.supported_types)
 
 
-class _ConverterFactory:
+class ElasticApiConverterFactoryManager:
     """
-    Manages the instantiation of `ElasticApiConverter`
+    Wow, what a class name.
+    The purpose of this class is to provide `elastic_converter_factory`, a function passed to
+    `ElasticDataSource` that manages the instantiation of an `ElasticApiConverter`.
+    The reason we need this manager class around that function is because `ElasticApiConverter`
+    can't be instantiated until we already have an instance of `ElasticDataSource`. So this class
+    is instantiated, then its `elastic_converter_factory` method is provided to `ElasticDataSource`
+    when that is instantiated, then the `data_source` property is set, then we're done.
     """
     __slots__ = ['__data_source']
-    __data_source: DataSource | None
+    __data_source: ElasticDataSource | None
 
     def __init__(self) -> None:
         # The converter factory is instantisated before the data source, so this must be assigned
@@ -59,22 +65,25 @@ class _ConverterFactory:
         self.__data_source = None
 
     @property
-    def data_source(self) -> DataSource | None:
+    def data_source(self) -> ElasticDataSource | None:
         """
         Fetch the data source, or `None` if it hasn't been instantiated yet
         """
         return self.__data_source
 
     @data_source.setter
-    def data_source(self, data_source: DataSource) -> None:
+    def data_source(self, data_source: ElasticDataSource) -> None:
         self.__data_source = data_source
 
     def elastic_converter_factory(self) -> ElasticApiConverter:
-        # TODO CHECK NOT NONE OR USE DICT FROM BEFORE
+        if self.data_source is None:
+            raise Exception(
+                "TOL INTERNAL ERROR: elastic_converter_factory called before the data_source "
+                "was assigned in _ConverterFactoryManager"
+            )
+
         parser = DefaultParser(self.data_source)
         return ElasticApiConverter(parser)
-
-    # TODO ds dict
 
 
 class _FilterFactory:
@@ -103,7 +112,7 @@ def create_elastic_datasource(
     using the configuration required for the client
     """
     client_factory = None  # TODO
-    manager = _ConverterFactory()
+    manager = ElasticApiConverterFactoryManager()
     elastic_ds = ElasticDataSource(
         config,
         client_factory,
