85f989e99bdf2cd6531a8eaed7f21daaec08e2b8
Thomas Assheton <ta21@sanger.ac.uk>
2026-02-04 12:32:56 +0000

    uncomment

--- src/tol/elastic/factory.py
+++ src/tol/elastic/factory.py
@@ -16,37 +16,35 @@ from ..core import (
 )
 from ..core.relationship import RelationshipConfig
 
-# if TYPE_CHECKING:
-#     from . import ElasticDataSource
 from . import ElasticDataSource
 
 
-# class _ElasticDSDict(Mapping):
-#     """
-#     A wrapper around an `ElasticDataSource` that only
-#     lets you access it if you provide on object type
-#     supported by the data source. This class masquerades as a dictionary
-#     (and is considered as a `dict` type in the Parser),
-#     so you go about this via a key access using square brackets.
-#     I'm not quite sure why it's done like this, but as the code is
-#     set up right now all data sources need to use this pattern.
-#     """
-#     __slots__ = ['__data_source']
-#     __data_source: ElasticDataSource
+class _ElasticDSDict(Mapping):
+    """
+    A wrapper around an `ElasticDataSource` that only
+    lets you access it if you provide on object type
+    supported by the data source. This class masquerades as a dictionary
+    (and is considered as a `dict` type in the Parser),
+    so you go about this via a key access using square brackets.
+    I'm not quite sure why it's done like this, but as the code is
+    set up right now all data sources need to use this pattern.
+    """
+    __slots__ = ['__data_source']
+    __data_source: ElasticDataSource
 
-#     def __init__(self, data_source: ElasticDataSource) -> None:
-#         self.__data_source = data_source
+    def __init__(self, data_source: ElasticDataSource) -> None:
+        self.__data_source = data_source
     
-#     def __getitem__(self, key: str) -> ElasticDataSource:
-#         if key not in self.__data_source.supported_types:
-#             raise KeyError()
-#         return self.__data_source
+    def __getitem__(self, key: str) -> ElasticDataSource:
+        if key not in self.__data_source.supported_types:
+            raise KeyError()
+        return self.__data_source
 
-#     def __iter__(self) -> Iterator:
-#         return iter(self.__data_source.supported_types)
+    def __iter__(self) -> Iterator:
+        return iter(self.__data_source.supported_types)
 
-#     def __len__(self) -> int:
-#         return len(self.__data_source.supported_types)
+    def __len__(self) -> int:
+        return len(self.__data_source.supported_types)
 
 
 class _ConverterFactory:
