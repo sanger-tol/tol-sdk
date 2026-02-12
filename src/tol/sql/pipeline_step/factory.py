dbaf01bbd0931eafd797624bfbabbe4501baf2e6
Luke Wilson <lw23@sanger.ac.uk>
2026-02-12 09:29:31 +0000

    lint

--- src/tol/sql/pipeline_step/factory.py
+++ src/tol/sql/pipeline_step/factory.py
@@ -191,7 +191,7 @@ def create_pipeline_step_models(
         rejection_reason: Mapped[str] = mapped_column(
             nullable=True
         )
-        
+
         hidden: Mapped[str] = mapped_column(
             nullable=False,
             default=False
