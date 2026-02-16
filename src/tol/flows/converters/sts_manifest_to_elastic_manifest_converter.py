cd2bedba2084aa69e56f6a2153fdfb1c1b935ab7
Logan Howat <lh31@sanger.ac.uk>
2026-02-16 11:25:38 +0000

    TOLP-9499: Removing prefixes from Elastic converters

--- src/tol/flows/converters/sts_manifest_to_elastic_manifest_converter.py
+++ src/tol/flows/converters/sts_manifest_to_elastic_manifest_converter.py
@@ -13,16 +13,16 @@ from ...core import (
 class StsManifestToElasticManifestConverter(
         DataObjectToDataObjectOrUpdateConverter):
     def convert(self, data_object: DataObject) -> Iterable[DataObject]:
-        attributes = {f'sts_{k}': v for k, v in data_object.attributes.items()}
-        attributes['sts_project'] = data_object.project.id \
+        attributes = data_object.attributes.copy()
+        attributes['project'] = data_object.project.id \
             if data_object.project is not None else None
-        attributes['sts_programme'] = data_object.project.programme \
+        attributes['programme'] = data_object.project.programme \
             if data_object.project is not None else None
-        attributes['sts_status'] = data_object.manifest_status.status \
+        attributes['status'] = data_object.manifest_status.status \
             if data_object.manifest_status is not None else None
-        attributes['sts_shipment_status'] = data_object.shipment_status.status \
+        attributes['shipment_status'] = data_object.shipment_status.status \
             if data_object.shipment_status is not None else None
-        attributes['sts_compliance_status'] = data_object.compliance_status.status \
+        attributes['compliance_status'] = data_object.compliance_status.status \
             if data_object.compliance_status is not None else None
 
         # wildlife_compliance_processors
@@ -30,17 +30,18 @@ class StsManifestToElasticManifestConverter(
         if data_object.wildlife_compliance_processors is not None:
             for wcp in data_object.wildlife_compliance_processors:
                 wcp_attribute.append(wcp.user.fullname)
-        attributes['sts_wildlife_compliance_processors'] = wcp_attribute
+        attributes['wildlife_compliance_processors'] = wcp_attribute
 
         ret = self._data_object_factory(
             'manifest',
             data_object.id,
-            attributes=attributes
+            attributes=attributes,
+            to_one={
+                'sampleset': self._data_object_factory(
+                    'sampleset',
+                    data_object.sampleset.id
+                ) if data_object.sampleset is not None else None,
+            }
         )
-        if data_object.sampleset is not None:
-            ret.sts_sampleset = self._data_object_factory(
-                'sampleset',
-                data_object.sampleset.id
-            )
 
         yield ret
