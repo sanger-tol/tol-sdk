10e894a3ef4f4bf9eb48d8618fa311c25a820e8e
Luke Wilson <lw23@sanger.ac.uk>
2026-02-24 15:59:56 +0000

    TOLP-9440 - Validation Status System

--- src/tol/api_base/pipeline_steps.py
+++ src/tol/api_base/pipeline_steps.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import os
 import typing
 from dataclasses import dataclass
+from datetime import datetime
 from typing import Any, List
 
 from flask import Blueprint, request
@@ -17,7 +18,8 @@ from .misc import (
     default_ctx_getter
 )
 from ..core import (
-    DataSourceError
+    DataSourceError,
+    DataSourceFilter
 )
 
 if typing.TYPE_CHECKING:
@@ -32,6 +34,7 @@ class UploadData:
     spreadsheet_config: str
     pipeline_id: int
     destination: str
+    upload_name: str
     user_id: int
     dry_run: bool
 
@@ -41,6 +44,7 @@ REQUIRED_FIELDS: List = [
     's3_filename',
     'spreadsheet_config',
     'pipeline_id',
+    'upload_name',
     'destination',
     'dry_run'
 ]
@@ -108,9 +112,11 @@ def pipeline_steps_blueprint(
                 's3_bucket': upload_data.s3_bucket,
                 's3_filename': upload_data.s3_filename,
                 'spreadsheet_config': upload_data.spreadsheet_config,
+                'upload_name': upload_data.upload_name,
                 'pipeline_id': upload_data.pipeline_id,
                 'destination': upload_data.destination,
-                'user_id': upload_data.user_id
+                'user_id': upload_data.user_id,
+                'validation_status': 'validation_in_progress'
             },
         )
 
@@ -186,15 +192,100 @@ def pipeline_steps_blueprint(
             [upload]
         )
 
-    @bp.post('')
-    def run_pipeline_steps() -> tuple[dict[str, Any], int]:
-
-        ctx = ctx_getter()
+    def __check_auth(ctx: Any = None) -> None:
         if role is not None:
             if not ctx or not ctx.authenticated:
                 raise ForbiddenError()
             if role not in ctx.roles:
                 raise ForbiddenError()
+
+    @bp.route('/revalidate', methods=['POST'])
+    def revalidate_upload() -> tuple[dict[str, str], int]:
+
+        allowed_validation_statuses = ['validation_system_error', 'validation_timeout']
+
+        ctx = ctx_getter()
+        __check_auth(ctx)
+
+        body: dict[str, Any] = request.json.get('data', {})
+        __check_required_fields(body, required_fields=['upload_ids'])
+
+        upload_ids: list[str] = body['upload_ids']
+
+        f = DataSourceFilter()
+        f.and_ = {
+            'id': {'in_list': {'value': upload_ids}}}
+
+        existing_uploads = sql_ds.get_list(
+            'upload',
+            object_filters=f
+        )
+
+        if not existing_uploads:
+            raise DataSourceError(
+                'Not Found',
+                'No uploads found for the provided upload IDs.',
+                404
+            )
+
+        existing_by_id = {upload.id: upload for upload in existing_uploads}
+
+        for upload in existing_uploads:
+            if upload.validation_status not in allowed_validation_statuses:
+                raise DataSourceError(
+                    'Invalid Request',
+                    f'Upload with ID {upload.id} cannot be revalidated as its validation \
+                    status is not in {allowed_validation_statuses} status.',
+                    400
+                )
+
+        uploads = [sql_ds.data_object_factory(
+            'upload',
+            upload_id,
+            attributes={
+                'validation_status': 'validation_in_progress',
+                'rejection_reason': None,
+                'date_started': datetime.now(),
+                'failure_message': None,
+                'validation_results': [],
+                'completed': False,
+                'flow_run_id': None
+            },
+        ) for upload_id in upload_ids]
+
+        if not uploads:
+            raise DataSourceError(
+                'Not Found',
+                'No uploads found for the provided upload IDs.',
+                404
+            )
+
+        sql_ds.upsert(
+            'upload',
+            uploads
+        )
+
+        flow_run_ids = [__insert_flow_run(
+            upload_id=upload_id,
+            pipeline_id=existing_by_id[upload_id].pipeline_id,
+            s3_filename=existing_by_id[upload_id].s3_filename,
+            dry_run=False
+        ) for upload_id in upload_ids]
+
+        for upload_id, flow_run_id in zip(upload_ids, flow_run_ids):
+            __upsert_flow_run_id(
+                upload_id=upload_id,
+                flow_run_id=flow_run_id
+            )
+
+        return {'success': True, 'upload_and_flow_run_ids': list(zip(upload_ids,
+                                                                     flow_run_ids))}, 200
+
+    @bp.post('')
+    def run_pipeline_steps() -> tuple[dict[str, Any], int]:
+
+        ctx = ctx_getter()
+        __check_auth(ctx)
 
         user_id = ctx.user_id if ctx and ctx.authenticated else None
 
@@ -212,6 +303,7 @@ def pipeline_steps_blueprint(
             spreadsheet_config=body['spreadsheet_config'],
             pipeline_id=pipeline_id,
             destination=body['destination'],
+            upload_name=body['upload_name'],
             user_id=user_id,
             dry_run=body['dry_run']
         )
