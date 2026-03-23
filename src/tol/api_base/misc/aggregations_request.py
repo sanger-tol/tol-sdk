55d5a449bfc009e67d22aaf4a73b6df8e589149e
Thomas Assheton <ta21@sanger.ac.uk>
2026-03-23 11:31:14 +0000

    Remove old parameters and body code
    Account for MultiDict

--- src/tol/api_base/misc/aggregations_request.py
+++ src/tol/api_base/misc/aggregations_request.py
@@ -2,8 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-from ...api_client.exception import BadPostJsonError
-from .filter_utils import FilterUtils
+from werkzeug.datastructures import MultiDict
 
 
 class AggregationParameters:
@@ -13,21 +12,10 @@ class AggregationParameters:
     """
 
     __slots__ = ['__request_args']
-    __request_args: dict[str, str]
+    __request_args: dict
 
-    def __init__(self, request_args: dict[str, str]) -> None:
-        self.__request_args = request_args
-
-    @property
-    def filter(self) -> str | None:  # noqa A003
-        """
-        The optional filter JSON string.
-        """
-        filter_string = self.__request_args.get('filter')
-        if filter_string is None:
-            return None
-
-        return FilterUtils.parse_to_datasource_filter('filter', filter_string)
+    def __init__(self, request_args: MultiDict) -> None:
+        self.__request_args = request_args.to_dict()
 
 
 class AggregationBody:
@@ -39,18 +27,5 @@ class AggregationBody:
     __slots__ = ['__body_dict']
     __body_dict: dict
 
-    def __init__(self, body_dict: dict) -> None:
-        self.__body_dict = body_dict
-
-    @property
-    def aggs(self) -> dict:
-        """
-        The optional aggregations dict.
-        """
-        body_dict = self.__body_dict.get('aggs')
-        if body_dict is None:
-            raise BadPostJsonError(
-                'aggs',
-                message='"aggs" must be given'
-            )
-        return body_dict
+    def __init__(self, body_dict: MultiDict) -> None:
+        self.__body_dict = body_dict.to_dict()
