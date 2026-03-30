0430373cd157d0445df387967e75fc1cc8743b05
Thomas Assheton <ta21@sanger.ac.uk>
2026-03-30 10:15:57 +0100

    Add individual aggregation methods to operator

--- src/tol/core/operator/aggregator.py
+++ src/tol/core/operator/aggregator.py
@@ -5,7 +5,7 @@
 from __future__ import annotations
 
 import typing
-from abc import ABC
+from abc import ABC, abstractmethod
 
 from ._filterable import _Filterable
 
@@ -29,3 +29,36 @@ class Aggregator(_Filterable, ABC):
         maximum_categories: int | None = None,
     ) -> dict:
         return {'hey': 'there'}
+    
+    @abstractmethod
+    def get_date_bar_chart_aggregation(
+        self,
+        x_axis: str,
+        date_interval: str,
+        break_down_by: str | None,
+        stat: str | None,
+        stat_field: str | None,
+        cumulative: bool,
+    ) -> dict:
+        raise NotImplementedError
+
+    @abstractmethod
+    def get_categorical_bar_chart_aggregation(
+        self,
+        x_axis: str,
+        break_down_by: str | None,
+        stat: str | None,
+        stat_field: str | None,
+        maximum_categories: int,
+    ) -> dict:
+        raise NotImplementedError
+
+    def get_scatter_plot_aggregation(
+        self,
+        x_axis: str,
+        y_axis: str,
+        break_down_by: str | None,
+    ) -> dict:
+        # TODO: This is concrete and should be implemented here
+        raise NotImplementedError
+
