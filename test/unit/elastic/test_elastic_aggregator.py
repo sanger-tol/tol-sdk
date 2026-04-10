0f06a362eaf060dd7fa877ad9b567fcd8dee7c93
Thomas Assheton <ta21@sanger.ac.uk>
2026-04-10 10:49:03 +0100

    Add placeholders for the scatter aggregation tests

--- test/unit/elastic/test_elastic_aggregator.py
+++ test/unit/elastic/test_elastic_aggregator.py
@@ -6,6 +6,14 @@ from tol.elastic import ElasticDataSource
 
 
 class TestElasticAggregator:
+    def test_scatter_aggregation(self, mock_elastic_data_source: ElasticDataSource):
+        # TODO Once the scatter aggregation has been implemented
+        pass
+
+    def test_scatter_aggregation_cumulative(self, mock_elastic_data_source: ElasticDataSource):
+        # TODO Once the scatter aggregation has been implemented
+        pass
+
     def test_invalid_combination(self, mock_elastic_data_source: ElasticDataSource):
         aggregation_result = mock_elastic_data_source.get_aggregations(
             'obj_type',
