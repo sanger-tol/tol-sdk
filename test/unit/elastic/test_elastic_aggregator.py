d649839bc6537acbaaf4157af3333931b32effcf
Thomas Assheton <ta21@sanger.ac.uk>
2026-04-10 11:22:07 +0100

    Remove duplicate test

--- test/unit/elastic/test_elastic_aggregator.py
+++ test/unit/elastic/test_elastic_aggregator.py
@@ -13,12 +13,3 @@ class TestElasticAggregator:
     def test_scatter_aggregation_cumulative(self, mock_elastic_data_source: ElasticDataSource):
         # TODO Once the scatter aggregation has been implemented
         pass
-
-    def test_invalid_combination(self, mock_elastic_data_source: ElasticDataSource):
-        aggregation_result = mock_elastic_data_source.get_aggregations(
-            'obj_type',
-            None,
-            x_axis='field3',
-            maximum_categories=8,
-        )
-        assert aggregation_result is None
