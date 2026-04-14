99c2afd43fe210e8b4ec0aa2301578734879910a
Andrew Varley <av10@sanger.ac.uk>
2026-04-14 08:57:33 +0100

    TOLP-9692 Moved standard pipeline functions into new PipelineUtils class from flows

---
+++ src/tol/pipeline/__init__.py
@@ -0,0 +1,7 @@
+# SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from .error import PipelineError  # noqa: F401
+from .types import *  # noqa: F401, F403
+from .utils import PipelineUtils  # noqa: F401
---
+++ test/unit/pipeline/test_pipeline_utils.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from tol.pipeline import (
+    PipelineUtils,
+    StepConfig,
+)
+
+
+class TestPipelineUtils:
+    def test_import_class(self) -> None:
+        """
+        `import_class` imports the class specified by the module and factory
+        name.
+        """
+
+        # import a class we know exists
+        cls = PipelineUtils.import_class(
+            'tol.validators',
+            'AllowedKeysValidator',
+        )
+
+        # it's the class we expect
+        assert cls.__name__ == 'AllowedKeysValidator'
+
+    def test_instantiate_step(self) -> None:
+        """
+        `instantiate_step` instantiates a step class specified by the module and
+        factory name in the config, passing the config details to the class.
+        """
+
+        # define a config for a step we know exists
+        config = StepConfig(
+            module='tol.validators',
+            class_name='AllowedKeysValidator',
+            is_validator=True,
+            config_details={
+                'allowed_keys': ['a', 'b', 'c'],
+            },
+        )
+
+        # instantiate the step
+        step = PipelineUtils.instantiate_step(
+            config,
+            data_object_factory=None,
+        )
+
+        # it's an instance of the class we expect
+        assert step.__class__.__name__ == 'AllowedKeysValidator'
+        # it has the config we expect
+        assert step._AllowedKeysValidator__config.allowed_keys == ['a', 'b', 'c']
