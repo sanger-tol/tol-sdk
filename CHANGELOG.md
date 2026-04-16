ff10664802bfdfba7b898d1fbc9721739fa98f0d
Andrew Varley <av10@sanger.ac.uk>
2026-04-16 09:04:53 +0100

    TOLP-9380 Added MultipleConverter

---
+++ CHANGELOG.md
@@ -0,0 +1,14 @@
+<!--
+SPDX-FileCopyrightText: 2026 Genome Research Ltd.
+
+SPDX-License-Identifier: MIT
+-->
+
+# Changelog
+
+## tol-sdk `2.0.10`
+15-04-2026
+- Added MultipleConverter to chain multiple converters in a pipeline step
+- Added PipelineUtils to better reuse standard converter/validator instantiation
+- Added a Config to Treeofsex converter
+- Added changelog file to track tol-sdk version updates and changes
--- test/unit/pipeline/test_pipeline_utils.py
+++ test/unit/pipeline/test_pipeline_utils.py
@@ -24,7 +24,7 @@ class TestPipelineUtils:
         # it's the class we expect
         assert cls.__name__ == 'AllowedKeysValidator'
 
-    def test_instantiate_step(self) -> None:
+    def test_instantiate_step_validator(self) -> None:
         """
         `instantiate_step` instantiates a step class specified by the module and
         factory name in the config, passing the config details to the class.
@@ -50,3 +50,32 @@ class TestPipelineUtils:
         assert step.__class__.__name__ == 'AllowedKeysValidator'
         # it has the config we expect
         assert step._AllowedKeysValidator__config.allowed_keys == ['a', 'b', 'c']
+
+    def test_instantiate_step_converter(self) -> None:
+        """
+        `instantiate_step` instantiates a step class specified by the module and
+        factory name in the config, passing the config details to the class.
+        """
+
+        # define a config for a step we know exists
+        config = StepConfig(
+            module='tol.flows.converters',
+            class_name='PrefixFieldConverter',
+            is_validator=False,
+            config_details={
+                'field_name': 'field1',
+                'prefix': 'pre',
+            },
+        )
+
+        # instantiate the step
+        step = PipelineUtils.instantiate_step(
+            config,
+            data_object_factory=None,
+        )
+
+        # it's an instance of the class we expect
+        assert step.__class__.__name__ == 'PrefixFieldConverter'
+        # it has the config we expect
+        assert step._PrefixFieldConverter__config.field_name == 'field1'
+        assert step._PrefixFieldConverter__config.prefix == 'pre'
