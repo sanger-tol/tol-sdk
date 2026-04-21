79eb270152f25ea9e7d97b7bc7577d00337f5039
Andrew Varley <av10@sanger.ac.uk>
2026-04-21 13:39:34 +0100

    Lint

--- src/tol/flows/converters/elastic_object_to_portaldb_object_converter.py
+++ src/tol/flows/converters/elastic_object_to_portaldb_object_converter.py
@@ -2,7 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from typing import (
     Iterable
 )
@@ -18,38 +18,37 @@ class ElasticObjectToPortaldbObjectConverter(
 
     @dataclass(slots=True, frozen=True, kw_only=True)
     class Config:
-        pass
+        fields: dict = field(default_factory=dict)
+        destination_object_type: str = 'tolid_event'
+        id_field: str = 'id'
+        incremental: bool = False
 
     __slots__ = ['__config']
     __config: Config
 
-    def __init__(self, data_object_factory, config: Config, fields: dict = {},
-                 destination_object_type: str = 'tolid_event',
-                 id_field: str = 'id', incremental: bool = False):
+    def __init__(self, data_object_factory, config: Config):
         super().__init__(data_object_factory)
         self.__config = config
-        self.__fields = fields
-        self.__destination_object_type = destination_object_type
-        self.__id_field = id_field
-        self.__incremental = incremental
 
     def convert(self, data_object: DataObject) -> Iterable[DataObject]:
         if data_object is not None:
 
             ret = self._data_object_factory(
-                self.__destination_object_type,
-                data_object.get_field_by_name(self.__id_field),
-                attributes=self.__fields
+                self.__config.destination_object_type,
+                data_object.get_field_by_name(self.__config.id_field),
+                attributes=self.__config.fields
             )
 
-            if self.__incremental and self.__destination_object_type == 'tolid_event':
+            if self.__config.incremental \
+                    and self.__config.destination_object_type == 'tolid_event':
                 obj = ret._host.get_one(
                     'tolid_event',
-                    data_object.get_field_by_name(self.__id_field)
+                    data_object.get_field_by_name(self.__config.id_field)
                 )
                 count_field_value = obj.tol_tum_action_count if obj else 0
                 new_count = count_field_value + 1 if count_field_value else 1
-                ret.attributes['tol_tum_action_count'] = new_count if self.__incremental else None
+                ret.attributes['tol_tum_action_count'] = \
+                    new_count if self.__config.incremental else None
 
             yield ret
         else:
