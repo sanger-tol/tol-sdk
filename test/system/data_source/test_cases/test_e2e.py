5dcd9b746bd7b96c8a3191f6f2b4618c100fe07c
James Gilbert <jgrg@sanger.ac.uk>
2026-04-27 12:55:12 +0100

    Try combined and_ filter in the hope it will trigger a table analyze

--- test/system/data_source/test_cases/test_e2e.py
+++ test/system/data_source/test_cases/test_e2e.py
@@ -832,25 +832,22 @@ class TestEndToEnd:
         # SQL / Postgres needs to have analysed the table for the stats to be
         # built, which we trigger by requesting matches to the `str_column`
         # and `int_column`:
-        found = []
-        for and_clause in (
-            {
-                'str_column': {
-                    'eq': {'value': 'B'},
-                }
-            },
-            {
-                'int_column': {
-                    'eq': {'value': 5},
-                }
-            },
-        ):
-            found.extend(
-                data_source.get_list(
-                    'root',
-                    DataSourceFilter(and_=and_clause),
-                )
+        found = list(
+            data_source.get_list(
+                'root',
+                DataSourceFilter(
+                    and_={
+                        'str_column': {
+                            'eq': {'value': 'B'},
+                        },
+                        'int_column': {
+                            'eq': {'value': 5},
+                        },
+                    }
+                ),
             )
+        )
+        assert len(found) == 67
 
         stats = data_source.get_stats(
             'root',
