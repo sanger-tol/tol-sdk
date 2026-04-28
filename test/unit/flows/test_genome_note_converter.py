072cbe98d1be79a0fb1a045205f5e930918732c5
Shweta Bhagat <sb83@sanger.ac.uk>
2026-04-28 15:08:12 +0100

    tests

--- test/unit/flows/test_genome_note_converter.py
+++ test/unit/flows/test_genome_note_converter.py
@@ -2,95 +2,130 @@
 #
 # SPDX-License-Identifier: MIT
 
-import unittest
-from unittest.mock import MagicMock
+from unittest import TestCase
 
+from tol.core import (
+    DataSource,
+    core_data_object,
+)
 from tol.flows.converters.genome_note_converter import GenomeNoteConverter
 
 
-class TestGenomeNoteConverter(unittest.TestCase):
-    def setUp(self):
-        self.mock_factory = MagicMock()
-        self.mock_data_object = MagicMock()
-        self.mock_data_object.attributes = {
-            'reviewer_reports': 'should be ignored',
-            'publication_metadata': 'should be ignored',
-            'metadata': {
-                'doi': '10.1234/example',
-                'title': 'Genome Note',
-                'authors': [
-                    {'surname': 'Smith', 'given_names': 'John'},
-                    {'surname': 'Doe', 'given_names': 'Jane'},
-                ],
-                'keywords': ['genome', 'note'],
-                'background': 'background text',
-                'bioproject': 'PRJ123',
-            },
-            'methods': {
-                'sample_acquisition': 'method1',
-                'nucleic_acid_extraction': 'method2',
-            },
-            'assembly_stats': {
-                'sequence_report': {
-                    'isolate_info': 'iso',
-                    'assembly_accession': 'GCA_000001',
-                },
-                'analysis': {'busco': {}},
-                'assembly_graphs': {'kmer': {}},
-                'chromosomal_pseudomolecules': ['chr1'],
-                'samples': ['sample1'],
-                'species': ['species1'],
-                'other': {'genome_annotation': 'ann'},
-            },
-            'software_tools': ['tool1'],
-            'references': ['ref1'],
-        }
-        self.converter = GenomeNoteConverter(self.mock_factory, GenomeNoteConverter.Config())
+class _MockDataSource(DataSource):
+    @property
+    def supported_types(self):
+        return ['genome_note']
+
+    @property
+    def attribute_types(self):
+        raise NotImplementedError()
+
+
+class TestGenomeNoteConverter(TestCase):
 
     def test_convert(self):
-        """Test that the converter ignores, renames, and copies attributes correctly."""
-        class DummyObj:
-            def __init__(self, attributes):
-                self.attributes = attributes
-        self.mock_factory.side_effect = lambda attributes: DummyObj(attributes)
+        destination = _MockDataSource(config={})
+        core_data_object(destination)
 
-        result = list(self.converter.convert(self.mock_data_object))
-        self.assertEqual(len(result), 1)
-        attrs = result[0].attributes
+        converter = GenomeNoteConverter(
+            data_object_factory=destination.data_object_factory,
+            config=GenomeNoteConverter.Config(),
+        )
 
-        # Ignored fields should not be present
-        self.assertNotIn('reviewer_reports', attrs)
-        self.assertNotIn('publication_metadata', attrs)
-        # Metadata fields should be present at top level
-        self.assertEqual(attrs['doi'], '10.1234/example')
-        self.assertEqual(attrs['title'], 'Genome Note')
-        self.assertEqual(attrs['keywords'], ['genome', 'note'])
-        self.assertEqual(attrs['background'], 'background text')
-        self.assertEqual(attrs['bioproject'], 'PRJ123')
-        # Authors should be a list of dicts
-        self.assertEqual(attrs['authors'], [
-            {'surname': 'Smith', 'given_names': 'John'},
-            {'surname': 'Doe', 'given_names': 'Jane'},
-        ])
-        # References should be present
-        self.assertEqual(attrs['references'], ['ref1'])
-        # Methods should be renamed and present
-        self.assertEqual(attrs['method_sample_acquisition'], 'method1')
-        self.assertEqual(attrs['method_nucleic_acid_extraction'], 'method2')
-        # Sequence report fields should be at top level
-        self.assertEqual(attrs['isolate_info'], 'iso')
-        self.assertEqual(attrs['assembly_accession'], 'GCA_000001')
-        # Analysis and assembly_graphs should be renamed
-        self.assertIn('assembly_stats_analysis', attrs)
-        self.assertIn('assembly_stats_assembly_graphs', attrs)
-        # Remaining assembly_stats fields
-        self.assertEqual(attrs['chromosomal_pseudomolecules'], ['chr1'])
-        self.assertEqual(attrs['samples'], ['sample1'])
-        self.assertEqual(attrs['species'], ['species1'])
-        self.assertEqual(attrs['other'], {'genome_annotation': 'ann'})
-        # As-is field should be present
-        self.assertEqual(attrs['software_tools'], ['tool1'])
+        CoreDataObject = destination.data_object_factory  # noqa N806
 
+        obj = CoreDataObject(
+            id_='input_id',
+            type_='genome_note',
+            attributes={
+                'reviewer_reports': 'should be ignored',
+                'publication_metadata': 'should be ignored',
+                'metadata': {
+                    'doi': '10.1234/example',
+                    'title': 'Genome Note',
+                    'authors': [
+                        {
+                            'surname': 'Smith',
+                            'given_names': 'John',
+                        },
+                        {
+                            'surname': 'Doe',
+                            'given_names': 'Jane',
+                        },
+                    ],
+                    'keywords': ['genome', 'note'],
+                    'background': 'background text',
+                    'bioproject': 'PRJ123',
+                },
+                'methods': {
+                    'sample_acquisition': 'method1',
+                    'nucleic_acid_extraction': 'method2',
+                },
+                'assembly_stats': {
+                    'sequence_report': {
+                        'isolate_info': 'iso',
+                        'assembly_accession': 'GCA_000001',
+                    },
+                    'analysis': {
+                        'busco': {},
+                    },
+                    'assembly_graphs': {
+                        'kmer': {},
+                    },
+                    'chromosomal_pseudomolecules': ['chr1'],
+                    'samples': ['sample1'],
+                    'species': [
+                        {
+                            'scientific_name': 'Species one',
+                            'taxon_id': '12345',
+                        },
+                    ],
+                    'other': {
+                        'genome_annotation': 'ann',
+                    },
+                },
+                'software_tools': ['tool1'],
+                'references': ['ref1'],
+            },
+        )
 
-if __name__ == '__main__':
-    unittest.main()
+        converteds = converter.convert(obj)
+        ret = next(converteds)
+
+        self.assertEqual(ret.type, 'genome_note')
+        self.assertEqual(ret.id, '10.1234/example')
+
+        self.assertEqual(ret.attributes, {
+            'data_availability': '',
+            'title': 'Genome Note',
+            'abstract': '',
+            'keywords': ['genome', 'note'],
+            'background': 'background text',
+            'bioproject': 'PRJ123',
+            'authors': [
+                'John Smith',
+                'Jane Doe',
+            ],
+            'references': ['ref1'],
+            'method_sample_acquisition': 'method1',
+            'method_nucleic_acid_extraction': 'method2',
+            'isolate_info': 'iso',
+            'assembly_accession': 'GCA_000001',
+            'assembly_stats_analysis': {
+                'busco': {},
+            },
+            'assembly_stats_assembly_graphs': {
+                'kmer': {},
+            },
+            'chromosomal_pseudomolecules': ['chr1'],
+            'samples': ['sample1'],
+            'other': {
+                'genome_annotation': 'ann',
+            },
+            'species_scientific_name': 'Species one',
+            'species_taxon_id': '12345',
+            'software_tools': ['tool1'],
+        })
+
+        with self.assertRaises(StopIteration):
+            next(converteds)
