87c1ff9d4d351e6c2a903e85954a9b721783ffaa
Shweta Bhagat <sb83@sanger.ac.uk>
2026-05-13 15:39:32 +0100

    converter code changes

--- src/tol/flows/converters/sts_sample_project_to_elastic_sample_converter.py
+++ src/tol/flows/converters/sts_sample_project_to_elastic_sample_converter.py
@@ -119,6 +119,14 @@ class StsSampleProjectToElasticSampleConverter(
                     attributes['disposed'] = not bool(is_restore)
                 else:
                     attributes['disposed'] = True
+            
+            attributes['disposed'] = False
+            if 'disposal' in s.to_one_relationships and s.disposal is not None:
+                is_restore = getattr(s.disposal, 'restore?', None)
+                if is_restore is not None:
+                    attributes['disposed'] = not bool(is_restore)
+                else:
+                    attributes['disposed'] = True
 
             # Make tolid a relationship
             if s.public_name is not None and s.public_name != '':
