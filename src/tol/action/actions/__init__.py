2c0c43179c618e19f87b0940f0af4116f3dd707e
Kiernan Harding <kh16@sanger.ac.uk>
2026-09-10 15:11:39 +0100

    sort imports

--- src/tol/action/actions/__init__.py
+++ src/tol/action/actions/__init__.py
@@ -2,7 +2,16 @@
 #
 # SPDX-License-Identifier: MIT
 
-from .set_relationship_action import SetRelationshipAction  # noqa
-from .set_status_action import SetStatusAction  # noqa
-from .topup_action import TopupAction  # noqa
-from .upsert_action import UpsertAction  # noqa
+import importlib
+import re
+
+
+def __getattr__(name: str):
+	"""Lazily load an action class from its snake_case module.
+
+	Import and attribute errors are allowed to propagate to ``ActionUtils``.
+	"""
+	module_name = re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+	action_module = importlib.import_module(f'.{module_name}', __name__)
+	return getattr(action_module, name)
+
